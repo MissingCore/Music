15ad5b8c35ab3938f6331d89663df7a5fbe27597
cyanChill <anthonyliang9@gmail.com>
2024-04-18 18:40:06 -0400

    chore: Add functionality to `shuffle` button
    - Toggling it off will make `trackList` in the default order w/ no shuffling & update the `trackIdx` accordingly
    - Toggling it on will shuffle `trackList` & update `trackIdx` accordingly

--- src/features/playback/api/configs.ts
+++ src/features/playback/api/configs.ts
@@ -1,4 +1,8 @@
+import { atom } from "jotai";
 import { unwrap } from "jotai/utils";
+
+import { playingInfoAsyncAtom } from "./playing";
+import { getTrackList, shuffle } from "../utils/trackList";
 
 import { createAtomWithStorage } from "@/lib/jotai";
 
@@ -9,5 +13,28 @@ export const repeatAtom = unwrap(repeatAsyncAtom, (prev) => prev ?? false);
 
 /** @description [FOR INTERNAL USE ONLY] */
 export const shuffleAsyncAtom = createAtomWithStorage("shuffle", false);
+const shuffleUnwrapAtom = unwrap(shuffleAsyncAtom, (prev) => prev ?? false);
 /** @description If the next track should be "random". */
-export const shuffleAtom = unwrap(shuffleAsyncAtom, (prev) => prev ?? false);
+export const shuffleAtom = atom(
+  (get) => get(shuffleUnwrapAtom),
+  async (get, set) => {
+    const currShuffleStatus = await get(shuffleAsyncAtom);
+    const currPlayingInfo = await get(playingInfoAsyncAtom);
+
+    if (currPlayingInfo.listSrc) {
+      let newTrackList = await getTrackList(currPlayingInfo.listSrc);
+      if (!currShuffleStatus) newTrackList = shuffle(newTrackList);
+      const newIdx = newTrackList.findIndex(
+        (id) => id === currPlayingInfo.trackId,
+      )!;
+
+      set(playingInfoAsyncAtom, {
+        ...currPlayingInfo,
+        trackList: newTrackList,
+        trackIdx: newIdx,
+      });
+    }
+
+    set(shuffleAsyncAtom, !currShuffleStatus);
+  },
+);
