f3b554a7108a2982530084b443c4c98f09d75030
cyanChill <anthonyliang9@gmail.com>
2024-04-20 18:42:32 -0400

    chore: Fix issue where tapping on the slider didn't update the current position of the track
    - Switch to a JS-based slider
      - This handles the `onSlidingComplete` event differently
    - Additional benefit is that is provides more ways to directly style the track & thumb

--- src/features/playback/components/MediaSlider.tsx
+++ src/features/playback/components/MediaSlider.tsx
@@ -1,5 +1,6 @@
-import Slider from "@react-native-community/slider";
+import { Slider } from "@miblanchard/react-native-slider";
 import { useAtom, useAtomValue, useSetAtom } from "jotai";
+import { useState } from "react";
 import { Text, View } from "react-native";
 
 import { updateTrackPosAtom } from "../api/controls";
@@ -8,30 +9,44 @@ import { currentTrackDataAtom, trackPositionMsAtom } from "../api/playing";
 import Colors from "@/constants/Colors";
 import { getTrackDuration } from "@/features/track/components/TrackDuration";
 
-/** @description Allows seeking on the current track. */
+/**
+ * @description Allows seeking on the current track & displays the
+ *  current track position.
+ */
 export function MediaSlider() {
   const updateTrackPos = useSetAtom(updateTrackPosAtom);
   const trackData = useAtomValue(currentTrackDataAtom);
   const [trackPosMs, setTrackPosMs] = useAtom(trackPositionMsAtom);
+  const [isSliding, setIsSliding] = useState(false);
+  const [slidingTrackPos, setSlidingTrackPos] = useState<number | null>(null);
 
   if (!trackData) return null;
 
   return (
     <View className="mt-16 w-full p-4">
       <Slider
-        value={trackPosMs}
+        value={slidingTrackPos ?? trackPosMs}
         minimumValue={0}
         maximumValue={Math.floor(trackData.duration * 1000)}
-        onSlidingComplete={updateTrackPos}
-        onValueChange={setTrackPosMs}
+        onSlidingStart={() => setIsSliding(true)}
+        onSlidingComplete={() => {
+          updateTrackPos();
+          setIsSliding(false);
+          setSlidingTrackPos(null);
+        }}
+        onValueChange={([newPosMs]) => {
+          if (isSliding) setSlidingTrackPos(Math.floor(newPosMs));
+          setTrackPosMs(Math.floor(newPosMs));
+        }}
         minimumTrackTintColor={Colors.accent500}
-        maximumTrackTintColor={Colors.surface50}
+        maximumTrackTintColor={Colors.surface500}
         thumbTintColor={Colors.foreground50}
+        trackStyle={{ height: 8, borderRadius: 24 }}
       />
 
       <View className="flex-row justify-between">
         <Text className="font-geistMono text-sm text-foreground50">
-          {getTrackDuration(trackPosMs / 1000)}
+          {getTrackDuration((slidingTrackPos ?? trackPosMs) / 1000)}
         </Text>
         <Text className="font-geistMono text-sm text-foreground50">
           {getTrackDuration(trackData.duration)}
