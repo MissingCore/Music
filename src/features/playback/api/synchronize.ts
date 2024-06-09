MissingCore/Music

commit df9d1debddb784488456744a7c52833709cebffe

fix: Slow favorite/unfavorite action for tracks
- Caused by unoptimization in `resynchronizeOnAtom` where we always change values even though they don't need to be changed

src/features/playback/api/synchronize.ts
@@ -23,21 +23,23 @@ export const resynchronizeOnAtom = atom(
   null,
   async (get, set, { action, data }: ResynchronizeArgs) => {
     // Synchronize "Recently Played" list.
-    const recentlyPlayed = await get(recentlyPlayedAsyncAtom);
-    let newRecentList = [...recentlyPlayed];
+    if (action !== "update") {
+      const recentlyPlayed = await get(recentlyPlayedAsyncAtom);
+      let newRecentList = [...recentlyPlayed];
 
-    if (action === "delete") {
-      newRecentList = recentlyPlayed.filter(
-        (reference) => !areTrackReferencesEqual(reference, data),
-      );
-    } else if (action === "rename") {
-      newRecentList = recentlyPlayed.map((reference) => {
-        if (!areTrackReferencesEqual(reference, data.old)) return reference;
-        return data.latest;
-      });
+      if (action === "delete") {
+        newRecentList = recentlyPlayed.filter(
+          (reference) => !areTrackReferencesEqual(reference, data),
+        );
+      } else if (action === "rename") {
+        newRecentList = recentlyPlayed.map((reference) => {
+          if (!areTrackReferencesEqual(reference, data.old)) return reference;
+          return data.latest;
+        });
+      }
+
+      set(recentlyPlayedAsyncAtom, newRecentList);
     }
-
-    set(recentlyPlayedAsyncAtom, newRecentList);
 
     // Synchronize track list.
     const playingMedia = await get(playingMediaAsyncAtom);
@@ -53,6 +55,7 @@ export const resynchronizeOnAtom = atom(
             trackList.reference,
             action === "rename" ? data.old : data!,
           );
+
     if (shouldResynchronizeTrackList) {
       if (action === "delete") {
         newPlayingMedia.listIndex = 0;
@@ -70,9 +73,9 @@ export const resynchronizeOnAtom = atom(
       } else if (action === "rename") {
         newTrackList.reference = data.latest;
       }
-    }
 
-    set(playingMediaAsyncAtom, newPlayingMedia);
-    set(trackListAsyncAtom, newTrackList);
+      set(playingMediaAsyncAtom, newPlayingMedia);
+      set(trackListAsyncAtom, newTrackList);
+    }
   },
 );

commit 1f88ada3925d6bcdf665c7777898ed28caed65a9

fix: Revert regression introduced in df9d1de
- When we excluded the "update" check from resynchronizing the "Recently Played" list, we inadvertently prevented any changes in track count from being applied
- The exclusion was due to the delay from pressing the "Favorite Track" button
  - From further testing, some of the delay might have been contributed by the cached dev build files remaining

src/features/playback/api/synchronize.ts
@@ -23,23 +23,21 @@ export const resynchronizeOnAtom = atom(
   null,
   async (get, set, { action, data }: ResynchronizeArgs) => {
     // Synchronize "Recently Played" list.
-    if (action !== "update") {
-      const recentlyPlayed = await get(recentlyPlayedAsyncAtom);
-      let newRecentList = [...recentlyPlayed];
+    const recentlyPlayed = await get(recentlyPlayedAsyncAtom);
+    let newRecentList = [...recentlyPlayed];
 
-      if (action === "delete") {
-        newRecentList = recentlyPlayed.filter(
-          (reference) => !areTrackReferencesEqual(reference, data),
-        );
-      } else if (action === "rename") {
-        newRecentList = recentlyPlayed.map((reference) => {
-          if (!areTrackReferencesEqual(reference, data.old)) return reference;
-          return data.latest;
-        });
-      }
-
-      set(recentlyPlayedAsyncAtom, newRecentList);
+    if (action === "delete") {
+      newRecentList = recentlyPlayed.filter(
+        (reference) => !areTrackReferencesEqual(reference, data),
+      );
+    } else if (action === "rename") {
+      newRecentList = recentlyPlayed.map((reference) => {
+        if (!areTrackReferencesEqual(reference, data.old)) return reference;
+        return data.latest;
+      });
     }
+
+    set(recentlyPlayedAsyncAtom, newRecentList);
 
     // Synchronize track list.
     const playingMedia = await get(playingMediaAsyncAtom);