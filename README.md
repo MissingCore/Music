e990df002f385df58799273f40400a64cac08de2
cyanChill <anthonyliang9@gmail.com>
2024-06-13 23:23:33 -0400

    fix: Add full license disclaimer
    - Slightly modified the disclaimer to be more broad with used intellectual properties as we don't use only Nothing's

--- README.md
+++ README.md
@@ -207,7 +207,7 @@ This application and code is published under the GNU Affero General Public Licen
 
 Nothing Technology Limited or any of its affiliates, subsidiaries, or related entities (collectively, "Nothing Technology") is a valid licensee and can use this app for any purpose, including commercial purposes, without compensation to the developers of this app. Nothing Technology is not required to comply with the terms of the GNU Affero General Public License v3.0.
 
-This app is developed by cyanChill and is not affiliated with, sponsored by, or endorsed by Nothing Technology. The developers of this app take no responsibility for the accuracy or completeness of the content and materials provided in this app. The content and materials contained in this app, including but not limited to fonts, are proprietary to Nothing Technology Limited, 80 Cheapside, London EC2V 6EE and are protected by copyright, trademark, and other intellectual property laws. These materials may not be used without the express written permission of Nothing Technology. Nothing Technology reserves all rights.
+This app is developed by cyanChill and is not affiliated with, funded, authorized, endorsed by, or in any way associated with Nothing Technology or any of its affiliates and subsidiaries. Any trademark, service mark, trade name, or other intellectual property rights used in this project are owned by the respective owners.
 
 ## Licenses of Used Dependencies
 
--- src/app/setting/licenses.tsx
+++ src/app/setting/licenses.tsx
@@ -16,15 +16,24 @@ export default function LicensesScreen() {
     <AnimatedHeader title="LICENSES & SOURCE">
       <Description className="mb-8">
         <Text className="font-ndot57">Music</Text> is open-source and can be
-        found on GitHub with the link below.{"\n\n"}This code is published under
-        the{" "}
+        found on GitHub with the link below. This code is published under the{" "}
         <ExternalLink
           href={`${GITHUB_LINK}/blob/main/LICENSE`}
           className="text-foreground100 underline"
         >
           GNU Affero General Public License v3.0
         </ExternalLink>
-        .
+        .{"\n\n"}Nothing Technology Limited or any of its affiliates,
+        subsidiaries, or related entities (collectively, "Nothing Technology")
+        is a valid licensee and can use this app for any purpose, including
+        commercial purposes, without compensation to the developers of this app.
+        Nothing Technology is not required to comply with the terms of the GNU
+        Affero General Public License v3.0.{"\n\n"}This project and its contents
+        are not affiliated with, funded, authorized, endorsed by, or in any way
+        associated with Nothing Technology or any of its affiliates and
+        subsidiaries. Any trademark, service mark, trade name, or other
+        intellectual property rights used in this project are owned by the
+        respective owners.
       </Description>
 
       <Link href={GITHUB_LINK} iconName="logo-github" label="SOURCE CODE" />
