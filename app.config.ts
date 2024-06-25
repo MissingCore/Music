cf3f57a57228e894cffe7f6dd3bed325743d087f
Anthony Liang <83375816+cyanChill@users.noreply.github.com>
2024-06-25 01:30:40 -0400

    ci: Automatic App Bundle Upload & APK Deobfuscation (#13)
    # Why
    
    This aims to help speed up the process of publishing `.aab` files
    generated from our workflows as we no longer have to manually upload the
    `.aab` directly into the Play Console.
    
    In addition, to help with deobfuscation of the `.apk` files we publish
    in each release (since the minification & obfuscation process in the
    last PR applies to them as well), we now also zip up the `mapping.txt`
    and `native-debug-symbols.zip` files needed for deobfuscation.
    
    
    # How
    
    We utilize the `r0adkll/upload-google-play@v1` GitHub Action to upload
    the `.aab` file generated to the "Internal Testing" track in the Play
    console.
    
    For the deobfuscation files, we used the Unix `zip` command to zip the 2
    files mentioned above, and attach it to the release generated from the
    `softprops/action-gh-release@v2` GitHub Action through the `files`
    input.
    
    
    # Test Plan
    
    We utilized the `eas-local.yml` (now called `test-workflows.yml`) file
    to test out the code we'll add to the main workflows to accomplish these
    tasks. We created a new release, `v1.0.0-rc.4.5-auto-upload` to test if
    the upload to Play Console workflow worked.
    
    
    # Checklist
    
    <!--
    Please check the appropriate items below if they apply to your diff.
    -->
    
    - [x] Documentation is up to date to reflect these changes (ie:
    `CHANGELOG.md` & `README.md`).
    - [ ] Add new dependencies into `THIRD_PARTY.md`.
    - [ ] This diff will work correctly for `pnpm android:prod`.

--- app.config.ts
+++ app.config.ts
@@ -4,7 +4,7 @@ export default (): ExpoConfig => ({
   name: "Music",
   slug: "Music",
   description: "A Nothing inspired music player.",
-  version: "1.0.0-rc.4.5-minify-v2",
+  version: "1.0.0-rc.4.5-auto-upload",
   platforms: ["android"],
   githubUrl: "https://github.com/MissingCore/Music",
   orientation: "portrait",
@@ -15,7 +15,7 @@ export default (): ExpoConfig => ({
   assetBundlePatterns: ["**/*"],
   android: {
     package: "com.cyanchill.missingcore.music",
-    versionCode: 7,
+    versionCode: 8,
     adaptiveIcon: {
       foregroundImage: "./assets/adaptive-icon.png",
       monochromeImage: "./assets/adaptive-icon-monochrome.png",
