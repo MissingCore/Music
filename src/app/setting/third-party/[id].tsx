b4293e9fa7aa31b967695978792a102a90fe4374
cyanChill <anthonyliang9@gmail.com>
2024-07-03 16:26:15 -0400

    chore: Updated license related setting screens

--- src/app/setting/third-party/[id].tsx
+++ src/app/setting/third-party/[id].tsx
@@ -8,11 +8,11 @@ import { Back } from "@/components/navigation/back";
 import { NavLink } from "@/components/navigation/nav-link";
 import { Description } from "@/components/ui/text";
 
-/** @description Screen for `/setting/licenses/[...id]` route. */
-export default function CurrentLicenseScreen() {
-  const { id } = useLocalSearchParams<{ id: string[] }>();
+/** @description Screen for `/setting/third-party/[id]` route. */
+export default function PackageLicenseScreen() {
+  const { id } = useLocalSearchParams<{ id: string }>();
 
-  const licenseInfo = LicensesList[id!.join("/") as keyof typeof LicensesList];
+  const licenseInfo = LicensesList[id! as keyof typeof LicensesList];
 
   if (!licenseInfo) return <Back />;
 
@@ -30,18 +30,11 @@ export default function CurrentLicenseScreen() {
         )}
       </Description>
 
-      <NavLink
-        href={licenseInfo.source}
-        iconName={
-          licenseInfo.source.startsWith("https://github.com")
-            ? "logo-github"
-            : "globe-outline"
-        }
-        label="Source"
-        external
-      />
+      <View className="-mx-4">
+        <NavLink href={licenseInfo.source} label="Source" external />
+      </View>
 
-      <View className="mb-6 border-t border-surface700" />
+      <View className="mb-6 mt-2 h-px bg-surface850" />
 
       <Description intent="setting">{licenseInfo.licenseText}</Description>
     </AnimatedHeader>
