7b816674f79203cbe971038ceae8cd08183b94bf
cyanChill <anthonyliang9@gmail.com>
2024-07-28 00:37:20 -0400

    style: Fix breadcrumb animation
    - Had a problem where the exiting animation on removed elements that are positioned outside of the screen width isn't displayed
      - ie: We immediately jump to the new ending segment of the breadcrumb and don't experience the "exiting" animation
    - We currently have a really janky fix that I should probably improve
    - Also combined `FolderScreen` & `FolderContents` into a single component as we no longer have the breadcrumbs there

--- src/app/(app)/(home)/folder/[...id].tsx
+++ src/app/(app)/(home)/folder/[...id].tsx
@@ -10,6 +10,7 @@ import { useFolderContentForPath } from "@/api/file-nodes/[...id]";
 import { folderPathAtom } from "./_layout";
 
 import { ActionButton } from "@/components/form/action-button";
+import { LoadingIndicator } from "@/components/ui/loading";
 import { Description } from "@/components/ui/text";
 import { Track } from "@/features/track/components/track";
 
@@ -20,29 +21,21 @@ export default function FolderScreen() {
   }>();
   const updateFolderPath = useSetAtom(folderPathAtom);
 
+  const fullPath = id.join("/");
+
+  const { isPending, error, data } = useFolderContentForPath(fullPath);
+
   useEffect(() => {
     updateFolderPath(id);
   }, [id, updateFolderPath]);
 
-  return (
-    <ScrollView
-      showsVerticalScrollIndicator={false}
-      contentContainerClassName="grow pt-2"
-    >
-      <FolderContents
-        currPath={id.map((segment) => encodeURIComponent(segment)).join("/")}
-      />
-    </ScrollView>
-  );
-}
-
-function FolderContents({ currPath }: { currPath: string }) {
-  const { isPending, error, data } = useFolderContentForPath(
-    decodeURIComponent(currPath),
-  );
-
-  if (isPending) return <View className="w-full flex-1 px-4" />;
-  else if (error) {
+  if (isPending) {
+    return (
+      <View className="w-full flex-1 px-4">
+        <LoadingIndicator />
+      </View>
+    );
+  } else if (error) {
     return (
       <View className="w-full flex-1 px-4">
         <Description intent="error">Error: Directory not found</Description>
@@ -59,44 +52,53 @@ function FolderContents({ currPath }: { currPath: string }) {
   // Information about this track list.
   const trackSource = {
     type: "folder",
-    name: `[Folder] ${decodeURIComponent(currPath).split("/").at(-1)}`,
-    id: `${decodeURIComponent(currPath)}/`,
+    name: `[Folder] ${fullPath.split("/").at(-1)}`,
+    id: `${fullPath}/`,
   } as const;
 
   return (
-    <FlashList
-      estimatedItemSize={66} // 58px Height + 8px Margin Bottom
-      data={[...data.subDirectories, undefined, ...data.tracks]}
-      keyExtractor={(_, index) => `${index}`}
-      renderItem={({ item }) => {
-        // Render divider if we have subdirectories & tracks.
-        if (item === undefined) {
-          if (data.subDirectories.length > 0 && data.tracks.length > 0)
-            return <View className="mb-2 h-px bg-surface850" />;
-          else return null;
-        }
-        return (
-          <View className="mb-2">
-            {isTrackContent(item) ? (
-              <Track {...{ ...item, trackSource }} />
-            ) : (
-              <ActionButton
-                onPress={() => router.push(`/folder/${currPath}/${item.name}`)}
-                textContent={[item.name, null]}
-                Image={
-                  <View className="pointer-events-none rounded-sm bg-surface800 p-1">
-                    <FolderOutline size={40} />
-                  </View>
-                }
-                icon={{ Element: <ArrowRight size={24} /> }}
-              />
-            )}
-          </View>
-        );
-      }}
+    <ScrollView
       showsVerticalScrollIndicator={false}
-      contentContainerStyle={{ paddingHorizontal: 16 }}
-    />
+      contentContainerClassName="grow pt-2"
+    >
+      <FlashList
+        estimatedItemSize={66} // 58px Height + 8px Margin Bottom
+        data={[...data.subDirectories, undefined, ...data.tracks]}
+        keyExtractor={(_, index) => `${index}`}
+        renderItem={({ item }) => {
+          // Render divider if we have subdirectories & tracks.
+          if (item === undefined) {
+            if (data.subDirectories.length > 0 && data.tracks.length > 0)
+              return <View className="mb-2 h-px bg-surface850" />;
+            else return null;
+          }
+          return (
+            <View className="mb-2">
+              {isTrackContent(item) ? (
+                <Track {...{ ...item, trackSource }} />
+              ) : (
+                <ActionButton
+                  onPress={() =>
+                    router.push(
+                      `/folder/${id.map((segment) => encodeURIComponent(segment)).join("/")}/${item.name}`,
+                    )
+                  }
+                  textContent={[item.name, null]}
+                  Image={
+                    <View className="pointer-events-none rounded-sm bg-surface800 p-1">
+                      <FolderOutline size={40} />
+                    </View>
+                  }
+                  icon={{ Element: <ArrowRight size={24} /> }}
+                />
+              )}
+            </View>
+          );
+        }}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingHorizontal: 16 }}
+      />
+    </ScrollView>
   );
 }
 
--- src/app/(app)/(home)/folder/_layout.tsx
+++ src/app/(app)/(home)/folder/_layout.tsx
@@ -2,8 +2,13 @@ import { Link, Stack } from "expo-router";
 import { atom, useAtomValue } from "jotai";
 import { ScopeProvider } from "jotai-scope";
 import { Fragment, useEffect, useRef } from "react";
-import { Text, View } from "react-native";
-import Animated, { FadeInLeft, FadeOutRight } from "react-native-reanimated";
+import { View } from "react-native";
+import Animated, {
+  FadeInLeft,
+  FadeOutRight,
+  useAnimatedStyle,
+  useSharedValue,
+} from "react-native-reanimated";
 
 import { cn } from "@/lib/style";
 import { ScrollRow } from "@/components/ui/container";
@@ -38,38 +43,87 @@ export default function FolderLayout() {
 /** Custom folder structure breadcrumbs. */
 function Breadcrumbs() {
   const pathSegments = useAtomValue(folderPathAtom);
+  const prevPathSegments = useRef<string[]>([]);
   const breadcrumbsRef = useRef<ScrollRow.Ref>(null);
+  const containerWidth = useSharedValue(0);
+  const currWidth = useSharedValue(0);
+  const lastWidth = useSharedValue(0);
+  const removedWidth = useSharedValue(0);
+
+  const onLayoutShift = (newWidth: number) => {
+    currWidth.value = newWidth;
+    if (newWidth < lastWidth.value) {
+      removedWidth.value = lastWidth.value - newWidth;
+    }
+    lastWidth.value = newWidth;
+  };
+
+  const wrapperStyle = useAnimatedStyle(() => ({
+    paddingRight: removedWidth.value,
+  }));
 
   useEffect(() => {
-    if (breadcrumbsRef.current) breadcrumbsRef.current.scrollToEnd();
+    // Automatically scroll if we've going deeper in the tree.
+    if (prevPathSegments.current.length <= pathSegments.length) {
+      if (breadcrumbsRef.current) breadcrumbsRef.current.scrollToEnd();
+    }
+    prevPathSegments.current = pathSegments;
   }, [pathSegments]);
 
   return (
-    <ScrollRow ref={breadcrumbsRef} className="pb-2">
-      {pathSegments.map((dirName, idx) => (
-        <Fragment key={idx}>
-          {idx !== 0 && (
-            <Text className="px-1 font-geistMono text-sm text-foreground50">
-              /
-            </Text>
-          )}
-          <Animated.View entering={FadeInLeft} exiting={FadeOutRight}>
-            <Link
-              href={`/folder/${pathSegments
-                .slice(0, idx + 1)
-                .map((segment) => encodeURIComponent(segment))
-                .join("/")}`}
-              disabled={idx === pathSegments.length - 1}
-              className={cn(
-                "font-geistMono text-sm text-foreground50 active:opacity-75",
-                { "text-accent50": idx === pathSegments.length - 1 },
-              )}
-            >
-              {dirName}
-            </Link>
-          </Animated.View>
-        </Fragment>
-      ))}
+    <ScrollRow
+      ref={breadcrumbsRef}
+      onLayout={({ nativeEvent }) => {
+        containerWidth.value = nativeEvent.layout.width;
+      }}
+      className="pb-2"
+    >
+      <Animated.View
+        onLayout={({ nativeEvent }) => onLayoutShift(nativeEvent.layout.width)}
+        className="flex-row gap-2"
+      >
+        {pathSegments.map((dirName, idx) => (
+          <Fragment key={idx}>
+            {idx !== 0 && (
+              <Animated.Text
+                entering={FadeInLeft}
+                exiting={FadeOutRight}
+                className="px-1 font-geistMono text-sm text-foreground50"
+              >
+                /
+              </Animated.Text>
+            )}
+            <Animated.View entering={FadeInLeft} exiting={FadeOutRight}>
+              <Link
+                href={`/folder/${pathSegments
+                  .slice(0, idx + 1)
+                  .map((segment) => encodeURIComponent(segment))
+                  .join("/")}`}
+                disabled={idx === pathSegments.length - 1}
+                className={cn(
+                  "font-geistMono text-sm text-foreground50 active:opacity-75",
+                  { "text-accent50": idx === pathSegments.length - 1 },
+                )}
+              >
+                {dirName}
+              </Link>
+            </Animated.View>
+          </Fragment>
+        ))}
+      </Animated.View>
+      <Animated.View
+        onLayout={({ nativeEvent }) => {
+          if (nativeEvent.layout.width !== 0) {
+            breadcrumbsRef.current?.scrollTo({
+              x: currWidth.value - removedWidth.value - containerWidth.value,
+            });
+            setTimeout(() => {
+              removedWidth.value = 0;
+            }, 300);
+          }
+        }}
+        style={wrapperStyle}
+      />
     </ScrollRow>
   );
 }
