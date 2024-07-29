dbb840e43ce12d417178813671868f30f97219da
cyanChill <anthonyliang9@gmail.com>
2024-07-29 17:02:14 -0400

    docs: Instructions for data migration
    - This is to help prevent running the same adjustment logic multiple time, wasting time
    - This is to also remind us to ensure that this code works and complies to any changes we make (ie: works after we do some schema changes)

--- src/features/indexing/Config.ts
+++ src/features/indexing/Config.ts
@@ -7,19 +7,24 @@ export const AdjustmentOptions = [
 
 export type AdjustmentOption = (typeof AdjustmentOptions)[number];
 
-/** History of data adjustments due to "breaking" changes. */
+/**
+ * History of data adjustments due to "breaking" changes.
+ *
+ * TODO: When adding new entries, make sure to:
+ *  - Remove the use of the `AdjustmentOption` in earlier changes. This
+ *  helps prevent duplicate logic from being run if the user updates to
+ *  the latest version from a very old version.
+ *  - Periodically update the adjustment code to work with the latest
+ *  version and have it work with any new changes we added (ie: if we
+ *  add a new schema field).
+ */
 export const OverrideHistory: Record<
   number,
   { version: string; changes: AdjustmentOption[] }
 > = {
   0: {
     version: "v1.0.0-rc.10",
-    changes: [
-      "artwork-retry",
-      "invalid-tracks-retry",
-      "album-fracturization",
-      "library-scan",
-    ],
+    changes: ["artwork-retry", "album-fracturization"],
   },
   1: {
     version: "v1.0.0-rc.11",
