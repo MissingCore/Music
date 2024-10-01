62a60a3cfebeeb94eaa3e7a0e7f76f682af0b05a
cyanChill <anthonyliang9@gmail.com>
2024-09-30 20:54:18 -0400

    chore: Enable German translation option

--- mobile/src/modules/i18n/constants.ts
+++ mobile/src/modules/i18n/constants.ts
@@ -1,5 +1,6 @@
 /** List of available languages that our app supports. */
 export const LANGUAGES = [
+  { code: "de", name: "Deutsch" },
   { code: "en", name: "English" },
   { code: "ja", name: "日本語" },
 ] as const;
