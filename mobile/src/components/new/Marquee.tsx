3debb4296011e14a4798c4ea4e1178d3f5234342
cyanChill <83375816+cyanChill@users.noreply.github.com>
2024-10-02 00:51:47 -0400

    chore: Add `<Marquee />` component
    - It's not the typical marquee where the start of the content can be seen when we reach the end

---
+++ mobile/src/components/new/Marquee.tsx
@@ -0,0 +1,62 @@
+import Animated, {
+  Easing,
+  useAnimatedStyle,
+  useSharedValue,
+  withDelay,
+  withRepeat,
+  withSequence,
+  withTiming,
+} from "react-native-reanimated";
+import { useEffect, useState } from "react";
+
+/** Used to progressively display long content. */
+export function Marquee({ children }: { children: React.ReactNode }) {
+  const offset = useSharedValue(0);
+  const [containerWidth, setContainerWidth] = useState(-1);
+  const [contentWidth, setContentWidth] = useState(-1);
+
+  useEffect(() => {
+    if (containerWidth === -1 || contentWidth === -1) return;
+    if (contentWidth <= containerWidth) return;
+
+    const scrollRoom = contentWidth - containerWidth;
+    // Make sure we translate `24px` a second.
+    const totalDuration = (scrollRoom / 24) * 1000;
+
+    // Only attempt marquee animation if the content is wide enough.
+    offset.value = withRepeat(
+      withSequence(
+        withDelay(
+          4000,
+          withTiming(scrollRoom, {
+            duration: totalDuration,
+            easing: Easing.linear,
+          }),
+        ),
+        withDelay(4000, withTiming(0, { duration: 0, easing: Easing.linear })),
+      ),
+      -1,
+    );
+  }, [containerWidth, contentWidth, offset]);
+
+  const animatedStyles = useAnimatedStyle(() => ({
+    transform: [{ translateX: -offset.value }],
+  }));
+
+  return (
+    <Animated.ScrollView
+      onLayout={(e) => setContainerWidth(e.nativeEvent.layout.width)}
+      horizontal
+      pointerEvents="none"
+      showsHorizontalScrollIndicator={false}
+      contentContainerClassName="grow justify-center overflow-hidden"
+    >
+      <Animated.View
+        onLayout={(e) => setContentWidth(e.nativeEvent.layout.width)}
+        style={animatedStyles}
+      >
+        {children}
+      </Animated.View>
+    </Animated.ScrollView>
+  );
+}
