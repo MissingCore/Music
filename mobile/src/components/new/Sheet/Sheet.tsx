124d05011430e24e00d41147de3fa6f9e265f495
cyanChill <83375816+cyanChill@users.noreply.github.com>
2024-10-27 20:23:57 -0400

    chore: Accept optional title for sheet
    - By default, wrap sheet children with a `<View />`

--- mobile/src/components/new/Sheet/Sheet.tsx
+++ mobile/src/components/new/Sheet/Sheet.tsx
@@ -1,4 +1,6 @@
 import { cssInterop } from "nativewind";
+import type { StyleProp, ViewStyle } from "react-native";
+import { View } from "react-native";
 import type { ActionSheetProps } from "react-native-actions-sheet";
 import ActionSheet from "react-native-actions-sheet";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
@@ -15,13 +17,16 @@ const WrappedActionSheet = cssInterop(ActionSheet, {
 export function Sheet({
   title,
   snapTop,
-  containerClassName,
+  contentContainerClassName,
+  contentContainerStyle,
+  children,
   ...rest
 }: ActionSheetProps & {
   title?: string;
   /** If the sheet should open at max screen height. */
   snapTop?: boolean;
-  containerClassName?: string;
+  contentContainerClassName?: string;
+  contentContainerStyle?: StyleProp<ViewStyle>;
 }) {
   const insets = useSafeAreaInsets();
 
@@ -29,11 +34,9 @@ export function Sheet({
     <WrappedActionSheet
       gestureEnabled
       CustomHeaderComponent={<SheetHeader title={title} />}
-      containerClassName={cn(
-        "rounded-t-lg bg-canvas p-4 pt-0 dark:bg-neutral5",
-        containerClassName,
-        { "flex-1": snapTop },
-      )}
+      containerClassName={cn("rounded-t-lg bg-canvas dark:bg-neutral5", {
+        "flex-1": snapTop,
+      })}
       // Have sheet max height be right before the top app bar.
       //  - Note 1: We've patched the package as it only relies on the top
       //  insets for IOS
@@ -41,6 +44,13 @@ export function Sheet({
       //  work as mentioned: https://github.com/ammarahm-ed/react-native-actions-sheet/issues/322#issuecomment-2029560154
       safeAreaInsets={{ ...insets, top: insets.top + 56 }}
       {...rest}
-    />
+    >
+      <View
+        style={contentContainerStyle}
+        className={cn("p-4 pt-0", contentContainerClassName)}
+      >
+        {children}
+      </View>
+    </WrappedActionSheet>
   );
 }
