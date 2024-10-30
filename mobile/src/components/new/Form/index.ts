90d1ea22225761c6ea758d53d21082cf5c7633b2
cyanChill <83375816+cyanChill@users.noreply.github.com>
2024-10-30 16:48:27 -0400

    chore: Updated input fields
    - Combine them into a single file
    - Removed use of ref (since we never really used them)
      - I think this was added for supporting the inputs exported by Gorhom Bottom Sheets, but we discarded their use since the `editable` prop didn't work

--- mobile/src/components/new/Form/index.ts
+++ mobile/src/components/new/Form/index.ts
@@ -1,10 +1,8 @@
 export { Button } from "./Button";
 export { IconButton } from "./IconButton";
+export { NumericInput, TextInput } from "./Input";
 export { Legend, LegendItem } from "./Legend";
-export { NumericInput } from "./NumericInput";
 export { ProgressBar } from "./ProgressBar";
 export { Ripple } from "./Ripple";
 export { Checkbox, Radio } from "./Selection";
 export { Switch } from "./Switch";
-export { TextInput } from "./TextInput";
-export { useInputRef } from "./useInputRef";
