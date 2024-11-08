add63e332637a7d1d9b083c2190629307ff379d8
cyanChill <83375816+cyanChill@users.noreply.github.com>
2024-11-07 22:49:15 -0500

    chore: Update back behavior on tab navigation
    - `tabBar={() => null}` is probably the best (or only) way of hiding the tab bar besides maybe applying `display: "none"`

--- mobile/src/app/(main)/(home)/_layout.tsx
+++ mobile/src/app/(main)/(home)/_layout.tsx
@@ -2,7 +2,11 @@ import { MaterialTopTabs } from "@/layouts";
 
 export default function HomeLayout() {
   return (
-    <MaterialTopTabs initialRouteName="index" tabBar={() => null}>
+    <MaterialTopTabs
+      initialRouteName="index"
+      backBehavior="history"
+      tabBar={() => null}
+    >
       <MaterialTopTabs.Screen name="index" />
       <MaterialTopTabs.Screen name="folder" />
       <MaterialTopTabs.Screen name="playlist" />
