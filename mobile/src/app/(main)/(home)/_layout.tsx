4204120aaacc9ae5a4b156e1678b0b1f5a193261
cyanChill <83375816+cyanChill@users.noreply.github.com>
2024-11-08 18:54:44 -0500

    chore: Modify Tab history to work like Stack
    - Modifiying the event object returned by the `state` listener or a clone of that object will modify the state used without using the `reset` function
    - This might not be the intended behavior (so it's "fragile" code) and it might change in future versions

--- mobile/src/app/(main)/(home)/_layout.tsx
+++ mobile/src/app/(main)/(home)/_layout.tsx
@@ -1,11 +1,55 @@
+import type {
+  EventArg,
+  ParamListBase,
+  TabNavigationState,
+} from "@react-navigation/native";
+import { useCallback, useMemo, useRef } from "react";
+
 import { MaterialTopTabs } from "@/layouts";
 
+type TabState = EventArg<
+  "state",
+  any,
+  { state: TabNavigationState<ParamListBase> }
+>;
+
 export default function HomeLayout() {
+  // Should be fine to store navigation state in ref as it doesn't affect rendering.
+  //  - https://react.dev/learn/referencing-values-with-refs#when-to-use-refs
+  const prevTabState = useRef<TabState>();
+
+  /** Have Tab history operate like Stack history. */
+  const manageAsStackHistory = useCallback((e: TabState) => {
+    if (prevTabState.current) {
+      // Get top of history.
+      const currRoute = e.data.state.history.at(-1)!;
+      // See if route was seen previously.
+      const oldHistory = prevTabState.current.data.state.history;
+      const atIndex = oldHistory.findIndex(({ key }) => currRoute.key === key);
+      // Handle if we visited this tab earlier.
+      if (atIndex !== -1) {
+        // FIXME: Modifying the value in `e` for some reason modifies the
+        // original reference (even if we cloned `e` via object spreading).
+        //  - This might be fragile code, so we might swap over to the use
+        //  of the `reset` function.
+        //  - https://reactnavigation.org/docs/navigation-actions/#reset
+        e.data.state.history = oldHistory.toSpliced(atIndex + 1);
+      }
+    }
+    prevTabState.current = e;
+  }, []);
+
+  const listeners = useMemo(
+    () => ({ state: manageAsStackHistory }),
+    [manageAsStackHistory],
+  );
+
   return (
     <MaterialTopTabs
       initialRouteName="index"
       backBehavior="history"
-      tabBar={() => null}
+      tabBar={noop}
+      screenListeners={listeners}
     >
       <MaterialTopTabs.Screen name="index" />
       <MaterialTopTabs.Screen name="folder" />
@@ -16,3 +60,5 @@ export default function HomeLayout() {
     </MaterialTopTabs>
   );
 }
+
+const noop = () => null;
