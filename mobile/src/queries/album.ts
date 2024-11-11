a57a33b3ebf79f222abe8039955453a541963a59
cyanChill <83375816+cyanChill@users.noreply.github.com>
2024-11-10 20:43:28 -0500

    chore: Initial idea for `@/queries`

---
+++ mobile/src/queries/album.ts
@@ -0,0 +1,15 @@
+import { createQueryKeys } from "@lukemorales/query-key-factory";
+
+import { getAlbum, getAlbums } from "@/api/new/album";
+
+/** Query keys used in `useQuery` for albums. */
+export const albumKeys = createQueryKeys("albums", {
+  all: {
+    queryKey: null,
+    queryFn: () => getAlbums(),
+  },
+  detail: (albumId: string) => ({
+    queryKey: [albumId],
+    queryFn: () => getAlbum({ id: albumId }),
+  }),
+});
