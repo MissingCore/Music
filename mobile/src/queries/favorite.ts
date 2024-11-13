fa138a85c9c7b2412892071faf4c9f6234d7389a
cyanChill <83375816+cyanChill@users.noreply.github.com>
2024-11-13 00:23:20 -0500

    chore: Move all keys into single file
    - Makes accessing the keys easier for invalidations
    - Supposedly in v2 of `query-key-factory` we should be able to use more options from `useQuery` such as `staleTime` & `gcTime`

--- mobile/src/queries/favorite.ts
+++ mobile/src/queries/favorite.ts
@@ -1,45 +1,18 @@
-import { createQueryKeys } from "@lukemorales/query-key-factory";
 import { useQuery } from "@tanstack/react-query";
-import { eq } from "drizzle-orm";
 import { useTranslation } from "react-i18next";
 
-import { albums, playlists } from "@/db/schema";
 import { formatForCurrentScreen, formatForMediaCard } from "@/db/utils";
 
-import { getAlbums } from "@/api/new/album";
-import { getPlaylists, getSpecialPlaylist } from "@/api/new/playlist";
+import { queries } from "./keyStore";
 
 import { ReservedPlaylists } from "@/modules/media/constants";
-
-/** Get favorited albums & playlists. */
-async function getFavoriteLists() {
-  const [favAlbums, favPlaylists] = await Promise.all([
-    getAlbums([eq(albums.isFavorite, true)]),
-    getPlaylists([eq(playlists.isFavorite, true)]),
-  ]);
-  return { albums: favAlbums, playlists: favPlaylists };
-}
-
-//#region Query Keys
-/** Query keys used in `useQuery` for favorite media. */
-export const favoriteKeys = createQueryKeys("favorites", {
-  lists: {
-    queryKey: null,
-    queryFn: () => getFavoriteLists(),
-  },
-  tracks: {
-    queryKey: [ReservedPlaylists.favorites],
-    queryFn: () => getSpecialPlaylist(ReservedPlaylists.favorites),
-  },
-});
-//#endregion
 
 //#region Queries
 /** Return list of `MediaCard.Content` of favorited albums & playlists. */
 export function useFavoriteListsForCards() {
   const { t } = useTranslation();
   return useQuery({
-    ...favoriteKeys.lists,
+    ...queries.favorites.lists,
     select: (data) =>
       [
         ...data.albums.map((album) =>
@@ -55,7 +28,7 @@ export function useFavoriteListsForCards() {
 /** Get the number of favorited tracks. */
 export function useFavoriteTracksCount() {
   return useQuery({
-    ...favoriteKeys.tracks,
+    ...queries.favorites.tracks,
     select: (data) => data.tracks.length,
   });
 }
@@ -64,7 +37,7 @@ export function useFavoriteTracksCount() {
 export function useFavoriteTracksForCurrentPage() {
   const { t } = useTranslation();
   return useQuery({
-    ...favoriteKeys.tracks,
+    ...queries.favorites.tracks,
     select: (data) => ({
       ...formatForCurrentScreen({ type: "playlist", data, t }),
       imageSource: ReservedPlaylists.favorites,
---
+++ mobile/src/queries/keyStore.ts
@@ -0,0 +1,99 @@
+import { createQueryKeyStore } from "@lukemorales/query-key-factory";
+import { eq } from "drizzle-orm";
+
+import { albums, playlists } from "@/db/schema";
+
+import { getAlbum, getAlbums } from "@/api/new/album";
+import { getArtist, getArtistAlbums, getArtists } from "@/api/new/artist";
+import { getFolder } from "@/api/new/folder";
+import {
+  getPlaylist,
+  getPlaylists,
+  getSpecialPlaylist,
+} from "@/api/new/playlist";
+import { getTrack, getTrackPlaylists, getTracks } from "@/api/new/track";
+
+import { ReservedPlaylists } from "@/modules/media/constants";
+
+/** All of the reusuable query keys. */
+export const queries = createQueryKeyStore({
+  /** Query keys used in `useQuery` for albums. */
+  albums: {
+    all: {
+      queryKey: null,
+      queryFn: () => getAlbums(),
+    },
+    detail: (albumId: string) => ({
+      queryKey: [albumId],
+      queryFn: () => getAlbum(albumId),
+    }),
+  },
+  /** Query keys used in `useQuery` for artists. */
+  artists: {
+    all: {
+      queryKey: null,
+      queryFn: () => getArtists(),
+    },
+    detail: (artistName: string) => ({
+      queryKey: [artistName],
+      queryFn: async () => ({
+        ...(await getArtist(artistName)),
+        albums: await getArtistAlbums(artistName),
+      }),
+    }),
+  },
+  /** Query keys used in `useQuery` for favorite media. */
+  favorites: {
+    lists: {
+      queryKey: null,
+      queryFn: () => getFavoriteLists(),
+    },
+    tracks: {
+      queryKey: [ReservedPlaylists.favorites],
+      queryFn: () => getSpecialPlaylist(ReservedPlaylists.favorites),
+    },
+  },
+  /** Query keys used in `useQuery` for folders. */
+  folders: {
+    detail: (folderPath: string) => ({
+      queryKey: [folderPath],
+      queryFn: () => getFolder(folderPath),
+    }),
+  },
+  /** Query keys used in `useQuery` for playlists. */
+  playlists: {
+    all: {
+      queryKey: null,
+      queryFn: () => getPlaylists(),
+    },
+    detail: (playlistName: string) => ({
+      queryKey: [playlistName],
+      queryFn: () => getPlaylist(playlistName),
+    }),
+  },
+  /** Query keys used in `useQuery` for tracks. */
+  tracks: {
+    all: {
+      queryKey: null,
+      queryFn: () => getTracks(),
+    },
+    detail: (trackId: string) => ({
+      queryKey: [trackId],
+      queryFn: () => getTrack(trackId),
+      contextQueries: {
+        playlists: {
+          queryFn: () => getTrackPlaylists(trackId),
+        },
+      },
+    }),
+  },
+});
+
+/** Get favorited albums & playlists. */
+async function getFavoriteLists() {
+  const [favAlbums, favPlaylists] = await Promise.all([
+    getAlbums([eq(albums.isFavorite, true)]),
+    getPlaylists([eq(playlists.isFavorite, true)]),
+  ]);
+  return { albums: favAlbums, playlists: favPlaylists };
+}
