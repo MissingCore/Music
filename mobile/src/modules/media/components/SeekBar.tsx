3eba01f867f397b59b2b6c95f6d45452a1b2a040
cyanChill <83375816+cyanChill@users.noreply.github.com>
2024-11-21 16:17:46 -0500

    chore: Simplify `<SeekBar />`
    - We added a bunch of logic that probably won't be used often (ie: preserving scrub percentage position if the current track changes)
    - Spotify's implementation of the scrubber is: the scrubbed duration doesn't change when tracks change, so when we let go and that position is greater than the current track duration, then we play the next track
    - We removed the rubberbanding from normal behavior by having a 250ms `setTimeout` when we set `setSlidingTrackPos(null)` and moved it after running `MusicControl.seekTo()`
      - There's some rubberbanding when we spam the seekbar a lot due to using a timer; but this also occurs on the Spotify seekbar as well

--- mobile/src/modules/media/components/SeekBar.tsx
+++ mobile/src/modules/media/components/SeekBar.tsx
@@ -1,11 +1,7 @@
 import { Slider } from "@miblanchard/react-native-slider";
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 import { View } from "react-native";
-import {
-  State,
-  usePlaybackState,
-  useProgress,
-} from "react-native-track-player";
+import { useProgress } from "react-native-track-player";
 
 import { useTheme } from "@/hooks/useTheme";
 import { MusicControls } from "../services/Playback";
@@ -20,50 +16,22 @@ import { StyledText } from "@/components/new/Typography";
  */
 export function SeekBar({ duration }: { duration: number }) {
   const { onSurface } = useTheme();
-  const { state: playbackState } = usePlaybackState();
   const { position } = useProgress(200);
-  const prevDuration = useRef(0);
-  const [isLifted, setIsLifted] = useState(true);
   const [slidingTrackPos, setSlidingTrackPos] = useState<number | null>(null);
-
-  useEffect(() => {
-    // To prevent "rubberbanding" as `useProgress()` takes an "extra frame"
-    // before it displayed the seeked position (±1 second in case `position`
-    // isn't exactly the same).
-    if (slidingTrackPos !== null) {
-      if (
-        position === slidingTrackPos ||
-        (position >= slidingTrackPos - 1 && position <= slidingTrackPos + 1)
-      )
-        setSlidingTrackPos(null);
-    }
-  }, [position, slidingTrackPos]);
-
-  useEffect(() => {
-    // Prevent "rubberbanding" when we seeked to the end of the seekbar.
-    if (isLifted && playbackState === State.Loading) setSlidingTrackPos(null);
-    // Preserve scrub percentage position.
-    if (duration !== prevDuration.current) {
-      if (!isLifted && slidingTrackPos !== null) {
-        setSlidingTrackPos(duration * (slidingTrackPos / prevDuration.current));
-      }
-      prevDuration.current = duration;
-    }
-  }, [playbackState, duration, isLifted, slidingTrackPos]);
 
   const displayedPos = slidingTrackPos ?? position;
   const clampedPos = displayedPos > duration ? duration : displayedPos;
 
   return (
-    <View className="w-full p-4">
+    <View className="w-full">
       <Slider
         value={clampedPos}
         minimumValue={0}
         maximumValue={duration}
-        onSlidingStart={() => setIsLifted(false)}
         onSlidingComplete={async ([newPos]) => {
-          setIsLifted(true);
           await MusicControls.seekTo(newPos!);
+          // Helps prevents "rubberbanding".
+          setTimeout(() => setSlidingTrackPos(null), 250);
         }}
         onValueChange={([newPos]) => setSlidingTrackPos(newPos!)}
         minimumTrackTintColor={Colors.red}
