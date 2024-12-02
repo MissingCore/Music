46df24a0e653b905c63f47397ce2da1061610ee5
cyanChill <83375816+cyanChill@users.noreply.github.com>
2024-12-02 16:20:09 -0500

    chore: Rename `PlaybackSevice.ts`
    - Mainly because we have a `Playback.ts` service for the media module

--- mobile/index.ts
+++ mobile/index.ts
@@ -4,7 +4,7 @@ import "intl-pluralrules";
 import "expo-router/entry";
 import TrackPlayer from "react-native-track-player";
 
-import { PlaybackService, setupPlayer } from "./src/services/PlaybackService";
+import { PlaybackService, setupPlayer } from "./src/services/RNTPService";
 
 /*
   Create custom entry point to potentially fix issues with
