4e644dc89a1b25b291de29a06d9ed103972a829c
cyanChill <83375816+cyanChill@users.noreply.github.com>
2024-12-13 23:36:24 -0500

    chore: Updated `/playlist/[id]` screen

--- mobile/src/app/(main)/(current)/playlist/[id].tsx
+++ mobile/src/app/(main)/(current)/playlist/[id].tsx
@@ -1,96 +1,94 @@
+import { FlashList } from "@shopify/flash-list";
 import { Stack, useLocalSearchParams } from "expo-router";
-import { useMemo } from "react";
+import { useTranslation } from "react-i18next";
 import { View } from "react-native";
 
-import { MoreVert } from "@/icons";
-import { useFavoriteTracksForScreen } from "@/queries/favorite";
-import { usePlaylistForScreen } from "@/queries/playlist";
+import { Edit, Favorite } from "@/icons";
+import { useFavoritePlaylist, usePlaylistForScreen } from "@/queries/playlist";
+import { useBottomActionsContext } from "@/hooks/useBottomActionsContext";
 import { CurrentListLayout } from "@/layouts/CurrentList";
 
+import { mutateGuard } from "@/lib/react-query";
+import { cn } from "@/lib/style";
 import { IconButton } from "@/components/Form";
 import { StyledText } from "@/components/Typography";
-import { ReservedPlaylists } from "@/modules/media/constants";
-import { TrackList } from "@/modules/media/components";
-import type { MediaList } from "@/modules/media/types";
+import { Track } from "@/modules/media/components";
 
 /** Screen for `/playlist/[id]` route. */
 export default function CurrentPlaylistScreen() {
-  const { id: _id } = useLocalSearchParams<{ id: string }>();
-  const id = _id!;
-
-  const isFavoriteTracks = useMemo(
-    () => id === ReservedPlaylists.favorites,
-    [id],
-  );
-
-  if (isFavoriteTracks) {
-    return <PlaylistListContent queryHook={useFavoriteTracksForScreen} />;
-  }
-  return (
-    <>
-      <Stack.Screen
-        options={{
-          headerRight: () => (
-            <IconButton
-              kind="ripple"
-              accessibilityLabel="View playlist settings."
-              onPress={() => console.log("Configuring playlist...")}
-            >
-              <MoreVert />
-            </IconButton>
-          ),
-        }}
-      />
-      <PlaylistListContent
-        id={id}
-        queryHook={usePlaylistForScreen}
-        origin="playlist"
-      />
-    </>
-  );
-}
-
-type PlaylistContent = {
-  origin?: MediaList;
-} & (
-  | { id: string; queryHook: typeof usePlaylistForScreen }
-  | { id?: never; queryHook: typeof useFavoriteTracksForScreen }
-);
-
-/** Basic structure of what we want to render on page. */
-function PlaylistListContent({ id, queryHook, origin }: PlaylistContent) {
-  const { isPending, error, data } = queryHook(id ?? "");
+  const { t } = useTranslation();
+  const { bottomInset } = useBottomActionsContext();
+  const { id } = useLocalSearchParams<{ id: string }>();
+  const { isPending, error, data } = usePlaylistForScreen(id);
+  const favoritePlaylist = useFavoritePlaylist(id);
 
   if (isPending) return <View className="w-full flex-1 px-4" />;
   else if (error) {
     return (
-      <View className="w-full flex-1 px-4">
-        <StyledText preset="dimOnCanvas" className="text-base">
-          Error: Playlist not found
-        </StyledText>
+      <View className="w-full flex-1 p-4">
+        <StyledText center>{t("response.noContent")}</StyledText>
       </View>
     );
   }
+
+  // Add optimistic UI updates.
+  const isToggled = favoritePlaylist.isPending
+    ? !data.isFavorite
+    : data.isFavorite;
 
   // Information about this track list.
   const trackSource = { type: "playlist", id: data.name } as const;
 
   return (
-    <CurrentListLayout
-      title={data.name}
-      metadata={data.metadata}
-      imageSource={data.imageSource}
-      mediaSource={trackSource}
-    >
-      <TrackList
-        data={data.tracks}
-        trackSource={trackSource}
-        // ListEmptyComponent={
-        //   <Description>
-        //     {id ? "No tracks in playlist." : "No favorited tracks."}
-        //   </Description>
-        // }
+    <>
+      <Stack.Screen
+        options={{
+          headerRight: () => (
+            <View className="flex-row gap-1">
+              <IconButton
+                kind="ripple"
+                accessibilityLabel={t(
+                  `common.${isToggled ? "unF" : "f"}avorite`,
+                )}
+                onPress={() => mutateGuard(favoritePlaylist, !data.isFavorite)}
+              >
+                <Favorite filled={isToggled} />
+              </IconButton>
+              <IconButton
+                kind="ripple"
+                accessibilityLabel={t("playlist.edit")}
+                onPress={() => console.log("Configuring playlist...")}
+              >
+                <Edit />
+              </IconButton>
+            </View>
+          ),
+        }}
       />
-    </CurrentListLayout>
+      <CurrentListLayout
+        title={data.name}
+        metadata={data.metadata}
+        imageSource={data.imageSource}
+        mediaSource={trackSource}
+      >
+        <FlashList
+          estimatedItemSize={56} // 48px Height + 8px Margin Top
+          data={data.tracks}
+          keyExtractor={({ id }) => id}
+          renderItem={({ item, index }) => (
+            <View className={cn("mx-4", { "mt-2": index > 0 })}>
+              <Track {...item} trackSource={trackSource} />
+            </View>
+          )}
+          overScrollMode="never"
+          showsVerticalScrollIndicator={false}
+          contentContainerClassName="pt-4"
+          contentContainerStyle={{ paddingBottom: bottomInset.onlyPlayer + 16 }}
+          ListEmptyComponent={
+            <StyledText center>{t("response.noTracks")}</StyledText>
+          }
+        />
+      </CurrentListLayout>
+    </>
   );
 }
