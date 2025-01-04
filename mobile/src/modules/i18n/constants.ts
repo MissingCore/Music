82ca0da67183177289e07c92df689778530c9c06
cyanChill <83375816+cyanChill@users.noreply.github.com>
2025-01-04 18:27:39 -0500

    chore: Enabled French translation option

--- mobile/src/modules/i18n/constants.ts
+++ mobile/src/modules/i18n/constants.ts
@@ -3,6 +3,7 @@ export const LANGUAGES = [
   { code: "de", name: "Deutsch" },
   { code: "en", name: "English" },
   { code: "es", name: "Español" },
+  { code: "fr", name: "Français" },
   { code: "id", name: "Indonesia" },
   { code: "ja", name: "日本語" },
 ] as const;
