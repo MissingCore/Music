MissingCore/Music

commit c6f6293049389ab3fcd337b243b510ecbe106eb1

chore: Fix some flakey code
- Removed unneeded `eslint-disable-next-line`.
- Replace use of `!` assertion with `?`.

mobile/src/modules/search/hooks/useSearch.ts
@@ -21,14 +21,15 @@ export function useSearch<TScope extends SearchCategories>(
     return Object.fromEntries(
       scope.map((mediaType) => [
         mediaType,
-        data[mediaType].filter(
-          (i) =>
+        data[mediaType].filter((i) => {
+          return (
             // Partial match with the `name` field.
             i.name.toLocaleLowerCase().includes(q) ||
             // Track's album starts with the query.
             // @ts-expect-error - We ensured the `album` field is present.
-            (i.album && i.album.name.toLocaleLowerCase().startsWith(q)),
-        ),
+            (i.album !== null && i.album.name.toLocaleLowerCase().startsWith(q))
+          );
+        }),
       ]),
     ) as Pick<SearchResults, TScope[number]>;
   }, [data, query, scope]);

mobile/src/api/artist.ts
@@ -62,10 +62,8 @@ export async function createArtist(entry: typeof artists.$inferInsert) {
 /** Update specified artist. */
 export async function updateArtist(
   id: string,
-  values: Partial<typeof artists.$inferInsert>,
+  values: Partial<Omit<typeof artists.$inferInsert, "name">>,
 ) {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { name: _, ...rest } = values;
-  return db.update(artists).set(rest).where(eq(artists.name, id));
+  return db.update(artists).set(values).where(eq(artists.name, id));
 }
 //#endregion

mobile/src/api/track.ts
@@ -33,8 +33,8 @@ export async function getTrack<
   });
   if (!track) throw new Error(i18next.t("response.noTracks"));
   const hasArtwork =
-    // @ts-expect-error - `options.columns` is defined.
-    options?.columns === undefined || options?.columns.includes("artwork");
+    options?.columns === undefined ||
+    options?.columns.includes("artwork" as TCols);
   return {
     ...track,
     ...(hasArtwork ? { artwork: getTrackCover(track) } : {}),
@@ -68,8 +68,8 @@ export async function getTracks<
     orderBy: (fields) => iAsc(fields.name),
   });
   const hasArtwork =
-    // @ts-expect-error - `options.columns` is defined.
-    options?.columns === undefined || options?.columns.includes("artwork");
+    options?.columns === undefined ||
+    options?.columns.includes("artwork" as TCols);
   return allTracks.map((t) => ({
     ...t,
     ...(hasArtwork ? { artwork: getTrackCover(t) } : {}),

mobile/src/app/setting/index.tsx
@@ -17,7 +17,7 @@ export default function SettingScreen() {
   const { t, i18n } = useTranslation();
   const { hasNewUpdate } = useHasNewUpdate();
 
-  const currLang = LANGUAGES.find(({ code }) => code === i18n.language)!.name;
+  const currLang = LANGUAGES.find(({ code }) => code === i18n.language)?.name;
 
   return (
     <StandardScrollLayout>
@@ -40,7 +40,7 @@ export default function SettingScreen() {
         />
         <ListItem
           titleKey="title.language"
-          description={currLang}
+          description={currLang ?? "English"}
           onPress={() => SheetManager.show("LanguageSheet")}
           last
         />

mobile/src/db/utils.ts
@@ -97,7 +97,7 @@ export function formatForTrack(
   if (type === "artist") description = album?.name ?? "—";
   else if (type === "album") {
     description = formatSeconds(duration);
-    if (artistName && album!.artistName !== artistName) {
+    if (artistName && album?.artistName !== artistName) {
       description += ` • ${artistName}`;
     }
   }

mobile/src/modules/scanning/helpers/_album-fracturization.ts
@@ -29,7 +29,7 @@ export async function fixAlbumFracturization() {
       doesAlbumCopyExists(al, dupAl.key),
     );
     if (existsIdx !== -1) {
-      duplicateCombos[existsIdx]!.ids.push(al.id);
+      duplicateCombos[existsIdx]?.ids.push(al.id);
       return;
     }
     // Don't need to check last index for duplicate.

mobile/src/modules/scanning/helpers/artwork.ts
@@ -47,7 +47,7 @@ export async function findAndSaveArtwork() {
   uncheckedTracks.forEach((t) => {
     const key = t.albumId;
     if (key === null) singles.push(t);
-    else if (Object.hasOwn(albumTracks, key)) albumTracks[key]!.push(t);
+    else if (Object.hasOwn(albumTracks, key)) albumTracks[key]?.push(t);
     else albumTracks[key] = [t];
   });
 

mobile/src/queries/artist.ts
@@ -43,7 +43,7 @@ export function useArtistsForIndex() {
           ? artist.name.charAt(0).toLocaleUpperCase()
           : "#";
         if (Object.hasOwn(groupedArtists, key)) {
-          groupedArtists[key]!.push(artist);
+          groupedArtists[key]?.push(artist);
         } else groupedArtists[key] = [artist];
       });
 

commit 1104f46d111fcaac29adf7ae2abe8f5049fc74e9

fix: Bug with search caused by c6f6293
- To ensure that we return a boolean, we used the `i.album !== null` check, but it's possible that `i.album` is `undefined`, which would cause the following check to throw an error.

mobile/src/modules/search/hooks/useSearch.ts
@@ -27,7 +27,7 @@ export function useSearch<TScope extends SearchCategories>(
             i.name.toLocaleLowerCase().includes(q) ||
             // Track's album starts with the query.
             // @ts-expect-error - We ensured the `album` field is present.
-            (i.album !== null && i.album.name.toLocaleLowerCase().startsWith(q))
+            (!!i.album && i.album.name.toLocaleLowerCase().startsWith(q))
           );
         }),
       ]),