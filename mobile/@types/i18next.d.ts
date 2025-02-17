f774d39d827e9496e960afdf8c59382588c4b303
cyanChill <83375816+cyanChill@users.noreply.github.com>
2025-02-16 23:43:46 -0500

    chore: Update note on why interpolation keys aren't type-safe

--- mobile/@types/i18next.d.ts
+++ mobile/@types/i18next.d.ts
@@ -3,9 +3,8 @@ import type { resources } from "../src/modules/i18n/translations/resources";
 declare module "i18next" {
   interface CustomTypeOptions {
     /*
-      Note that interpolation keys inside the translation aren't
-      type-safe (except the built-in `count`). This is due to the
-      contents of the JSON file not being imported "as const".
+      Note that interpolation keys can't be inferred from JSON file (yet).
+        - See: https://www.i18next.com/overview/typescript#not-working-interpolation-values
     */
     resources: (typeof resources)["en"];
   }
