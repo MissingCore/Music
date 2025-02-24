fbfc2b88a53fd3b2eb86778113f8cb7e923cf36e
cyanChill <83375816+cyanChill@users.noreply.github.com>
2025-02-24 15:08:33 -0500

    chore: Slight fix to sheet title marquee behavior

--- mobile/src/components/Sheet.tsx
+++ mobile/src/components/Sheet.tsx
@@ -74,8 +74,8 @@ export function Sheet({
 /** Header component to be used in `<Sheet />`. */
 function SheetHeader({ title }: { title?: string }) {
   return (
-    <View className="gap-2 pb-6">
-      <View className="mx-auto my-[10px] h-1 w-1/12 min-w-6 rounded-full bg-onSurface" />
+    <View className={cn("gap-2 px-4 pb-2", { "pb-6": !!title })}>
+      <View className="mx-auto my-[10px] h-1 w-8 rounded-full bg-onSurface" />
       {title ? (
         <Marquee center>
           <StyledText className="text-lg">{title}</StyledText>
