683807b72b86ed8776dfaebb1d12f264e97b41ac
cyanChill <83375816+cyanChill@users.noreply.github.com>
2025-03-05 22:36:32 -0500

    chore: Link to screen by clicking on "Playing From"
    - Like with formatting content for `<MediaCard />`, folders aren't supported (yet).

--- mobile/src/screens/NowPlaying/TopAppBar.tsx
+++ mobile/src/screens/NowPlaying/TopAppBar.tsx
@@ -1,6 +1,8 @@
+import type { Href } from "expo-router";
+import { router } from "expo-router";
 import { useMemo } from "react";
 import { useTranslation } from "react-i18next";
-import { View } from "react-native";
+import { Pressable, View } from "react-native";
 
 import { useUserPreferencesStore } from "~/services/UserPreferences";
 import { useMusicStore } from "~/modules/media/services/Music";
@@ -9,6 +11,7 @@ import { useTheme } from "~/hooks/useTheme";
 import { Marquee } from "~/components/Containment/Marquee";
 import { SafeContainer } from "~/components/Containment/SafeContainer";
 import { StyledText } from "~/components/Typography/StyledText";
+import { ReservedPlaylists } from "~/modules/media/constants";
 
 /**
  * Header bar design for "Now Playing" screen which automatically displays
@@ -17,6 +20,7 @@ import { StyledText } from "~/components/Typography/StyledText";
 export function NowPlayingTopAppBar() {
   const { t } = useTranslation();
   const { canvas } = useTheme();
+  const playingSource = useMusicStore((state) => state.playingSource);
   const listName = useMusicStore((state) => state.sourceName);
   const usedDesign = useUserPreferencesStore((state) => state.nowPlayingDesign);
 
@@ -25,12 +29,23 @@ export function NowPlayingTopAppBar() {
     [canvas, usedDesign],
   );
 
+  const listHref = useMemo(() => {
+    if (!playingSource || playingSource.type === "folder") return undefined;
+    const { type, id } = playingSource;
+    if (type === "playlist" && id === ReservedPlaylists.tracks) return "/track";
+    return `/${type}/${encodeURIComponent(id)}` satisfies Href;
+  }, [playingSource]);
+
   return (
     <SafeContainer className="relative">
       <View className="h-14 flex-row items-center justify-between gap-4 p-1">
         <View className="size-12" />
 
-        <View className="shrink gap-0.5 rounded bg-canvas/75 px-4 py-2 dark:bg-canvas/60">
+        <Pressable
+          onPress={() => (listHref ? router.navigate(listHref) : undefined)}
+          disabled={listHref === undefined}
+          className="shrink gap-0.5 rounded bg-canvas/75 px-4 py-2 dark:bg-canvas/60"
+        >
           <Marquee color={shadowColor} center wrapperClassName="shrink">
             <StyledText className="text-xxs/[1.125]" dim>
               {t("term.playingFrom")}
@@ -39,7 +54,7 @@ export function NowPlayingTopAppBar() {
           <Marquee color={shadowColor} center wrapperClassName="shrink">
             <StyledText className="text-xs/[1.125]">{listName}</StyledText>
           </Marquee>
-        </View>
+        </Pressable>
 
         <View className="size-12" />
       </View>
