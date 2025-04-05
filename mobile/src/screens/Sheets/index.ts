fc48c9da74e6a8256d96d91c4c11b6c9a27282fe
cyanChill <83375816+cyanChill@users.noreply.github.com>
2025-04-05 15:41:52 -0400

    chore: Convert artwork sheets to use True Sheet

--- mobile/src/screens/Sheets/index.ts
+++ mobile/src/screens/Sheets/index.ts
@@ -1,11 +1,6 @@
 import type { SheetDefinition } from "react-native-actions-sheet";
 import { registerSheet } from "react-native-actions-sheet";
 
-import {
-  AlbumArtworkSheet,
-  ArtistArtworkSheet,
-  PlaylistArtworkSheet,
-} from "./Artwork";
 import TrackSheet from "./Track";
 import TrackToPlaylistSheet from "./TrackToPlaylist";
 
@@ -14,9 +9,6 @@ import TrackToPlaylistSheet from "./TrackToPlaylist";
   return `null` due waiting for data (ie: React Query), when the data
   appears, the sheet won't render as it expects a sheet on initial render.
 */
-registerSheet("AlbumArtworkSheet", AlbumArtworkSheet);
-registerSheet("ArtistArtworkSheet", ArtistArtworkSheet);
-registerSheet("PlaylistArtworkSheet", PlaylistArtworkSheet);
 registerSheet("TrackSheet", TrackSheet);
 registerSheet("TrackToPlaylistSheet", TrackToPlaylistSheet);
 
@@ -24,9 +16,6 @@ registerSheet("TrackToPlaylistSheet", TrackToPlaylistSheet);
 // across the app for all registered sheets.
 declare module "react-native-actions-sheet" {
   interface Sheets {
-    AlbumArtworkSheet: SheetDefinition<{ payload: { id: string } }>;
-    ArtistArtworkSheet: SheetDefinition<{ payload: { id: string } }>;
-    PlaylistArtworkSheet: SheetDefinition<{ payload: { id: string } }>;
     TrackSheet: SheetDefinition<{ payload: { id: string } }>;
     TrackToPlaylistSheet: SheetDefinition<{ payload: { id: string } }>;
   }
