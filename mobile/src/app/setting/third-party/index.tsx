a968d295c7c5dae31da22ac6c8a4f9c38c9f025c
cyanChill <83375816+cyanChill@users.noreply.github.com>
2025-05-02 20:01:29 -0400

    chore: Use a hook instead of a component for rendering `<ListItem />` from data

--- mobile/src/app/setting/third-party/index.tsx
+++ mobile/src/app/setting/third-party/index.tsx
@@ -1,26 +1,28 @@
 import { router } from "expo-router";
 
 import LicensesList from "~/resources/licenses.json";
-import { StandardScrollLayout } from "~/layouts/StandardScroll";
 
-import { ListRenderer } from "~/components/Containment/List";
+import { useListPresets } from "~/components/Containment/List";
+import { FlashList } from "~/components/Defaults";
 
 /** Screen for `/setting/third-party` route. */
 export default function ThirdPartyScreen() {
+  const presets = useListPresets({
+    data: Object.entries(LicensesList),
+    renderOptions: {
+      getTitle: ([_, item]) => item.name,
+      getDescription: ([_, item]) => `${item.license} (${item.version})`,
+      onPress:
+        ([id]) =>
+        () =>
+          router.navigate(`/setting/third-party/${encodeURIComponent(id)}`),
+    },
+  });
   return (
-    <StandardScrollLayout>
-      <ListRenderer
-        data={Object.entries(LicensesList)}
-        keyExtractor={([id]) => id}
-        renderOptions={{
-          getTitle: ([_, item]) => item.name,
-          getDescription: ([_, item]) => `${item.license} (${item.version})`,
-          onPress:
-            ([id]) =>
-            () =>
-              router.navigate(`/setting/third-party/${encodeURIComponent(id)}`),
-        }}
-      />
-    </StandardScrollLayout>
+    <FlashList
+      keyExtractor={([id]) => id}
+      contentContainerClassName="p-4"
+      {...presets}
+    />
   );
 }
