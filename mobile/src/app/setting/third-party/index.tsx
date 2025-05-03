7aecdba49c76c92ef8632b4ec542752a372507f8
cyanChill <83375816+cyanChill@users.noreply.github.com>
2025-05-02 22:02:03 -0400

    fix: Temporary style workaround as horizontal padding doesn't seem to be working the way we expect it

--- mobile/src/app/setting/third-party/index.tsx
+++ mobile/src/app/setting/third-party/index.tsx
@@ -21,6 +21,7 @@ export default function ThirdPartyScreen() {
   return (
     <FlashList
       keyExtractor={([id]) => id}
+      className="mx-4"
       contentContainerClassName="p-4"
       {...presets}
     />
