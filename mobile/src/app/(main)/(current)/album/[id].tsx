1658f79c00bb2fcc1d983933fd52605e3c546425
cyanChill <83375816+cyanChill@users.noreply.github.com>
2025-05-07 19:44:09 -0400

    chore: Improve recycling by using `getItemType` prop

--- mobile/src/app/(main)/(current)/album/[id].tsx
+++ mobile/src/app/(main)/(current)/album/[id].tsx
@@ -9,6 +9,7 @@ import { useBottomActionsContext } from "~/hooks/useBottomActionsContext";
 import { CurrentListLayout } from "~/layouts/CurrentList";
 
 import { mutateGuard } from "~/lib/react-query";
+import { isNumber } from "~/utils/validation";
 import { FlashList } from "~/components/Defaults";
 import { IconButton } from "~/components/Form/Button";
 import { PagePlaceholder } from "~/components/Transition/Placeholder";
@@ -74,11 +75,10 @@ export default function CurrentAlbumScreen() {
         <FlashList
           estimatedItemSize={56} // 48px Height + 8px Margin Top
           data={formattedData}
-          keyExtractor={(item) =>
-            typeof item === "number" ? `${item}` : item.id
-          }
+          keyExtractor={(item) => (isNumber(item) ? `${item}` : item.id)}
+          getItemType={(item) => (isNumber(item) ? "label" : "row")}
           renderItem={({ item, index }) =>
-            typeof item === "number" ? (
+            isNumber(item) ? (
               <Em dim className={index > 0 ? "mt-4" : undefined}>
                 {t("term.disc", { count: item })}
               </Em>
