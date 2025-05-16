e47b8c74b032904222ac7a9fe62b0367cf11ad5f
cyanChill <83375816+cyanChill@users.noreply.github.com>
2025-05-16 16:09:53 -0400

    chore: Reduce defer render delay to `1ms`
    - Technically, that's enough as we want to delay it enough so that it doesn't slow anything else down.
    - This also solves the off-chance that someone being really quick clicking the button to present a sheet which hasn't been rendered in the `500ms` yet.

--- mobile/src/lib/react.tsx
+++ mobile/src/lib/react.tsx
@@ -2,13 +2,13 @@ import { useDelayedReady } from "~/hooks/useDelayedReady";
 
 /**
  * Delay the initial render of a functional component by a set amount of
- * milliseconds (defaults to `500ms`).
+ * milliseconds (defaults to `1ms`).
  *
  * Useful for delaying the render of a component might stall the JS thread.
  */
 export function deferInitialRender<T, U extends React.FunctionComponent<T>>(
   RenderedComponent: U,
-  delayMs = 500,
+  delayMs = 1,
 ) {
   return function DeferredRender(props: React.ComponentProps<U>) {
     const shouldRender = useDelayedReady(delayMs);
@@ -20,14 +20,14 @@ export function deferInitialRender<T, U extends React.FunctionComponent<T>>(
 
 /**
  * Delay the initial render of children by a set amount of milliseconds
- *  (defaults to `500ms`).
+ *  (defaults to `1ms`).
  */
 export function DeferRender(props: {
-  /** Defaults to `500ms`. */
+  /** Defaults to `1ms`. */
   delayMs?: number;
   children: React.ReactNode;
 }) {
-  const shouldRender = useDelayedReady(props.delayMs ?? 500);
+  const shouldRender = useDelayedReady(props.delayMs ?? 1);
   if (!shouldRender) return null;
   return props.children;
 }
