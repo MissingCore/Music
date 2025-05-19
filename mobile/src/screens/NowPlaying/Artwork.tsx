403d22a90d3f482191c3bc88581d3f6a7705d483
cyanChill <83375816+cyanChill@users.noreply.github.com>
2025-05-18 21:30:16 -0400

    fix: Seekbar "lag" after releasing vinyl gesture
    - This can be more easily seen when we load all the `(home)` routes and then spam interact with the vinyl.
    - Also disable press action on vinyl when we're spinning it.
    
    ------
    Cherry-pick 33d29b0

--- mobile/src/screens/NowPlaying/Artwork.tsx
+++ mobile/src/screens/NowPlaying/Artwork.tsx
@@ -72,11 +72,15 @@ function PlainArtwork(props: ArtworkProps) {
 
 /** Seekbar variant that uses the vinyl artwork. */
 function VinylSeekBar(props: ArtworkProps) {
-  const { wrapperRef, initCenter, vinylStyle, seekGesture } = useVinylSeekbar();
+  const { wrapperRef, isActive, initCenter, vinylStyle, seekGesture } =
+    useVinylSeekbar();
   return (
     <GestureDetector gesture={seekGesture}>
       <Animated.View ref={wrapperRef} onLayout={initCenter} style={vinylStyle}>
-        <Vinyl onPress={MusicControls.playToggle} {...props} />
+        <Vinyl
+          onPress={!isActive ? MusicControls.playToggle : undefined}
+          {...props}
+        />
       </Animated.View>
     </GestureDetector>
   );
--- mobile/src/screens/NowPlaying/helpers/useVinylSeekbar.ts
+++ mobile/src/screens/NowPlaying/helpers/useVinylSeekbar.ts
@@ -1,4 +1,4 @@
-import { useCallback, useMemo, useRef } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { Gesture } from "react-native-gesture-handler";
 import type Animated from "react-native-reanimated";
 import {
@@ -23,6 +23,7 @@ export function useVinylSeekbar() {
 
   const wrapperRef = useRef<Animated.View>(null);
   const hasMounted = useRef(false);
+  const [isActive, setIsActive] = useState(false);
   // Coordinates pointing to the center of the vinyl.
   const centerX = useSharedValue(0);
   const centerY = useSharedValue(0);
@@ -32,6 +33,8 @@ export function useVinylSeekbar() {
   // Rotation progress based on "seeking" on vinyl.
   const seekProgress = useSharedValue<number | null>(null);
   const prevAngle = useSharedValue(0);
+  // Angle to debounce `runOnJS`.
+  const debounceAngle = useSharedValue<number | null>(null);
 
   const duration = useMemo(() => activeTrack?.duration ?? 0, [activeTrack]);
   const maxDegrees = useMemo(() => convertUnit(duration), [duration]);
@@ -51,37 +54,40 @@ export function useVinylSeekbar() {
     async (seconds: number) => {
       await TrackPlayer.seekTo(seconds);
       trueProgress.value = convertUnit(seconds);
-      seekProgress.value = null;
+      debounceAngle.value = seekProgress.value = null;
       // Helps prevents "rubberbanding" in seekbar.
       setTimeout(() => setSliderPos(null), 250);
     },
-    [setSliderPos, seekProgress, trueProgress],
+    [debounceAngle, setSliderPos, seekProgress, trueProgress],
   );
 
-  const renderedPosition = sliderPos ?? position;
-  if (renderedPosition === 0) {
-    // Reset animation when position goes back to 0s.
-    cancelAnimation(trueProgress);
-    trueProgress.value = 0;
-  } else if (renderedPosition < duration - 1) {
-    trueProgress.value = withTiming(
-      convertUnit(renderedPosition),
-      // Prevent vinyl rotation on mount.
-      { duration: hasMounted.current ? 500 : 0, easing: Easing.linear },
-    );
-    if (!hasMounted.current) hasMounted.current = true;
-  } else {
-    // Cancel animation ~1s before the end due to weird behaviors if
-    // the following image is large in size (ie: "animation spike").
-    cancelAnimation(trueProgress);
-  }
+  useEffect(() => {
+    const renderedPosition = sliderPos ?? position;
+    if (renderedPosition === 0) {
+      // Reset animation when position goes back to 0s.
+      cancelAnimation(trueProgress);
+      trueProgress.value = 0;
+    } else if (renderedPosition < duration - 1) {
+      trueProgress.value = withTiming(
+        convertUnit(renderedPosition),
+        // Prevent vinyl rotation on mount.
+        { duration: hasMounted.current ? 500 : 0, easing: Easing.linear },
+      );
+      if (!hasMounted.current) hasMounted.current = true;
+    } else {
+      // Cancel animation ~1s before the end due to weird behaviors if
+      // the following image is large in size (ie: "animation spike").
+      cancelAnimation(trueProgress);
+    }
+  }, [duration, position, sliderPos, trueProgress]);
 
   const seekGesture = Gesture.Pan()
     .shouldCancelWhenOutside(true)
     .hitSlop(32)
     .onStart(({ absoluteX, absoluteY }) => {
+      runOnJS(setIsActive)(true);
       runOnJS(setSliderPos)(position);
-      seekProgress.value = convertUnit(position);
+      debounceAngle.value = seekProgress.value = convertUnit(position);
       prevAngle.value = Math.atan2(
         absoluteY - centerY.value,
         absoluteX - centerX.value,
@@ -101,11 +107,16 @@ export function useVinylSeekbar() {
       if (newPosition < 0) seekProgress.value = 0;
       else if (newPosition > maxDegrees) seekProgress.value = maxDegrees;
       else seekProgress.value = newPosition;
-      runOnJS(setSliderPos)(convertUnit(seekProgress.value, "degrees"));
 
+      // Only run `setSliderPos` when we've rotated ~15deg (which is ~1s).
+      if (Math.abs((debounceAngle.value ?? 0) - seekProgress.value) > 15) {
+        runOnJS(setSliderPos)(convertUnit(seekProgress.value, "degrees"));
+        debounceAngle.value = seekProgress.value;
+      }
       prevAngle.value = currAngle;
     })
     .onEnd(() => {
+      runOnJS(setIsActive)(false);
       runOnJS(onEnd)(convertUnit(seekProgress.value ?? 0, "degrees"));
     });
 
@@ -114,7 +125,7 @@ export function useVinylSeekbar() {
     return { transform: [{ rotate: `${rotateAmount}deg` }] };
   });
 
-  return { wrapperRef, initCenter, vinylStyle, seekGesture };
+  return { wrapperRef, isActive, initCenter, vinylStyle, seekGesture };
 }
 
 /**
