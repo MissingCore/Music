927937ee7ee48831c95aa31597b2df2ca1c9de1c
cyanChill <83375816+cyanChill@users.noreply.github.com>
2025-06-01 17:37:52 -0400

    chore: Have seekbar reflect the last saved position on app launch

--- mobile/src/screens/NowPlaying/helpers/usePlayerProgress.ts
+++ mobile/src/screens/NowPlaying/helpers/usePlayerProgress.ts
@@ -1,7 +1,8 @@
 import { atom, useAtomValue, useSetAtom } from "jotai";
-import { useCallback, useRef, useState } from "react";
+import { useCallback, useMemo, useRef, useState } from "react";
 import { useProgress } from "react-native-track-player";
 
+import { useMusicStore } from "~/modules/media/services/Music";
 import { MusicControls } from "~/modules/media/services/Playback";
 
 /** Dragging slider position. */
@@ -11,6 +12,7 @@ const NextSliderPositionAtom = atom<number | null>(null);
 export function usePlayerProgress() {
   const debounceRef = useRef<NodeJS.Timeout>();
   const [updateInterval, setUpdateInterval] = useState(200);
+  const restoredPosition = useRestorePosition();
   const { position } = useProgress(updateInterval);
   const nextSliderPosition = useAtomValue(NextSliderPositionAtom);
   const setNextSliderPosition = useSetAtom(NextSliderPositionAtom);
@@ -31,8 +33,31 @@ export function usePlayerProgress() {
   );
 
   return {
-    position: nextSliderPosition ?? position,
+    position: nextSliderPosition ?? restoredPosition ?? position,
     setPosition: setNextSliderPosition,
     seekToPosition,
   };
+}
+
+/** Have the seekbar visually reflect the last saved position. */
+function useRestorePosition() {
+  const _hasRestoredPosition = useMusicStore(
+    (state) => state._hasRestoredPosition,
+  );
+  const _restoredTrackId = useMusicStore((state) => state._restoredTrackId);
+  const activeId = useMusicStore((state) => state.activeId);
+  const lastPosition = useMusicStore((state) => state.lastPosition);
+
+  return useMemo(() => {
+    if (
+      _hasRestoredPosition ||
+      lastPosition === undefined ||
+      _restoredTrackId === undefined ||
+      _restoredTrackId !== activeId
+    ) {
+      return undefined;
+    }
+    return lastPosition;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [_hasRestoredPosition, activeId]);
 }
