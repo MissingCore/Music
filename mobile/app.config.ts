b93e9ca19d0527226e151ffc789f8933a2b73c5e
cyanChill <83375816+cyanChill@users.noreply.github.com>
2025-06-13 19:41:28 -0400

    chore(deps): Bumped dependencies for RN 0.79
    - Haven't validated functionality (yet).

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -59,6 +59,7 @@ export default (): ExpoConfig => ({
       { android: { enforceNavigationBarContrast: false } },
     ],
   ],
+  newArchEnabled: false,
   experiments: {
     typedRoutes: true,
   },
