f05d47d344465730fd00fff80a3c7f7713bd8e2e
cyanChill <83375816+cyanChill@users.noreply.github.com>
2025-06-14 23:11:19 -0400

    fix: Incorrect toast position in global provider
    - Same issue caused by targeting Android API 35 as internally, `@backpackapp-io/react-native-toast` uses a height obtained from `useWindowDimensions()`.

--- mobile/src/providers/index.tsx
+++ mobile/src/providers/index.tsx
@@ -1,5 +1,7 @@
 import { Toasts } from "@backpackapp-io/react-native-toast";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { platformApiLevel } from "expo-device";
+import { useMemo } from "react";
 import { View } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
@@ -19,13 +21,14 @@ export function AppProvider(props: ChildrenWrapperProps) {
         <QueryClientProvider client={queryClient}>
           <RouteHandlers />
           <ChildrenWrapper {...props} />
-          <Toasts />
+          <ToastProvider />
         </QueryClientProvider>
       </GestureHandlerRootView>
     </ThemeProvider>
   );
 }
 
+//#region Edge-To-Edge
 type ChildrenWrapperProps = {
   children: React.ReactNode;
   systemTheme?: boolean;
@@ -46,3 +49,20 @@ function ChildrenWrapper(props: ChildrenWrapperProps) {
     <View style={{ paddingBottom: bottom }} className="flex-1" {...props} />
   );
 }
+//#endregion
+
+//#region Toast Provider
+function ToastProvider() {
+  const insets = useSafeAreaInsets();
+
+  // Need to provide some extra insets for the global toast provider due
+  // to Android API 35+ changing what we get for "height" from `useWindowDimensions()`.
+  //  - https://github.com/facebook/react-native/issues/47080#issuecomment-2421914957
+  const extraInsets = useMemo(() => {
+    if (!platformApiLevel || platformApiLevel < 35) return undefined;
+    return { bottom: insets.top + insets.bottom };
+  }, [insets]);
+
+  return <Toasts extraInsets={extraInsets} />;
+}
+//#endregion
