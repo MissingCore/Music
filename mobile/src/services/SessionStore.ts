1cc4cc34ac2a7efc765c82d55cb0d7abd46b8466
cyanChill <83375816+cyanChill@users.noreply.github.com>
2025-07-12 16:18:09 -0400

    chore: Sleep timer utility

--- mobile/src/services/SessionStore.ts
+++ mobile/src/services/SessionStore.ts
@@ -1,10 +1,13 @@
+import BackgroundTimer from "@boterop/react-native-background-timer";
 import { TrueSheet } from "@lodev09/react-native-true-sheet";
+import TrackPlayer from "@weights-ai/react-native-track-player";
 import { useStore } from "zustand";
 import { createStore } from "zustand/vanilla";
 
 import type { TrackWithAlbum } from "~/db/schema";
 
 import { getTrack } from "~/api/track";
+import { MusicControls } from "~/modules/media/services/Playback";
 
 interface SessionStore {
   /** The rate at which the media is played (from 0.25 to 2). */
@@ -14,13 +17,40 @@ interface SessionStore {
 
   /** Track displayed in global track sheet. */
   displayedTrack: (TrackWithAlbum & { _checked: number }) | null;
+
+  /** Stores the current sleep timer. */
+  sleepTimer: ReturnType<typeof BackgroundTimer.setTimeout> | null;
+  /** Epoch time where this sleep timer will end. */
+  endAt: number | null;
+  /** Create a sleep timer. */
+  createSleepTimer: (minutes: number) => void;
+  /** Clear the current sleep timer. */
+  clearSleepTimer: () => void;
 }
 
-export const sessionStore = createStore<SessionStore>()(() => ({
+export const sessionStore = createStore<SessionStore>()((set, get) => ({
   playbackSpeed: 1,
   volume: 1,
 
   displayedTrack: null,
+
+  sleepTimer: null,
+  endAt: null,
+  createSleepTimer: (minutes) => {
+    const currTimer = get().sleepTimer;
+    if (currTimer !== null) BackgroundTimer.clearTimeout(currTimer);
+    const durationMS = minutes * 60 * 1000;
+    const newTimer = setTimeout(async () => {
+      await MusicControls.stop();
+      await TrackPlayer.reset();
+    }, durationMS);
+    set({ sleepTimer: newTimer, endAt: Date.now() + durationMS });
+  },
+  clearSleepTimer: () => {
+    const currTimer = get().sleepTimer;
+    if (currTimer !== null) BackgroundTimer.clearTimeout(currTimer);
+    set({ sleepTimer: null, endAt: null });
+  },
 }));
 
 export const useSessionStore = <T>(selector: (state: SessionStore) => T): T =>
