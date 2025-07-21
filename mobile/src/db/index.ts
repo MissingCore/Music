48c7e41fa84aea69e263e923ba21ae102025c818
cyanChill <83375816+cyanChill@users.noreply.github.com>
2025-07-20 23:06:55 -0400

    fix: DB migrations changes not immediately available
    - Had an issue where after going from `main` to `dev`, entries in the Recent List disappeared.
    - The reason seems to be that the fields the queries look for hasn't been added in yet.
    - We now immediately fire the `migrate()` function when we load the `db` file.

--- mobile/src/db/index.ts
+++ mobile/src/db/index.ts
@@ -1,7 +1,14 @@
 import { drizzle } from "drizzle-orm/expo-sqlite";
+import { migrate } from "drizzle-orm/expo-sqlite/migrator";
 import { openDatabaseSync } from "expo-sqlite";
 
 import * as schema from "./schema";
+import migrations from "./drizzle/migrations";
 
 export const expoSQLiteDB = openDatabaseSync("db.db");
 export const db = drizzle(expoSQLiteDB, { schema, casing: "snake_case" });
+
+// Run migration on app start.
+migrate(db, migrations).catch((err) => {
+  console.log("Migration failed:", err);
+});
