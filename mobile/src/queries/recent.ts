17f3d2754d799fa3826a20a7e0bcc85a2cbdd04f
cyanChill <83375816+cyanChill@users.noreply.github.com>
2025-08-02 16:52:58 -0400

    fix: Always display loading spinner when going to "Recently Played" screen from home screen
    - Better UI-wise than seeing an empty space briefly.

--- mobile/src/queries/recent.ts
+++ mobile/src/queries/recent.ts
@@ -5,11 +5,11 @@ import { queries as q } from "./keyStore";
 //#region Queries
 /** Return a list of recently played media lists. */
 export function useRecentlyPlayedMediaLists() {
-  return useQuery({ ...q.recent.mediaLists, staleTime: 0 });
+  return useQuery({ ...q.recent.mediaLists, gcTime: 0, staleTime: 0 });
 }
 
 /** Return a list of recently played tracks. */
 export function useRecentlyPlayedTracks() {
-  return useQuery({ ...q.recent.tracks, staleTime: 0 });
+  return useQuery({ ...q.recent.tracks, gcTime: 0, staleTime: 0 });
 }
 //#endregion
