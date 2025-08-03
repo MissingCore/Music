d90c611cfbbf98157788fa87b8bb6889d160baa1
cyanChill <83375816+cyanChill@users.noreply.github.com>
2025-08-03 19:05:08 -0400

    chore: Revert new sheet gradient header design
    - After using it for a bit, it doesn't look that good.
    - Removed patch that removed the shadow on the top of the sheet.

--- mobile/src/components/Sheet.tsx
+++ mobile/src/components/Sheet.tsx
@@ -2,25 +2,18 @@ import { Toasts } from "@backpackapp-io/react-native-toast";
 import type { TrueSheetProps } from "@lodev09/react-native-true-sheet";
 import { TrueSheet } from "@lodev09/react-native-true-sheet";
 import { platformApiLevel } from "expo-device";
-import { LinearGradient } from "expo-linear-gradient";
 import type { ParseKeys } from "i18next";
 import { cssInterop } from "nativewind";
-import {
-  useCallback,
-  useImperativeHandle,
-  useMemo,
-  useRef,
-  useState,
-} from "react";
+import { useMemo, useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
 import type { PressableProps, StyleProp, ViewStyle } from "react-native";
-import { Easing, Pressable, View, useWindowDimensions } from "react-native";
-import { easeGradient } from "react-native-easing-gradient";
+import { View, useWindowDimensions } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 import { useTheme } from "~/hooks/useTheme";
 
+import { BorderRadius } from "~/constants/Styles";
 import { cn } from "~/lib/style";
 import { Marquee } from "./Containment/Marquee";
 import { Button } from "./Form/Button";
@@ -48,7 +41,6 @@ export function useSheetRef() {
 
 //#region Sheet
 export function Sheet({
-  ref,
   titleKey,
   globalKey,
   snapTop,
@@ -59,10 +51,8 @@ export function Sheet({
   onDismiss,
   ...props
 }: SheetProps & { ref?: TrueSheetRef }) {
-  const internalRef = useSheetRef();
-  useImperativeHandle(ref, () => internalRef.current!, [internalRef]);
-
   const { t } = useTranslation();
+  const { canvasAlt } = useTheme();
   const insets = useSafeAreaInsets();
   const { height: screenHeight } = useWindowDimensions();
   const [enableToast, setEnableToast] = useState(false);
@@ -79,20 +69,15 @@ export function Sheet({
     return screenHeight - insets.top - insets.bottom;
   }, [insets.bottom, insets.top, screenHeight]);
 
-  const closeSheet = useCallback(
-    () => internalRef.current?.dismiss(),
-    [internalRef],
-  );
-
   return (
     <TrueSheet
-      ref={internalRef}
       onLayout={(e) => setSheetHeight(e.nativeEvent.layout.height)}
       name={globalKey}
       sizes={[snapTop ? "large" : "auto"]}
-      backgroundColor="transparent"
-      cornerRadius={0}
-      maxHeight={trueScreenHeight}
+      backgroundColor={canvasAlt}
+      cornerRadius={BorderRadius.lg}
+      // Sheet max height will be just before the `<TopAppBar />`.
+      maxHeight={trueScreenHeight - 56}
       grabber={false}
       onPresent={(e) => {
         if (onPresent) onPresent(e);
@@ -117,28 +102,26 @@ export function Sheet({
       }}
       {...props}
     >
-      <HeaderApplicator
+      <SheetHeader
         title={titleKey ? t(titleKey) : undefined}
         getHeight={setHeaderHeight}
-        onClose={closeSheet}
+      />
+      <WrappedGestureHandlerRootView
+        style={[
+          // TrueSheet doesn't know the actual scrollable area, so we
+          // need to exclude the height taken up by the "SheetHeader"
+          // from the container that can hold a scrollable.
+          [{ maxHeight: trueScreenHeight - 56 - headerHeight }],
+          contentContainerStyle,
+        ]}
+        className={cn(
+          "gap-6 p-4 pt-0",
+          { "h-full pb-0": snapTop },
+          contentContainerClassName,
+        )}
       >
-        <WrappedGestureHandlerRootView
-          style={[
-            // TrueSheet doesn't know the actual scrollable area, so we
-            // need to exclude the height taken up by the "SheetHeader"
-            // from the container that can hold a scrollable.
-            [{ maxHeight: trueScreenHeight - headerHeight }],
-            contentContainerStyle,
-          ]}
-          className={cn(
-            "gap-6 p-4 pt-0",
-            { "h-full pb-0": snapTop },
-            contentContainerClassName,
-          )}
-        >
-          {children}
-        </WrappedGestureHandlerRootView>
-      </HeaderApplicator>
+        {children}
+      </WrappedGestureHandlerRootView>
       {enableToast ? (
         <Toasts
           // @ts-expect-error - We added the `sheetOpts` prop via a patch.
@@ -150,14 +133,6 @@ export function Sheet({
           globalAnimationConfig={disableToastAnim ? { duration: 1 } : undefined}
         />
       ) : null}
-      {/*
-        FIXME: A hack to give a non-transparent background to the area not
-        taken up by our content.
-      */}
-      <View
-        style={{ height: screenHeight }}
-        className="absolute left-0 top-full w-full bg-canvasAlt"
-      />
     </TrueSheet>
   );
 }
@@ -205,65 +180,22 @@ export function SheetButtonGroup(props: {
 //#endregion
 
 //#region Internal
-const GRADIENT_HEIGHT = 96;
-
-function HeaderApplicator(props: {
-  onClose: VoidFunction;
+function SheetHeader(props: {
   getHeight: (height: number) => void;
   title?: string;
-  children: React.ReactNode;
 }) {
-  const { theme, canvasAlt } = useTheme();
-  const { colors, locations } = useMemo(
-    () =>
-      easeGradient({
-        colorStops: {
-          0: { color: `${canvasAlt}00` },
-          ...(theme === "dark"
-            ? {
-                0.25: { color: `${canvasAlt}66` },
-                0.65: { color: `${canvasAlt}D9` },
-                0.8: { color: canvasAlt },
-              }
-            : {}),
-          1: { color: canvasAlt },
-        } as Record<number, { color: string }>,
-        easing: theme === "dark" ? Easing.linear : undefined,
-      }),
-    [theme, canvasAlt],
-  );
   return (
-    <>
-      <Pressable
-        accessible={false}
-        onPress={props.onClose}
-        className="absolute left-0 top-0 z-50 h-14 w-full"
-      />
-      <LinearGradient
-        colors={colors as [string, string, ...string[]]}
-        locations={locations as [number, number, ...number[]]}
-        style={{ height: GRADIENT_HEIGHT }}
-      />
-      {/*
-        FIXME: Weird issue which occurs on some devices, which is a random
-        horizontal line appears under the header depending on the bottom padding.
-      */}
-      <View className="bg-canvasAlt">
-        <View
-          onLayout={(e) =>
-            props.getHeight(e.nativeEvent.layout.height + GRADIENT_HEIGHT)
-          }
-          className={cn("px-4 pb-6", { "pb-2": !props.title })}
-        >
-          {props.title ? (
-            <Marquee center>
-              <StyledText className="text-lg">{props.title}</StyledText>
-            </Marquee>
-          ) : null}
-        </View>
-        {props.children}
-      </View>
-    </>
+    <View
+      onLayout={(e) => props.getHeight(e.nativeEvent.layout.height)}
+      className={cn("gap-2 px-4 pb-2", { "pb-6": !!props.title })}
+    >
+      <View className="mx-auto my-[10px] h-1 w-8 rounded-full bg-onSurface" />
+      {props.title ? (
+        <Marquee center>
+          <StyledText className="text-lg">{props.title}</StyledText>
+        </Marquee>
+      ) : null}
+    </View>
   );
 }
 //#endregion
