MissingCore/Music

commit 02ec4225f4f2c13c8d4156e53af3e6ef4f83e128

fix: Potential fix to `Cannot read property 'y' of undefined`
- This seems to be an issue with FlashList.
- This only seems to occur, based on other reports, when it has a `numColumns` prop that depends on the width of the device and a `ListEmptyComponent`.
- Putting a `key` prop in `useMediaCardListPreset()` and spreading it doesn't work.

mobile/src/components/Defaults.tsx
@@ -75,12 +75,36 @@ const WrappedFlashList = cssInterop(RawFlashList, {
 const RawAnimatedFlashList = Animated.createAnimatedComponent(WrappedFlashList);
 
 export function FlashList<T>(props: FlashListProps<T>) {
-  return <WrappedFlashList {...ScrollablePresets} {...props} />;
+  return (
+    <WrappedFlashList
+      // To prevent `TypeError: Cannot read property 'y' of undefined`
+      // crash from a list with `numColumns` and `ListEmptyComponent`.
+      key={
+        props.data?.length === 0
+          ? `empty-list-with-${props.numColumns}-cols`
+          : `non-empty-list-with-${props.numColumns}-cols`
+      }
+      {...ScrollablePresets}
+      {...props}
+    />
+  );
 }
 
 export function AnimatedFlashList<T>(props: FlashListProps<T>) {
-  // @ts-expect-error - Ref should be compatible.
-  return <RawAnimatedFlashList {...ScrollablePresets} {...props} />;
+  return (
+    // @ts-expect-error - Ref should be compatible.
+    <RawAnimatedFlashList
+      // To prevent `TypeError: Cannot read property 'y' of undefined`
+      // crash from a list with `numColumns` and `ListEmptyComponent`.
+      key={
+        props.data?.length === 0
+          ? `empty-list-with-${props.numColumns}-cols`
+          : `non-empty-list-with-${props.numColumns}-cols`
+      }
+      {...ScrollablePresets}
+      {...props}
+    />
+  );
 }
 
 export function useFlashListRef<T = any>() {

commit 6d0d24e67f2e609d9692bc36404300a61467ed87

fix: Flickering when navigating to a new folder that hasn't been fetched yet
- Caused by 02ec422 due to us always having a key, even when we don't need it (this issue only occurs when we have a dynamic  `numColumns` and a `ListEmptyComponent`).
- Fixed the code so that we only use the key if we have no values and `numColumns` is defined.

mobile/src/components/Defaults.tsx
@@ -80,7 +80,7 @@ export function FlashList<T>(props: FlashListProps<T>) {
       // To prevent `TypeError: Cannot read property 'y' of undefined`
       // crash from a list with `numColumns` and `ListEmptyComponent`.
       key={
-        props.data?.length === 0
+        props.data?.length === 0 && props.numColumns !== undefined
           ? `empty-list-with-${props.numColumns}-cols`
           : `non-empty-list-with-${props.numColumns}-cols`
       }
@@ -97,7 +97,7 @@ export function AnimatedFlashList<T>(props: FlashListProps<T>) {
       // To prevent `TypeError: Cannot read property 'y' of undefined`
       // crash from a list with `numColumns` and `ListEmptyComponent`.
       key={
-        props.data?.length === 0
+        props.data?.length === 0 && props.numColumns !== undefined
           ? `empty-list-with-${props.numColumns}-cols`
           : `non-empty-list-with-${props.numColumns}-cols`
       }