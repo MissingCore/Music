e3327bfacba229a663321242c1d8f3229f930c11
cyanChill <83375816+cyanChill@users.noreply.github.com>
2025-09-14 17:23:21 -0400

    chore: Move routing structure to  static config layout
    - Currently not handling the translated titles yet.
    - Haven't dealt with layouts for the most part yet.

---
+++ mobile/src/navigation/routes.tsx
@@ -0,0 +1,242 @@
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import type {
+  EventArg,
+  ParamListBase,
+  TabNavigationState,
+} from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useCallback, useMemo, useRef } from "react";
+
+import Home from "./screens/HomeView";
+import { ModifyTrack } from "~/screens/ModifyTrack";
+import NowPlaying from "./screens/NowPlayingView";
+import RecentlyPlayed from "./screens/RecentlyPlayedView";
+import Search from "./screens/SearchView";
+import Album from "./screens/albums/CurrentView";
+import Albums from "./screens/albums/View";
+import Artist from "./screens/artists/CurrentView";
+import Artists from "./screens/artists/View";
+import Folders from "./screens/folders/View";
+import CreatePlaylist from "./screens/playlists/CreateView";
+import Playlist from "./screens/playlists/CurrentView";
+import ModifyPlaylist from "./screens/playlists/ModifyView";
+import Playlists from "./screens/playlists/View";
+import AppearanceSettings from "./screens/settings/AppearanceSettingsView";
+import AppUpdate from "./screens/settings/AppUpdateView";
+import ExperimentalSettings from "./screens/settings/ExperimentalSettingsView";
+import HiddenTracks from "./screens/settings/HiddenTracksView";
+import HomeTabOrderSettings from "./screens/settings/HomeTabOrderSettingsView";
+import Insights from "./screens/settings/InsightsView";
+import PackageLicense from "./screens/settings/PackageLicenseView";
+import PlaybackSettings from "./screens/settings/PlaybackSettingsView";
+import SaveErrors from "./screens/settings/SaveErrorsView";
+import ScanningSettings from "./screens/settings/ScanningSettingsView";
+import ThirdParty from "./screens/settings/ThirdPartyView";
+import Settings from "./screens/settings/View";
+import FavoriteTracks from "./screens/tracks/FavoritesView";
+import Tracks from "./screens/tracks/View";
+
+import {
+  useTabsByVisibility,
+  useUserPreferencesStore,
+} from "~/services/UserPreferences";
+import { NowPlayingTopAppBar } from "~/screens/NowPlaying/TopAppBar";
+
+import { capitalize } from "~/utils/string";
+import { TopAppBar } from "~/components/TopAppBar";
+
+//#region Root Screens
+type TabState = EventArg<
+  "state",
+  any,
+  { state: TabNavigationState<ParamListBase> }
+>;
+
+const MaterialTopTab = createMaterialTopTabNavigator();
+
+const noop = () => null;
+
+const RootScreenComponents = {
+  album: Albums,
+  artist: Artists,
+  folder: Folders,
+  playlist: Playlists,
+  track: Tracks,
+} as const;
+
+function RootScreens() {
+  const homeTab = useUserPreferencesStore((s) => s.homeTab);
+  const { displayedTabs, hiddenTabs } = useTabsByVisibility();
+  // Should be fine to store navigation state in ref as it doesn't affect rendering.
+  //  - https://react.dev/learn/referencing-values-with-refs#when-to-use-refs
+  const prevTabState = useRef<TabState>(null);
+
+  const homeTabName = useMemo(
+    () => (homeTab === "home" ? "Home" : `${capitalize(homeTab)}s`),
+    [homeTab],
+  );
+
+  /** Have Tab history operate like Stack history. */
+  const manageAsStackHistory = useCallback(
+    (e: TabState) => {
+      if (prevTabState.current) {
+        // Get top of history.
+        const currRoute = e.data.state.history.at(-1)!;
+        const currIndex = e.data.state.index;
+        // See if route was seen previously.
+        const oldHistory = prevTabState.current.data.state.history;
+        const atIndex = oldHistory.findIndex((r) => currRoute.key === r.key);
+        // Handle if we visited this tab earlier.
+        if (atIndex !== -1) {
+          // FIXME: Modifying the value in `e` for some reason modifies the
+          // original reference (even if we cloned `e` via object spreading).
+          //  - This might be fragile code, so we might swap over to the use
+          //  of the `reset` function.
+          //  - https://reactnavigation.org/docs/navigation-actions/#reset
+          e.data.state.history = oldHistory
+            .toSpliced(currIndex === 0 ? 1 : atIndex + 1)
+            .filter((r) => !hiddenTabs.some((t) => r.key.startsWith(`${t}-`)));
+        }
+      }
+      prevTabState.current = e;
+    },
+    [hiddenTabs],
+  );
+
+  const listeners = useMemo(
+    () => ({ state: manageAsStackHistory }),
+    [manageAsStackHistory],
+  );
+
+  return (
+    <MaterialTopTab.Navigator
+      initialRouteName={homeTabName}
+      backBehavior="history"
+      tabBar={noop}
+      screenListeners={listeners}
+    >
+      <MaterialTopTab.Screen name="Home" component={Home} />
+      {displayedTabs.map((tabKey) => (
+        <MaterialTopTab.Screen
+          key={tabKey}
+          name={`${capitalize(tabKey)}s`}
+          component={RootScreenComponents[tabKey]}
+          options={{ lazy: true }}
+        />
+      ))}
+    </MaterialTopTab.Navigator>
+  );
+}
+//#endregion
+
+//#region Static Route Config
+export const RootStack = createNativeStackNavigator({
+  initialRouteName: "Home",
+  screenOptions: {
+    headerShown: false,
+  },
+  screens: {
+    Home: RootScreens,
+  },
+  groups: {
+    Current: {
+      screenOptions: {
+        animation: "fade",
+        header: TopAppBar,
+      },
+      screens: {
+        FavoriteTracks: { screen: FavoriteTracks, options: { title: "" } },
+        CreatePlaylist: { screen: CreatePlaylist, options: { title: "" } },
+        ModifyPlaylist: { screen: ModifyPlaylist, options: { title: "" } },
+        Playlist: { screen: Playlist, options: { title: "" } },
+        Album: { screen: Album, options: { title: "" } },
+        Artist: { screen: Artist, options: { title: "" } },
+        RecentlyPlayed: {
+          screen: RecentlyPlayed,
+          options: { title: "feat.playedRecent.title" },
+        },
+        ModifyTrack: { screen: ModifyTrack, options: { title: "" } },
+      },
+    },
+    Root: {
+      screenOptions: {
+        animation: "fade",
+        header: TopAppBar,
+      },
+      screens: {
+        NowPlaying: {
+          screen: NowPlaying,
+          options: {
+            animation: "slide_from_bottom",
+            header: NowPlayingTopAppBar,
+            headerTransparent: true,
+            headerShown: true,
+            headerTitle: "",
+          },
+        },
+        Search: {
+          screen: Search,
+          options: {
+            header: TopAppBar,
+            headerShown: true,
+            title: "",
+          },
+        },
+      },
+    },
+    Setting: {
+      screenOptions: {
+        animation: "fade",
+        header: TopAppBar,
+      },
+      screens: {
+        Settings: {
+          screen: Settings,
+          options: { title: "term.settings" },
+        },
+        AppUpdate: {
+          screen: AppUpdate,
+          options: { title: "feat.appUpdate.title" },
+        },
+        AppearanceSettings: {
+          screen: AppearanceSettings,
+          options: { title: "feat.appearance.title" },
+        },
+        HomeTabOrderSettings: {
+          screen: HomeTabOrderSettings,
+          options: { title: "feat.homeTabsOrder.title" },
+        },
+        Insights: {
+          screen: Insights,
+          options: { title: "feat.insights.title" },
+        },
+        HiddenTracks: {
+          screen: HiddenTracks,
+          options: { title: "feat.hiddenTracks.title" },
+        },
+        SaveErrors: {
+          screen: SaveErrors,
+          options: { title: "feat.saveErrors.title" },
+        },
+        PlaybackSettings: {
+          screen: PlaybackSettings,
+          options: { title: "feat.playback.title" },
+        },
+        ScanningSettings: {
+          screen: ScanningSettings,
+          options: { title: "feat.scanning.title" },
+        },
+        ExperimentalSettings: {
+          screen: ExperimentalSettings,
+          options: { title: "feat.experimental.title" },
+        },
+        ThirdParty: {
+          screen: ThirdParty,
+          options: { title: "feat.thirdParty.title" },
+        },
+        PackageLicense: { screen: PackageLicense, options: { title: "" } },
+      },
+    },
+  },
+});
+//#endregion
