68b4a38ad929c4dbae2adc6786b9ff5bd7e2fd67
cyanChill <83375816+cyanChill@users.noreply.github.com>
2025-09-24 15:25:12 -0400

    chore: Return found tracks
    - This should support either variant of "absolute" paths and relative paths.

--- mobile/src/modules/backup/M3U.ts
+++ mobile/src/modules/backup/M3U.ts
@@ -1,6 +1,9 @@
 import { getActualPath } from "@missingcore/react-native-actual-path";
 import { getDocumentAsync } from "expo-document-picker";
 import { StorageAccessFramework as SAF } from "expo-file-system";
+import { Paths } from "expo-file-system/next";
+
+import { db } from "~/db";
 
 import i18next from "~/modules/i18n";
 
@@ -16,14 +19,45 @@ export async function readM3UPlaylist() {
   if (!assets[0]) throw new Error(i18next.t("err.msg.noSelect"));
   // Using `getActualPath()` on `documentFile.uri` will give the wrong value.
   const fileLocation = await getActualPath(assets[0].uri);
+  if (!fileLocation) throw new Error(i18next.t("err.flow.generic.title"));
+  const fileDirectory = fileLocation.split("/").slice(0, -1).join("/");
   // FIXME: Current version of `expo-file-system/next` doesn't support SAF.
   const documentFile = await SAF.readAsStringAsync(assets[0].uri);
 
   // List of "file names" in the playlist.
-  const orderedTracks = documentFile
+  const trackPaths = documentFile
     .split(/\r?\n/)
     .filter((line) => !!line && !line.startsWith("#"));
-  console.log(fileLocation);
-  console.log(orderedTracks);
+  if (trackPaths.length === 0) throw new Error(i18next.t("err.msg.noContent"));
+
+  // Determine "strategy" (absolute or relative paths).
+  let strategy: "absolute" | "relative" | "unmodified" = "relative";
+  const slashStart = trackPaths[0]?.at(0) === "/";
+  if (trackPaths[0]?.startsWith("file:///")) strategy = "unmodified";
+  else {
+    const testUri = `file://${slashStart ? "" : "/"}${trackPaths[0]}`;
+    const track = await db.query.tracks.findFirst({
+      where: (fields, { eq }) => eq(fields.uri, testUri),
+    });
+    if (track) strategy = "absolute";
+  }
+
+  // Get uris so we can search our database.
+  const trackUris =
+    strategy === "absolute"
+      ? trackPaths.map((path) => `file://${slashStart ? "" : "/"}${path}`)
+      : strategy === "relative"
+        ? trackPaths.map((path) => `file://${Paths.join(fileDirectory, path)}`)
+        : trackPaths;
+
+  const playlistTracks = await db.query.tracks.findMany({
+    where: (fields, { inArray }) => inArray(fields.uri, trackUris),
+  });
+
+  // Ensure found values are in order.
+  const playlistTrackMap = Object.fromEntries(
+    playlistTracks.map((t) => [t.uri, t]),
+  );
+  return trackUris.map((uri) => playlistTrackMap[uri]).filter((t) => !!t);
 }
 //#endregion
