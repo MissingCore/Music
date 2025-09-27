fe3765a70b539f34b39ca197fd02d5353b46faa4
Anthony Liang <83375816+cyanChill@users.noreply.github.com>
2025-09-27 16:17:54 -0400

    fix: Scuffed playback when next track naturally plays (#304)

--- mobile/index.ts
+++ mobile/index.ts
@@ -5,7 +5,6 @@ import TrackPlayer from "@weights-ai/react-native-track-player";
 import { registerRootComponent } from "expo";
 
 import App from "./src/App";
-import { setupPlayer } from "./src/lib/react-native-track-player";
 import { PlaybackService } from "./src/services/RNTPService";
 
 registerRootComponent(App);
@@ -14,4 +13,3 @@ registerRootComponent(App);
   `react-native-track-player`.
 */
 TrackPlayer.registerPlaybackService(() => PlaybackService);
-setupPlayer(); // async
