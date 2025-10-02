b53daa82a0fabf11d4df86e609544a40624604ad
cyanChill <83375816+cyanChill@users.noreply.github.com>
2025-10-01 21:00:26 -0400

    chore: Ensure session storage's cached widget values gets cleared

--- mobile/src/modules/widget/utils/update.tsx
+++ mobile/src/modules/widget/utils/update.tsx
@@ -1,11 +1,15 @@
 import { requestWidgetUpdate } from "react-native-android-widget";
 
+import { sessionStore } from "~/services/SessionStore";
+
 import type { WidgetBaseProps } from "../types";
 import { MusicPlayerWidget } from "../MusicPlayerWidget";
 
 /** Have widget render "not found" state which opens the app on click. */
 export async function resetMusicPlayerWidget() {
-  await updateMusicPlayerWidget({ track: undefined, isPlaying: false });
+  const emptyState = { track: undefined, isPlaying: false };
+  sessionStore.setState({ latestWidgetData: emptyState });
+  await updateMusicPlayerWidget(emptyState);
 }
 
 /** Abstract updating the music player widget. */
