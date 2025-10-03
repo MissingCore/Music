6b121373198a5ff011beb39a5f02eb49b6a72899
cyanChill <83375816+cyanChill@users.noreply.github.com>
2025-10-03 01:11:31 -0400

    chore: Design with correct svgs

--- mobile/src/modules/widget/NowPlayingWidget.tsx
+++ mobile/src/modules/widget/NowPlayingWidget.tsx
@@ -5,6 +5,7 @@ import { WidgetDesign } from "./constants";
 import { WidgetArtwork } from "./components/WidgetArtwork";
 import { WidgetBaseLayout } from "./components/WidgetBaseLayout";
 import { WidgetCell } from "./components/WidgetCell";
+import { WidgetSVG } from "./components/WidgetSVG";
 
 type WidgetProps = WithDimensions<PlayerWidgetData & { openApp?: boolean }>;
 
@@ -12,6 +13,9 @@ export function NowPlayingWidget(props: WidgetProps) {
   const size = Math.min(props.width, props.height);
 
   const cellSize = (size - WidgetDesign.layoutGap) / 2;
+  const svgSize = cellSize / 2;
+
+  const positionOffset = cellSize + WidgetDesign.layoutGap;
 
   return (
     <WidgetBaseLayout height={size} width={size} transparent>
@@ -21,31 +25,25 @@ export function NowPlayingWidget(props: WidgetProps) {
           size={cellSize}
           style={{ borderRadius: WidgetDesign.radius }}
         >
-          <WidgetArtwork size={size} artwork={props.track?.artist ?? null} />
+          <WidgetArtwork
+            size={cellSize}
+            artwork={props.track?.artist ?? null}
+          />
+        </WidgetCell>
+
+        <WidgetCell size={cellSize} style={{ marginLeft: positionOffset }}>
+          <WidgetSVG name={props.isPlaying ? "play" : "pause"} size={svgSize} />
+        </WidgetCell>
+
+        <WidgetCell size={cellSize} style={{ marginTop: positionOffset }}>
+          <WidgetSVG name="prev" size={svgSize} />
         </WidgetCell>
 
         <WidgetCell
           size={cellSize}
-          style={{ marginLeft: cellSize + WidgetDesign.layoutGap }}
+          style={{ marginLeft: positionOffset, marginTop: positionOffset }}
         >
-          <WidgetArtwork size={size} artwork={null} />
-        </WidgetCell>
-
-        <WidgetCell
-          size={cellSize}
-          style={{ marginTop: cellSize + WidgetDesign.layoutGap }}
-        >
-          <WidgetArtwork size={size} artwork={null} />
-        </WidgetCell>
-
-        <WidgetCell
-          size={cellSize}
-          style={{
-            marginLeft: cellSize + WidgetDesign.layoutGap,
-            marginTop: cellSize + WidgetDesign.layoutGap,
-          }}
-        >
-          <WidgetArtwork size={size} artwork={null} />
+          <WidgetSVG name="next" size={svgSize} />
         </WidgetCell>
       </OverlapWidget>
     </WidgetBaseLayout>
