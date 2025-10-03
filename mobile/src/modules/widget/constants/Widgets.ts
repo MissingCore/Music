9857ef9fa17e29df36a6d53b76fed2a439480a29
cyanChill <83375816+cyanChill@users.noreply.github.com>
2025-10-03 16:44:46 -0400

    chore: Ability to exclude widgets to update via `updateWidgets`

--- mobile/src/modules/widget/constants/Widgets.ts
+++ mobile/src/modules/widget/constants/Widgets.ts
@@ -4,4 +4,6 @@ import { NowPlayingWidget } from "../NowPlayingWidget";
 export const nameToWidget = {
   ArtworkPlayer: ArtworkPlayerWidget,
   NowPlaying: NowPlayingWidget,
-};
+} as const;
+
+export type WidgetName = keyof typeof nameToWidget;
--- mobile/src/modules/widget/utils/update.tsx
+++ mobile/src/modules/widget/utils/update.tsx
@@ -1,8 +1,8 @@
 import { requestWidgetUpdate } from "react-native-android-widget";
 
 import type { PlayerWidgetData } from "../types";
-import { ArtworkPlayerWidget } from "../ArtworkPlayerWidget";
-import { NowPlayingWidget } from "../NowPlayingWidget";
+import type { WidgetName } from "../constants/Widgets";
+import { nameToWidget } from "../constants/Widgets";
 
 /** Have widget render "not found" state which opens the app on click. */
 export async function resetWidgets() {
@@ -10,15 +10,23 @@ export async function resetWidgets() {
 }
 
 /** Abstract updating all widgets. */
-export async function updateWidgets(args: PlayerWidgetData) {
-  return Promise.allSettled([
-    requestWidgetUpdate({
-      widgetName: "ArtworkPlayer",
-      renderWidget: (props) => <ArtworkPlayerWidget {...props} {...args} />,
-    }),
-    requestWidgetUpdate({
-      widgetName: "NowPlaying",
-      renderWidget: (props) => <NowPlayingWidget {...props} {...args} />,
-    }),
-  ]);
+export async function updateWidgets({
+  exclude,
+  ...args
+}: PlayerWidgetData & { exclude?: WidgetName[] }) {
+  const updatedWidgets = (Object.keys(nameToWidget) as WidgetName[]).filter(
+    (name) => (exclude ? !exclude.includes(name) : true),
+  );
+
+  return Promise.allSettled(
+    updatedWidgets.map((name) =>
+      requestWidgetUpdate({
+        widgetName: name,
+        renderWidget: (props) => {
+          const Widget = nameToWidget[name];
+          return <Widget {...props} {...args} />;
+        },
+      }),
+    ),
+  );
 }
