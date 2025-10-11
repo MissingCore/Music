74e5953f68a22f3584aca3bbe977a6648a9d8985
cyanChill <83375816+cyanChill@users.noreply.github.com>
2025-10-11 15:41:34 -0400

    chore: Updated app icon
    - Moving towards a speaker design.
    - It's monochrome and more simple as to better match the updated app icons in Nothing OS 4.
    - This will also allow us to have a separate app icon design for when the device is using light vs dark theme.
      - `ic_launcher` & `ic_launcher_round` is used for older Android versions (instead of their `xml` counterpart).
    - You may need to manually trigger an icon refresh to get the icon to reflect the current theme.
      - On my OnePlus 6, we need to click "Edit" after long-pressing on the app icon and then "Reset".
      - On my Nothing 2a, I need to switch between icon packs to trigger a refresh.
    - Also fixed issue where the bootsplash logo to reflect the current theme.

--- mobile/src/navigation/screens/OnboardingView.tsx
+++ mobile/src/navigation/screens/OnboardingView.tsx
@@ -24,7 +24,8 @@ import { StyledText, TStyledText } from "~/components/Typography/StyledText";
 export function Onboarding() {
   const { container, logo } = BootSplash.useHideAnimation({
     manifest: require("../../../assets/bootsplash/manifest.json"),
-    logo: require("../../../assets/bootsplash/logo.png"),
+    logo: require("../../../assets/bootsplash/light_logo.png"),
+    darkLogo: require("../../../assets/bootsplash/logo.png"),
 
     animate: () => {},
   });
