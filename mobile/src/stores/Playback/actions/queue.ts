02f84cb71f37f51dd2d335074a6262629092f7ec
cyanChill <83375816+cyanChill@users.noreply.github.com>
2025-10-15 16:53:52 -0400

    fix: Queue removal functions should also adjust `queuePosition`

--- mobile/src/stores/Playback/actions/queue.ts
+++ mobile/src/stores/Playback/actions/queue.ts
@@ -10,6 +10,7 @@ import { ToastOptions } from "~/lib/toast";
 export function add({ id, name }: { id: string; name: string }) {
   const { queue, queuePosition } = playbackStore.getState();
   toast(i18next.t("feat.modalTrack.extra.queueAdd", { name }), ToastOptions);
+
   if (queue.length === 0) return;
   playbackStore.setState({
     queue: queue.toSpliced(queuePosition + 1, 0, id),
@@ -18,15 +19,23 @@ export function add({ id, name }: { id: string; name: string }) {
 
 /** Remove track at specified index of current queue. */
 export function removeAtIndex(index: number) {
-  playbackStore.setState((prev) => ({
-    queue: prev.queue.toSpliced(index, 1),
-  }));
+  const { queue, queuePosition } = playbackStore.getState();
+
+  // If we removed a track before the active track, decremenet `queuePosition`.
+  let newQueuePosition = queuePosition;
+  if (index < queuePosition) newQueuePosition -= 1;
+
+  playbackStore.setState({
+    queue: queue.toSpliced(index, 1),
+    queuePosition: newQueuePosition,
+  });
 }
 
 /** Remove list of track ids in the current queue. */
 export function removeIds(ids: string[]) {
   const idSet = new Set(ids);
-  const { reset, orderSnapshot, queue, activeId } = playbackStore.getState();
+  const { reset, orderSnapshot, queue, activeId, queuePosition } =
+    playbackStore.getState();
 
   // If active track is removed, reset the playback store.
   if (activeId && idSet.has(activeId)) {
@@ -34,11 +43,20 @@ export function removeIds(ids: string[]) {
     return;
   }
 
+  // If we removed a track before the active track, decremenet `queuePosition`.
+  let newQueuePosition = queuePosition;
+
   const updatedSnapshot = orderSnapshot.filter((tId) => !idSet.has(tId));
-  const updatedQueue = queue.filter((tId) => !idSet.has(tId));
+  const updatedQueue = queue.filter((tId, index) => {
+    const isRemoved = idSet.has(tId);
+    if (isRemoved && index < queuePosition) newQueuePosition -= 1;
+    return !isRemoved;
+  });
+
   if (queue.length === updatedQueue.length) return;
   playbackStore.setState({
     orderSnapshot: updatedSnapshot,
     queue: updatedQueue,
+    queuePosition: newQueuePosition,
   });
 }
