9d4812400adeaf6f7328d91b93e7b03d207e8f3c
cyanChill <83375816+cyanChill@users.noreply.github.com>
2025-11-14 23:27:54 -0500

    chore: Rewrite of our incomplete draft to use the session store.
    - We'll probably not use gaps, but instead `justify-contents: space-between`.

--- mobile/src/navigation/screens/now-playing/components/Waveform.tsx
+++ mobile/src/navigation/screens/now-playing/components/Waveform.tsx
@@ -1,79 +1,84 @@
-import { useEffect, useMemo, useRef, useState } from "react";
+import { useCallback, useEffect, useMemo } from "react";
 import { useWindowDimensions } from "react-native";
 import { computeAmplitude } from "react-native-audio-analyzer";
 
 import { db } from "~/db";
-import type { TrackWithAlbum, WaveformSample } from "~/db/schema";
+import type { TrackWithAlbum } from "~/db/schema";
 import { waveformSamples } from "~/db/schema";
 
 import { usePreferenceStore } from "~/stores/Preference/store";
+import {
+  findAndSetCachedWaveform,
+  sessionStore,
+  useSessionStore,
+} from "~/services/SessionStore";
 
-type Track = TrackWithAlbum & { waveformSample: WaveformSample | null };
-
-export function useWaveformSamples(track: Track) {
-  const trackRef = useRef<Track | null>(null);
-  const [samples, setSamples] = useState<number[]>([]);
+export function useWaveformSamples(track: TrackWithAlbum) {
   const { width, scale } = useWindowDimensions();
   const waveformSlider = usePreferenceStore((s) => s.waveformSlider);
+  const activeWaveformContext = useSessionStore((s) => s.activeWaveformContext);
 
-  const numSamples = useMemo(
-    () => Math.round((width * scale - 32) / 10),
+  const estimatedBarCount = useMemo(
+    // FIXME: Calculations are currently based on the deviced width as
+    // we don't support landscape mode. However, Android 16 will ignore
+    // this for larger screen sizes.
+    () => Math.round(((width - 32) / 10) * scale),
     [scale, width],
   );
 
-  useEffect(() => {
-    if (trackRef.current?.id === track.id || !waveformSlider) return;
-    trackRef.current = track;
-    if (track.waveformSample !== null) {
-      setSamples(track.waveformSample.samples);
-      return;
+  const getTrackWaveform = useCallback(async () => {
+    //* See if a cached waveform exist.
+    const cachedWaveform = await findAndSetCachedWaveform(track.id);
+    if (cachedWaveform) return;
+
+    //* Compute waveform data otherwise.
+    let parsedUrl = track.uri;
+    if (parsedUrl.startsWith("file://")) parsedUrl = parsedUrl.slice(8);
+    let sampleData: number[] = [];
+    try {
+      sampleData = computeAmplitude(parsedUrl, estimatedBarCount);
+    } catch {}
+
+    // Normalize amplitude.
+    if (sampleData.length > 0) {
+      const multiplier = Math.pow(Math.max(...sampleData), -1);
+      sampleData = sampleData.map((n) => n * multiplier);
     }
-    trackRef.current.waveformSample = { trackId: track.id, samples: [] };
 
-    (async function () {
-      //* Compute waveform data.
-      let parsedUrl = track.uri;
-      if (parsedUrl.startsWith("file://")) parsedUrl = parsedUrl.slice(8);
-      let sampleData: number[] = [];
-      try {
-        // 100 samples should be enough for most screen sizes.
-        sampleData = computeAmplitude(parsedUrl, 100);
-      } catch {}
+    // Cache the data so we don't need to recompute this in the future.
+    const [foundWaveform] = await db
+      .insert(waveformSamples)
+      .values({ trackId: track.id, samples: sampleData })
+      .returning();
 
-      // Normalize amplitude.
-      if (sampleData.length > 0) {
-        const multiplier = Math.pow(Math.max(...sampleData), -1);
-        sampleData = sampleData.map((n) => n * multiplier);
-      }
+    sessionStore.setState({ activeWaveformContext: foundWaveform || null });
+  }, [estimatedBarCount, track]);
 
-      // Cache the data so we don't need to recompute this in the future.
-      await db
-        .insert(waveformSamples)
-        .values({ trackId: track.id, samples: sampleData });
-
-      if (trackRef.current) {
-        trackRef.current.waveformSample = {
-          trackId: track.id,
-          samples: sampleData,
-        };
-      }
-      setSamples(sampleData);
-    })();
-  }, [numSamples, track, waveformSlider]);
+  useEffect(() => {
+    if (!waveformSlider || activeWaveformContext?.trackId === track.id) return;
+    getTrackWaveform();
+  }, [
+    activeWaveformContext?.trackId,
+    getTrackWaveform,
+    track.id,
+    waveformSlider,
+  ]);
 
   // Since we might be storing more samples than we can render.
   const downSampledSamples = useMemo(() => {
-    if (!waveformSlider || samples.length === 0) return [];
-    const extraSamples = 100 - numSamples;
-    const extraSampleRatio = Math.ceil(extraSamples / numSamples) + 1;
+    if (!waveformSlider || activeWaveformContext?.samples.length === 0) {
+      return [];
+    }
+    const extraSamples = 100 - estimatedBarCount;
+    const extraSampleRatio = Math.ceil(extraSamples / estimatedBarCount) + 1;
 
-    console.log(numSamples, extraSamples, extraSampleRatio);
+    console.log(estimatedBarCount, extraSamples, extraSampleRatio);
 
     const newSamples: number[] = [];
     for (let i = 0; i < 100; i += extraSampleRatio) {}
 
     return newSamples;
-  }, [numSamples, samples, waveformSlider]);
+  }, [activeWaveformContext, estimatedBarCount, waveformSlider]);
 
   return downSampledSamples;
 }
