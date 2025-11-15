599e0be52a2ff3773015c002d78e6ac708928b57
cyanChill <83375816+cyanChill@users.noreply.github.com>
2025-11-15 17:16:16 -0500

    chore: Draw out the general waveform shape
    - Also added downsampling to get a more interesting shape since it takes the same amount of time to get say 100 & 1000 samples.

--- mobile/src/navigation/screens/now-playing/components/Waveform.tsx
+++ mobile/src/navigation/screens/now-playing/components/Waveform.tsx
@@ -1,5 +1,6 @@
-import { useCallback, useEffect, useMemo } from "react";
-import { useWindowDimensions } from "react-native";
+import { Canvas, Group, RoundedRect } from "@shopify/react-native-skia";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { View, useWindowDimensions } from "react-native";
 import { computeAmplitude } from "react-native-audio-analyzer";
 
 import { db } from "~/db";
@@ -11,9 +12,62 @@ import {
   sessionStore,
   useSessionStore,
 } from "~/services/SessionStore";
+import { useTheme } from "~/hooks/useTheme";
 
+//#region Waveform
+interface WaveformProps {
+  amplitudes: number[];
+}
+
+const MIN_BAR_HEIGHT = 2;
+const BAR_WIDTH = 1.75;
+const BAR_GAP = 1.5;
+
+export function Waveform({ amplitudes }: WaveformProps) {
+  const { onSurface } = useTheme();
+
+  const [canvasHeight, setCanvasHeight] = useState(0);
+  const [canvasWidth, setCanvasWidth] = useState(0);
+
+  const bars = useMemo(
+    () =>
+      amplitudes.map((amplitude, index) => {
+        const barHeight = Math.max(MIN_BAR_HEIGHT, canvasHeight * amplitude);
+        const x = (BAR_WIDTH + BAR_GAP) * index;
+        const y = (canvasHeight - barHeight) / 2;
+        return (
+          <RoundedRect
+            key={index}
+            r={8}
+            x={x}
+            y={y}
+            height={barHeight}
+            width={BAR_WIDTH}
+            color={onSurface}
+          />
+        );
+      }),
+    [amplitudes, canvasHeight, onSurface],
+  );
+
+  return (
+    <View
+      onLayout={(e) => {
+        setCanvasHeight(e.nativeEvent.layout.height);
+        setCanvasWidth(e.nativeEvent.layout.width);
+      }}
+    >
+      <Canvas style={{ height: "100%", width: "100%" }}>
+        <Group>{bars}</Group>
+      </Canvas>
+    </View>
+  );
+}
+//#endregion
+
+//#region useWaveformSamples
 export function useWaveformSamples(id: string, uri: string) {
-  const { width, scale } = useWindowDimensions();
+  const { width } = useWindowDimensions();
   const waveformSlider = usePreferenceStore((s) => s.waveformSlider);
   const activeWaveformContext = useSessionStore((s) => s.activeWaveformContext);
 
@@ -21,8 +75,8 @@ export function useWaveformSamples(id: string, uri: string) {
     // FIXME: Calculations are currently based on the deviced width as
     // we don't support landscape mode. However, Android 16 will ignore
     // this for larger screen sizes.
-    () => Math.round(((width - 32) / 10) * scale),
-    [scale, width],
+    () => Math.round((width - 32) / (BAR_WIDTH + BAR_GAP)),
+    [width],
   );
 
   const getTrackWaveform = useCallback(async () => {
@@ -30,18 +84,28 @@ export function useWaveformSamples(id: string, uri: string) {
     const cachedWaveform = await findAndSetCachedWaveform(id);
     if (cachedWaveform) return;
 
+    // Downsampling from a multiplier will take the same amount of time.
+    const scaledBarCount = estimatedBarCount * 10;
+
     //* Compute waveform data otherwise.
     let parsedUrl = uri;
     if (parsedUrl.startsWith("file://")) parsedUrl = parsedUrl.slice(8);
     let sampleData: number[] = [];
     try {
-      sampleData = computeAmplitude(parsedUrl, estimatedBarCount);
+      sampleData = computeAmplitude(parsedUrl, scaledBarCount);
     } catch {}
 
-    // Normalize amplitude.
     if (sampleData.length > 0) {
-      const multiplier = Math.pow(Math.max(...sampleData), -1);
-      sampleData = sampleData.map((n) => n * multiplier);
+      const downSampledBars: number[] = [];
+      // Downsample the data.
+      for (let i = 0; i < estimatedBarCount; i++) {
+        const idx = Math.floor((i / estimatedBarCount) * scaledBarCount);
+        downSampledBars.push(sampleData[idx] ?? 0);
+      }
+
+      // Normalize amplitude.
+      const multiplier = Math.pow(Math.max(...downSampledBars), -1);
+      sampleData = downSampledBars.map((n) => n * multiplier);
     }
 
     // Cache the data so we don't need to recompute this in the future.
@@ -60,3 +124,4 @@ export function useWaveformSamples(id: string, uri: string) {
 
   return activeWaveformContext?.samples || [];
 }
+//#endregion
