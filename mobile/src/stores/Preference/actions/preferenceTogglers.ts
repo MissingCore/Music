4de5ad09c1f3d99aeed5793d949ab2965d32aab4
cyanChill <83375816+cyanChill@users.noreply.github.com>
2025-12-06 23:45:04 -0500

    chore: Make sure we reset `queuedNext` when we toggle the experiment

--- mobile/src/stores/Preference/actions/preferenceTogglers.ts
+++ mobile/src/stores/Preference/actions/preferenceTogglers.ts
@@ -44,6 +44,7 @@ export function toggleQueueAwareNext() {
   preferenceStore.setState((prev) => ({
     queueAwareNext: !prev.queueAwareNext,
   }));
+  playbackStore.setState({ queuedNext: 0 });
 }
 
 export function toggleRCNotification() {
