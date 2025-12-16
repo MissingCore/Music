2c2611b0d2ccd6d23a48e2df9c4d2c08422ac80d
cyanChill <83375816+cyanChill@users.noreply.github.com>
2025-12-16 14:14:08 -0500

    chore: Update natural playback delay sheet design
    - We plan on maybe moving this setting into the sheet on the "Now Playing" screen in the future.

---
+++ mobile/src/navigation/sheets/PlaybackDelaySheet.tsx
@@ -0,0 +1,20 @@
+import { usePreferenceStore } from "~/stores/Preference/store";
+import { PreferenceSetters } from "~/stores/Preference/actions";
+
+import type { TrueSheetRef } from "~/components/Sheet";
+import { DetachedNumericSheet } from "~/components/Sheet/DetachedNumeric";
+
+/** @deprecated We plan on integrating this setting in a different sheet. */
+export function PlaybackDelaySheet(props: { ref: TrueSheetRef }) {
+  const playbackDelay = usePreferenceStore((s) => s.playbackDelay);
+  return (
+    <DetachedNumericSheet
+      ref={props.ref}
+      titleKey="feat.playbackDelay.title"
+      descriptionKey="feat.playbackDelay.description"
+      valueLabelKey="plural.second_other"
+      value={playbackDelay}
+      setValue={PreferenceSetters.setPlaybackDelay}
+    />
+  );
+}
