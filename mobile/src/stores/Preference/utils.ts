41035342c7d789ba63e499cad93b86c1361a7808
cyanChill <83375816+cyanChill@users.noreply.github.com>
2025-12-16 15:49:55 -0500

    chore: Simplify Force LTR fix
    - Essentially do the same thing as what we do when we toggle the feature instead of conditionally firing the functions.
    - Also explictly require the boolean to help prevent this issue.

--- mobile/src/stores/Preference/utils.ts
+++ mobile/src/stores/Preference/utils.ts
@@ -5,14 +5,9 @@ import i18next from "~/modules/i18n";
 /** Ensure `i18next` & React Native is aware of the current app language. */
 export async function resolveLanguageConfigs(
   language: string,
-  forceLTR = false,
+  forceLTR: boolean,
 ) {
   await i18next.changeLanguage(language);
-  // Also fire when language is LTR as if `forceLTR = true` and we're coming
-  // from an RTL language, the next app launch will have the LTR language in
-  // an RTL layout.
-  if (!forceLTR || i18next.dir() === "ltr") {
-    I18nManager.allowRTL(i18next.dir() === "rtl");
-    I18nManager.forceRTL(i18next.dir() === "rtl");
-  }
+  I18nManager.allowRTL(forceLTR ? false : i18next.dir() === "rtl");
+  I18nManager.forceRTL(forceLTR ? false : i18next.dir() === "rtl");
 }
