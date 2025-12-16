b99c83f9c7586b08d37b45eebce93d54859d1cf3
cyanChill <83375816+cyanChill@users.noreply.github.com>
2025-12-15 21:44:47 -0500

    fix: Layout getting reset to RTL when `forceLTR = true` on next launch

--- mobile/src/stores/Preference/store.ts
+++ mobile/src/stores/Preference/store.ts
@@ -19,6 +19,7 @@ export const preferenceStore = createPersistedSubscribedStore<PreferenceStore>(
       // Try to use device language if no language is specified.
       await resolveLanguageConfigs(
         state.language || getLocales()[0]?.languageTag || "en",
+        state.forceLTR,
       );
       if (state.language === "") {
         const usedLanguage = i18next.resolvedLanguage;
