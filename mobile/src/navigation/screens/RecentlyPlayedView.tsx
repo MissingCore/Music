2b5182ed6bfac780ec4bfd766fdb595b4d06c8ca
cyanChill <83375816+cyanChill@users.noreply.github.com>
2025-12-19 16:56:23 -0500

    chore: For this version of FlashList, we don't need to double the horizontal padding when used inside of a Legend List

--- mobile/src/navigation/screens/RecentlyPlayedView.tsx
+++ mobile/src/navigation/screens/RecentlyPlayedView.tsx
@@ -113,9 +113,7 @@ function RecentlyPlayedLists(props: { data?: MediaCardContent[] }) {
         />
       )}
       className="-mx-4"
-      // FIXME: For some weird reason, only 50% of the horizontal padding
-      // gets applied on a horizontal FlashList.
-      contentContainerClassName="px-8 pb-6"
+      contentContainerClassName="px-4 pb-6"
       disableAutoLayout={I18nManager.isRTL}
     />
   );
