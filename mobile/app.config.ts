9bf6f82fa4e757a7391357ce6f9f81e86802a23c
cyanChill <83375816+cyanChill@users.noreply.github.com>
2025-12-27 20:11:58 -0500

    chore(deps): Bumped dependencies
    - Switched to using `expo-build-properties` to set values in `gradle.properties`.

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -96,6 +96,16 @@ export default (): ExpoConfig => ({
   },
   plugins: [
     [
+      "expo-build-properties",
+      {
+        android: {
+          enableBundleCompression: true,
+          enableProguardInReleaseBuilds: true,
+          enableShrinkResourcesInReleaseBuilds: true,
+        },
+      },
+    ],
+    [
       "expo-font",
       {
         fonts: [
