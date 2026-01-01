e190545361c26a4e4b50acced33a5cffe4918ec8
cyanChill <83375816+cyanChill@users.noreply.github.com>
2026-01-01 15:56:44 -0500

    chore: Flatten migrations

--- mobile/src/db/drizzle/migrations.js
+++ mobile/src/db/drizzle/migrations.js
@@ -12,8 +12,7 @@ import m0007 from "./0007_cooing_the_stranger.sql";
 import m0008 from "./0008_little_toad.sql";
 import m0009 from "./0009_ambitious_puppet_master.sql";
 import m0010 from "./0010_spooky_blue_marvel.sql";
-import m0011 from "./0011_silky_black_queen.sql";
-import m0012 from "./0012_typical_vector.sql";
+import m0011 from "./0011_complete_sally_floyd.sql";
 
 export default {
   journal,
@@ -30,6 +29,5 @@ export default {
     m0009,
     m0010,
     m0011,
-    m0012,
   },
 };
