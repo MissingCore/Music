3b6151bb4e5679877ffd95ce0c38a957b518cf0c
cyanChill <83375816+cyanChill@users.noreply.github.com>
2026-01-18 13:23:12 -0500

    chore: Update Sleep Timer sheet design
    - Swapped to using `DetachedSheet`.

--- mobile/src/navigation/screens/now-playing/sheets/SleepTimerSheet/index.tsx
+++ mobile/src/navigation/screens/now-playing/sheets/SleepTimerSheet/index.tsx
@@ -1,6 +1,6 @@
 import { useMemo, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { Keyboard, View } from "react-native";
+import { Keyboard } from "react-native";
 
 import { useSleepTimerStore } from "./store";
 
@@ -8,77 +8,74 @@ import { wait } from "~/utils/promise";
 import { ExtendedTButton } from "~/components/Form/Button";
 import { ClickwrapCheckbox } from "~/components/Form/Checkbox";
 import { NumericInput } from "~/components/Form/Input";
-import { Sheet } from "~/components/Sheet";
+import { DetachedSheet } from "~/components/Sheet/Detached";
 import type { TrueSheetRef } from "~/components/Sheet/useSheetRef";
 import { StyledText, TStyledText } from "~/components/Typography/StyledText";
-import { deferInitialRender } from "../../../../components/DeferredRender";
 
-/** Allows the user to set a time when the music will stop playing. */
-export const SleepTimerSheet = deferInitialRender(
-  function SleepTimerSheet(props: { ref: TrueSheetRef }) {
-    const { t } = useTranslation();
-    const sleepTimerLength = useSleepTimerStore((s) => s.duration);
-    const extendTimer = useSleepTimerStore((s) => s.extension);
-    const toggleExtension = useSleepTimerStore((s) => s.toggleExtension);
-    const endAt = useSleepTimerStore((s) => s.endAt);
-    const createTimer = useSleepTimerStore((s) => s.create);
-    const clearTimer = useSleepTimerStore((s) => s.clear);
-    const [minutes, setMinutes] = useState(`${sleepTimerLength}`);
+export function SleepTimerSheet(props: { ref: TrueSheetRef }) {
+  const { t } = useTranslation();
+  const sleepTimerLength = useSleepTimerStore((s) => s.duration);
+  const extendTimer = useSleepTimerStore((s) => s.extension);
+  const toggleExtension = useSleepTimerStore((s) => s.toggleExtension);
+  const endAt = useSleepTimerStore((s) => s.endAt);
+  const createTimer = useSleepTimerStore((s) => s.create);
+  const clearTimer = useSleepTimerStore((s) => s.clear);
+  const [minutes, setMinutes] = useState(`${sleepTimerLength}`);
 
-    const hasTimer = endAt !== null;
+  const hasTimer = endAt !== null;
 
-    const endString = useMemo(() => {
-      if (endAt === null) return;
-      return new Date(endAt).toLocaleTimeString(undefined, {
-        hour: "numeric",
-        minute: "numeric",
-      });
-    }, [endAt]);
+  const minutesAsNum = useMemo(() => {
+    const asNum = Number(minutes);
+    // Validate that it's a positive integer.
+    if (!Number.isInteger(asNum) || asNum <= 0) return;
+    return asNum;
+  }, [minutes]);
 
-    const onSubmit = async () => {
-      const asNum = Number(minutes);
-      // Validate that it's a positive integer.
-      if (!Number.isInteger(asNum) || asNum <= 0) return;
-      Keyboard.dismiss();
-      await wait(1);
-      createTimer(asNum);
-    };
+  const endString = useMemo(() => {
+    if (minutesAsNum === undefined) return "—";
+    const estEndTime = endAt ? endAt : Date.now() + minutesAsNum * 60000;
+    return new Date(estEndTime).toLocaleTimeString(undefined, {
+      hour: "numeric",
+      minute: "numeric",
+    });
+  }, [endAt, minutesAsNum]);
 
-    return (
-      <Sheet ref={props.ref} titleKey="feat.sleepTimer.title">
-        <TStyledText
-          dim
-          textKey="feat.sleepTimer.description"
-          className="text-center text-sm"
-        />
-        <View pointerEvents={hasTimer ? "none" : "auto"}>
-          <NumericInput
-            defaultValue={`${sleepTimerLength}`}
-            editable={!hasTimer}
-            onChangeText={(text) => setMinutes(text)}
-            className="mx-auto w-full max-w-1/2 border-b border-outline text-center"
-            forSheet
-          />
-        </View>
-        {hasTimer ? (
-          <StyledText dim center>
-            {t("feat.sleepTimer.extra.stopTime", { time: endString })}
-          </StyledText>
-        ) : null}
+  const onSubmit = async () => {
+    if (minutesAsNum === undefined) return;
+    Keyboard.dismiss();
+    await wait(1);
+    createTimer(minutesAsNum);
+  };
 
-        <ClickwrapCheckbox
-          textKey="feat.sleepTimer.extra.extend"
-          checked={extendTimer}
-          onCheck={toggleExtension}
-          disabled={hasTimer}
-        />
-
-        <ExtendedTButton
-          textKey={hasTimer ? "form.clear" : "feat.sleepTimer.extra.start"}
-          onPress={hasTimer ? clearTimer : onSubmit}
-          className="rounded-full"
-        />
-      </Sheet>
-    );
-  },
-);
+  return (
+    <DetachedSheet ref={props.ref} titleKey="feat.sleepTimer.title">
+      <TStyledText
+        textKey="feat.sleepTimer.description"
+        dim
+        className="text-sm"
+      />
+      <NumericInput
+        pointerEvents={hasTimer ? "none" : "auto"}
+        defaultValue={`${sleepTimerLength}`}
+        editable={!hasTimer}
+        onChangeText={(text) => setMinutes(text)}
+        className="mx-auto w-full max-w-1/2 border-b border-outline text-center"
+        forSheet
+      />
+      <StyledText dim>
+        {t("feat.sleepTimer.extra.stopTime", { time: endString })}
+      </StyledText>
+      <ClickwrapCheckbox
+        textKey="feat.sleepTimer.extra.extend"
+        checked={extendTimer}
+        onCheck={toggleExtension}
+        disabled={hasTimer}
+      />
+      <ExtendedTButton
+        textKey={hasTimer ? "form.clear" : "feat.sleepTimer.extra.start"}
+        onPress={hasTimer ? clearTimer : onSubmit}
+        className="rounded-full"
+      />
+    </DetachedSheet>
+  );
+}
