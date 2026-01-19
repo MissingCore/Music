47634761318c24e23e39c66e016fc186558d9a2f
cyanChill <83375816+cyanChill@users.noreply.github.com>
2026-01-18 19:18:13 -0500

    chore: Uninstall `@react-native-assets/slider`
    - This was used to make the `NSlider` slider used inside the sheet (which we switched to our custom `CachedSlider`.

--- mobile/src/components/Form/Slider.tsx
+++ mobile/src/components/Form/Slider.tsx
@@ -1,12 +1,6 @@
 import { Slider as RNSlider } from "@miblanchard/react-native-slider";
-import SheetSlider from "@react-native-assets/slider";
-import { useState } from "react";
-import { Pressable, View } from "react-native";
 
 import { useTheme } from "~/hooks/useTheme";
-
-import { cn } from "~/lib/style";
-import { StyledText } from "../Typography/StyledText";
 
 //#region Regular
 /** Slider with default styling. */
@@ -36,95 +30,6 @@ export function Slider(props: {
       containerStyle={{ height: props.thumbSize + 16 }}
       inverted={props.inverted}
     />
-  );
-}
-//#endregion
-
-//#region Nothing Slider
-/** Custom slider design to match the one in the Nothing X app. */
-export function NSlider(
-  props: NMarkProps & {
-    label: string;
-    icon: React.ReactNode;
-    formatValue: (value: number) => string;
-    step?: number;
-  },
-) {
-  const { primary, surfaceContainerLowest } = useTheme();
-  const [width, setWidth] = useState<number>();
-
-  const formattedValue = props.formatValue(props.value);
-
-  return (
-    <View
-      onLayout={(e) => setWidth(e.nativeEvent.layout.width)}
-      className="relative overflow-hidden rounded-full bg-surfaceContainerLowest"
-    >
-      <View
-        accessible
-        accessibilityLabel={`${props.label}: ${formattedValue}`}
-        pointerEvents="none"
-        className="absolute top-1/2 z-10 w-full -translate-y-1/2 flex-row items-center justify-center gap-2"
-      >
-        {props.icon}
-        <StyledText className="min-w-12 text-sm" bold>
-          {formattedValue}
-        </StyledText>
-      </View>
-      <SheetSlider
-        value={props.value}
-        minimumValue={props.min}
-        maximumValue={props.max}
-        step={props.step}
-        onValueChange={props.onChange as (val: number) => void}
-        minimumTrackTintColor={`${primary}33`} // 20% Opacity
-        maximumTrackTintColor={surfaceContainerLowest}
-        thumbSize={
-          props.value === props.min || props.value === props.max ? 0 : 2
-        }
-        trackHeight={64}
-        thumbStyle={{ height: 64, backgroundColor: primary }}
-        // The wrapper adds some extra padding, which this will negate.
-        style={{ height: 64 }}
-      />
-      {width !== undefined ? <NSliderMarks width={width} {...props} /> : null}
-    </View>
-  );
-}
-
-type NMarkProps = {
-  value: number;
-  min: number;
-  max: number;
-  trackMarks?: number[];
-  onChange: (value: number) => void | Promise<void>;
-};
-
-const markClass =
-  "absolute top-1/2 h-4 w-0.5 -translate-x-1/2 -translate-y-1/2";
-
-function NSliderMarks({ min, max, ...rest }: NMarkProps & { width: number }) {
-  if (!rest.trackMarks) return null;
-  return rest.trackMarks.map((val) =>
-    val !== min && val !== max ? (
-      <View
-        key={val}
-        pointerEvents="none"
-        style={{ left: ((val - min) / (max - min)) * rest.width }}
-        className={cn("bg-onSurface/5", markClass)}
-      />
-    ) : (
-      <Pressable
-        key={val}
-        accessible={false}
-        hitSlop={{ left: 16, right: 16, top: 24, bottom: 24 }}
-        onPress={() => rest.onChange(val)}
-        disabled={rest.value === val}
-        pointerEvents={rest.value === val ? "none" : "auto"}
-        style={{ left: ((val - min) / (max - min)) * rest.width }}
-        className={markClass}
-      />
-    ),
   );
 }
 //#endregion
