ba5e540d8251ccb3f0d4e01a766a429fa1c001f8
cyanChill <83375816+cyanChill@users.noreply.github.com>
2026-01-25 18:34:23 -0500

    chore: Initial synchronized lyrics highlighting

--- mobile/src/navigation/screens/now-playing/components/LyricsOverlay.tsx
+++ mobile/src/navigation/screens/now-playing/components/LyricsOverlay.tsx
@@ -6,6 +6,7 @@ import { View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 import { useLyricForTrack } from "~/queries/lyric";
+import { usePlaybackStore } from "~/stores/Playback/store";
 import { usePreferenceStore } from "~/stores/Preference/store";
 import { useTheme } from "~/hooks/useTheme";
 
@@ -17,7 +18,7 @@ import { StyledText, TStyledText } from "~/components/Typography/StyledText";
 const SCROLL_OFFSET = 64;
 const LINE_GAP = 16;
 
-export function LyricsOverlay(props: LyricsContentProps) {
+export function LyricsOverlay(props: { size: number; trackId: string }) {
   const { top } = useSafeAreaInsets();
   const { scheme, surface } = useTheme();
   const screenDesign = usePreferenceStore((s) => s.nowPlayingDesign);
@@ -35,7 +36,7 @@ export function LyricsOverlay(props: LyricsContentProps) {
       )}
     >
       <View style={{ width: props.size }} className="px-2">
-        <LyricsContent {...props} offset={lyricsOffset} />
+        <LyricsContent trackId={props.trackId} offset={lyricsOffset} />
       </View>
 
       <LinearGradient
@@ -58,20 +59,23 @@ export function LyricsOverlay(props: LyricsContentProps) {
   );
 }
 
-type LyricsContentProps = { size: number; trackId: string };
-
-function LyricsContent(props: LyricsContentProps & { offset: number }) {
+function LyricsContent(props: { trackId: string; offset: number }) {
   const { t } = useTranslation();
   const navigation = useNavigation();
   const { isPending, data, error } = useLyricForTrack(props.trackId);
 
   const lyricsLines = useMemo(() => {
-    if (!data?.lyrics) return "";
+    if (!data?.lyrics) return [];
     return data.lyrics.split("\n");
   }, [data?.lyrics]);
 
+  const isSynchronized = useMemo(
+    () => lyricsLines.every((line) => (!line ? true : LRC_LINE.test(line))),
+    [lyricsLines],
+  );
+
   if (isPending) return null;
-  if (error || !data) {
+  else if (error || !data) {
     return (
       <View
         style={{ paddingTop: props.offset }}
@@ -92,8 +96,9 @@ function LyricsContent(props: LyricsContentProps & { offset: number }) {
         </Button>
       </View>
     );
+  } else if (isSynchronized) {
+    return <SynchronizedLyrics lines={lyricsLines} offset={props.offset} />;
   }
-
   return (
     <FlatList
       data={lyricsLines}
@@ -111,3 +116,73 @@ function LyricsContent(props: LyricsContentProps & { offset: number }) {
     />
   );
 }
+
+//#region Synchronized Lyrics
+function SynchronizedLyrics(props: { lines: string[]; offset: number }) {
+  const position = usePlaybackStore((s) => s.lastPosition);
+
+  const parsedLines = useMemo(() => parseLines(props.lines), [props.lines]);
+
+  const activeIndex = useMemo(() => {
+    const positionMS = position * 1000;
+    const numLines = parsedLines.length;
+    for (let i = 0; i < numLines; i++) {
+      if (parsedLines[i]!.timeMS < positionMS) continue;
+      return Math.max(i - 1, 0);
+    }
+    return numLines - 1;
+  }, [parsedLines, position]);
+
+  return (
+    <FlatList
+      data={parsedLines}
+      keyExtractor={(_, index) => `${index}`}
+      renderItem={({ item, index }) => (
+        <StyledText
+          bold
+          className={cn("text-xl", {
+            "text-onSurfaceVariant/50": index !== activeIndex,
+          })}
+        >
+          {item.lyric}
+        </StyledText>
+      )}
+      contentContainerStyle={{
+        paddingTop: props.offset,
+        paddingBottom: SCROLL_OFFSET,
+        gap: LINE_GAP,
+      }}
+    />
+  );
+}
+
+const LRC_LINE = /^(\[[0-9]+:[0-9]+(\.[0-9]+)?\])+.*/;
+const LRC_TIMESTAMP_WITH_BRACKET = /\[[0-9]+:[0-9]+(\.[0-9]+)?\]/g;
+const LRC_TIMESTAMP = /[0-9]+/g;
+
+type SynchronizedLine = { timeMS: number; lyric: string };
+
+function parseLines(lines: string[]): SynchronizedLine[] {
+  const results: SynchronizedLine[] = [];
+  for (const line of lines) {
+    if (!line) continue;
+    const timeString = line.match(LRC_TIMESTAMP_WITH_BRACKET);
+    if (!timeString || timeString.length === 0) continue;
+    const lyricLine = line.replace(LRC_TIMESTAMP_WITH_BRACKET, "");
+    const [min, sec, ms = "0"] = timeString[0]!.match(LRC_TIMESTAMP) as [
+      string,
+      string,
+      ...string[],
+    ];
+    results.push({
+      timeMS:
+        Number.parseInt(min) * 60 * 1000 +
+        Number.parseInt(sec) * 1000 +
+        Number.parseInt(ms),
+      lyric: lyricLine,
+    });
+  }
+
+  return results;
+}
+//#endregion
