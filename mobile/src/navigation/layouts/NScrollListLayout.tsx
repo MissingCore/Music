b4505d14d3ba81e8d38a8d0efedac10365202d86
cyanChill <83375816+cyanChill@users.noreply.github.com>
2026-01-28 01:08:23 -0500

    chore: Move "header" component to `NScrollListLayout` file
    - If we don't provide additional actions, we use `FilledIconButton`. Otherwise, we use `IconButton` (as I liked the look of it on the "Now Playing" screen).

--- mobile/src/navigation/layouts/NScrollListLayout.tsx
+++ mobile/src/navigation/layouts/NScrollListLayout.tsx
@@ -1,7 +1,11 @@
 import type { LegendListProps } from "@legendapp/list";
+import type { ParseKeys } from "i18next";
+import { useMemo } from "react";
+import { useTranslation } from "react-i18next";
 import { View } from "react-native";
 import { useAnimatedScrollHandler } from "react-native-reanimated";
 
+import { MoreHoriz } from "~/resources/icons/MoreHoriz";
 import { usePreferenceStore } from "~/stores/Preference/store";
 import { useBottomActionsInset } from "../hooks/useBottomActions";
 
@@ -9,7 +13,13 @@ import {
   AnimatedLegendList,
   useAnimatedLegendListRef,
 } from "~/components/Defaults";
+import { FilledIconButton, IconButton } from "~/components/Form/Button/Icon";
+import { Marquee } from "~/components/Marquee";
 import { Scrollbar, useScrollbarContext } from "~/components/NScrollbar";
+import { SafeContainer } from "~/components/SafeContainer";
+import type { TrueSheetRef } from "~/components/Sheet/useSheetRef";
+import { useSheetRef } from "~/components/Sheet/useSheetRef";
+import { AccentText } from "~/components/Typography/AccentText";
 
 export function NScrollListLayout<TData>(
   props: Omit<LegendListProps<TData>, "onContentSizeChange" | "onLayout">,
@@ -41,5 +51,44 @@ export function NScrollListLayout<TData>(
         {...layoutInfo}
       />
     </View>
+  );
+}
+
+/** Accompanying header for `NScrollListLayout`. */
+export function NScrollListHeader(props: {
+  titleKey: ParseKeys;
+  OptionsSheet: (props: { ref: TrueSheetRef }) => React.JSX.Element;
+  /** Wrap additional "actions" which will appear before the "Screen Options" button. */
+  children?: React.ReactNode;
+}) {
+  const { t } = useTranslation();
+  const sheetRef = useSheetRef();
+
+  const IconButtonComponent = useMemo(
+    () => (props.children ? IconButton : FilledIconButton),
+    [props.children],
+  );
+
+  return (
+    <>
+      <props.OptionsSheet ref={sheetRef} />
+      <SafeContainer
+        additionalTopOffset={16}
+        className="flex-row items-center justify-between gap-4 p-4"
+      >
+        <Marquee>
+          <AccentText className="text-4xl">{t(props.titleKey)}</AccentText>
+        </Marquee>
+        <View className="flex-row items-center gap-1 rounded-full bg-surfaceContainerLowest">
+          {props.children}
+          <IconButtonComponent
+            Icon={MoreHoriz}
+            accessibilityLabel={t("feat.modalViewPreference.title")}
+            onPress={() => sheetRef.current?.present()}
+            size="sm"
+          />
+        </View>
+      </SafeContainer>
+    </>
   );
 }
--- mobile/src/navigation/screens/artists/View.tsx
+++ mobile/src/navigation/screens/artists/View.tsx
@@ -1,45 +1,26 @@
-import { useTranslation } from "react-i18next";
-
-import { MoreHoriz } from "~/resources/icons/MoreHoriz";
 import { useArtists } from "~/queries/artist";
 import { useViewLayout } from "~/stores/ViewPreference/hooks";
 
-import { NScrollListLayout } from "~/navigation/layouts/NScrollListLayout";
+import {
+  NScrollListHeader,
+  NScrollListLayout,
+} from "~/navigation/layouts/NScrollListLayout";
 import { ContentPlaceholder } from "~/navigation/components/Placeholder";
 import { ArtistsViewOptionsSheet } from "./sheets/ViewOptionsSheet";
 
 import type { ExtractQueryData } from "~/lib/react-query";
-import { FilledIconButton } from "~/components/Form/Button/Icon";
-import { Marquee } from "~/components/Marquee";
-import { SafeContainer } from "~/components/SafeContainer";
-import { useSheetRef } from "~/components/Sheet/useSheetRef";
-import { AccentText } from "~/components/Typography/AccentText";
 import type { LayoutItem } from "~/stores/ViewPreference/types";
 
 export default function Artists() {
-  const { t } = useTranslation();
   const { isPending, data } = useArtists();
   const presets = useViewLayout("artist", data, formatData);
-  const artistsViewOptionsSheetRef = useSheetRef();
 
   return (
     <>
-      <ArtistsViewOptionsSheet ref={artistsViewOptionsSheetRef} />
-
-      <SafeContainer
-        additionalTopOffset={16}
-        className="flex-row items-center justify-between gap-2 p-4"
-      >
-        <Marquee>
-          <AccentText className="text-4xl">{t("term.artists")}</AccentText>
-        </Marquee>
-        <FilledIconButton
-          Icon={MoreHoriz}
-          accessibilityLabel={t("feat.modalViewPreference.title")}
-          onPress={() => artistsViewOptionsSheetRef.current?.present()}
-          size="sm"
-        />
-      </SafeContainer>
+      <NScrollListHeader
+        titleKey="term.artist"
+        OptionsSheet={ArtistsViewOptionsSheet}
+      />
       <NScrollListLayout
         ListEmptyComponent={
           <ContentPlaceholder
