a9429c3809c75d777f6a2a9451fb606005df8e32
cyanChill <83375816+cyanChill@users.noreply.github.com>
2026-01-27 21:46:51 -0500

    chore: Draft of hook to handle generating the props for the different layouts we can have on a home screen
    - We might want to shrink the gap for the compact grid, but would require a rewrite as our margin logic won't work anymore.

---
+++ mobile/src/stores/HomeViewPreference/hooks.tsx
@@ -0,0 +1,100 @@
+import type { LegendListProps } from "@legendapp/list";
+import { useNavigation } from "@react-navigation/native";
+import { useMemo } from "react";
+
+import type { GCWProps } from "~/hooks/useGetColumn";
+import { useGetColumn } from "~/hooks/useGetColumn";
+import { useHomeViewPreferenceStore } from "./store";
+import type { LayoutItem, MutableLayout } from "./types";
+
+import { getMediaLinkContext } from "~/navigation/utils/router";
+
+import { cn } from "~/lib/style";
+import { MediaCard } from "~/modules/media/components/MediaCard";
+import { SearchResult } from "~/modules/search/components/SearchResult";
+
+const gridLayoutOptions: GCWProps = {
+  cols: 2,
+  gap: 12,
+  gutters: 32,
+  minWidth: 175,
+};
+const compactGridLayoutOptions: GCWProps = {
+  cols: 3,
+  gap: 12,
+  gutters: 32,
+  minWidth: 100,
+};
+
+/** Formats data to pass into `LegendList`. */
+export function useHomeViewLayout<TData extends Record<string, any>>(
+  screen: MutableLayout,
+  data: TData[] = [],
+  formatData: (data: TData) => LayoutItem,
+) {
+  const navigation = useNavigation();
+
+  const formattedData = useMemo(() => data.map(formatData), [data, formatData]);
+
+  //#region Layout Configs
+  const gridLayout = useGetColumn(gridLayoutOptions);
+  const compactGridLayout = useGetColumn(compactGridLayoutOptions);
+  const layoutOption = useHomeViewPreferenceStore((s) => s[`${screen}Layout`]);
+
+  const listLayoutArgs = useMemo(
+    () =>
+      ({
+        estimatedItemSize: 56, // 48px Height + 8px Margin Top
+        renderItem: ({ item: { id, ...item }, index }) => (
+          <SearchResult
+            button
+            type={screen}
+            {...item}
+            onPress={() =>
+              navigation.navigate(...getMediaLinkContext({ id, type: screen }))
+            }
+            className={cn({
+              "mt-2": index > 0,
+              "rounded-full pr-4": screen === "artist",
+            })}
+          />
+        ),
+      }) satisfies Omit<LegendListProps<LayoutItem>, "data">,
+    [navigation, screen],
+  );
+
+  // Handle both `grid` & `compactGrid` options.
+  const gridLayoutArgs = useMemo(() => {
+    const gridOpts = layoutOption === "grid" ? gridLayout : compactGridLayout;
+    return {
+      // ~40px for text content under `<MediaImage />` + 12px Margin Bottom
+      estimatedItemSize: gridOpts.width + 40 + 12,
+      renderItem: ({ item }) => (
+        <MediaCard
+          type={screen}
+          {...item}
+          source={item.imageSource}
+          size={gridOpts.width}
+          onPress={() =>
+            navigation.navigate(
+              ...getMediaLinkContext({ id: item.id, type: screen }),
+            )
+          }
+          className="mx-1.5 mb-3"
+        />
+      ),
+      className: "-mx-1.5 -mb-3",
+    } satisfies Omit<LegendListProps<LayoutItem>, "data">;
+  }, [navigation, layoutOption, screen, gridLayout, compactGridLayout]);
+  //#endregion
+
+  return useMemo(
+    () =>
+      ({
+        data: formattedData,
+        keyExtractor: ({ id }) => id,
+        ...(layoutOption === "list" ? listLayoutArgs : gridLayoutArgs),
+      }) satisfies LegendListProps<LayoutItem>,
+    [layoutOption, formattedData, listLayoutArgs, gridLayoutArgs],
+  );
+}
--- mobile/src/stores/HomeViewPreference/types.ts
+++ mobile/src/stores/HomeViewPreference/types.ts
@@ -1,2 +1,10 @@
 /** Screens where the layout of the content can change. */
-export type MutableLayout = "album" | "artist" | "playlist";
+export type MutableLayout = "artist";
+
+/** Fields that get rendered in our mutable layout. */
+export type LayoutItem = {
+  id: string;
+  title: string;
+  description: string;
+  imageSource: string | null;
+};
