fbccc71a501cbc4c9c7c24d5e768bcaa934f7650
cyanChill <83375816+cyanChill@users.noreply.github.com>
2026-01-29 18:07:31 -0500

    chore: Update Albums screen design
    - Temporarily using the sheet for the Artists screen.

--- mobile/src/navigation/screens/albums/View.tsx
+++ mobile/src/navigation/screens/albums/View.tsx
@@ -1,15 +1,62 @@
-import { useAlbumsForCards } from "~/queries/album";
-import { StickyActionListLayout } from "../../layouts/StickyActionScroll";
+import { useMemo } from "react";
 
-import { useMediaCardListPreset } from "~/modules/media/components/MediaCard";
+import { useAlbums } from "~/queries/album";
+import { usePreferenceStore } from "~/stores/Preference/store";
+import { useViewLayout } from "~/stores/ViewPreference/hooks/useViewLayout";
+import { useViewOrder } from "~/stores/ViewPreference/hooks/useViewOrder";
+
+import { ArtistsViewOptionsSheet } from "~/navigation/sheets/ViewOptionsSheet";
+import {
+  NScrollListHeader,
+  NScrollListLayout,
+} from "~/navigation/layouts/NScrollListLayout";
+import { ContentPlaceholder } from "~/navigation/components/Placeholder";
+
+import type { ExtractQueryData } from "~/lib/react-query";
 
 export default function Albums() {
-  const { isPending, data } = useAlbumsForCards();
-  const presets = useMediaCardListPreset({
-    data,
-    isPending,
-    errMsgKey: "err.msg.noAlbums",
-  });
+  const { isPending, data } = useAlbums();
+  const minAlbumLength = usePreferenceStore((s) => s.minAlbumLength);
 
-  return <StickyActionListLayout titleKey="term.albums" {...presets} />;
+  const filteredData = useMemo(
+    () => data?.filter(({ trackCount }) => trackCount >= minAlbumLength),
+    [data, minAlbumLength],
+  );
+
+  const sortedData = useViewOrder("album", filteredData, AlbumSortStrategies);
+  const presets = useViewLayout("album", sortedData, formatData);
+
+  return (
+    <>
+      <NScrollListHeader
+        titleKey="term.albums"
+        OptionsSheet={ArtistsViewOptionsSheet}
+      />
+      <NScrollListLayout
+        ListEmptyComponent={
+          <ContentPlaceholder
+            isPending={isPending}
+            errMsgKey="err.msg.noAlbums"
+          />
+        }
+        {...presets}
+      />
+    </>
+  );
 }
+
+//#region Utils
+type AlbumData = ExtractQueryData<typeof useAlbums>[number];
+
+const AlbumSortStrategies = {
+  name: null,
+  artistName: (a: AlbumData, b: AlbumData) =>
+    a.artistName.localeCompare(b.artistName),
+  duration: (a: AlbumData, b: AlbumData) => a.duration - b.duration,
+  trackCount: (a: AlbumData, b: AlbumData) => a.trackCount - b.trackCount,
+};
+
+function formatData({ id, name, artistName, artwork }: AlbumData) {
+  return { id, title: name, description: artistName, imageSource: artwork };
+}
+//#endregion
