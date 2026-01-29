37e5583d9e8679452c999d7c6190bfea5dfc673f
cyanChill <83375816+cyanChill@users.noreply.github.com>
2026-01-29 17:55:30 -0500

    chore: Fix export order

--- mobile/src/stores/ViewPreference/actions/setters.ts
+++ mobile/src/stores/ViewPreference/actions/setters.ts
@@ -2,13 +2,13 @@ import { viewPreferenceStore } from "../store";
 import type { LayoutOption, ScreenSortOptions } from "../constants";
 import type { MutableLayout, MutableOrder } from "../types";
 
+export function setLayout(screen: MutableLayout, layout: LayoutOption) {
+  viewPreferenceStore.setState({ [`${screen}Layout`]: layout });
+}
+
 export function setSortOrder<TScreen extends MutableOrder>(
   screen: TScreen,
   sortOrder: ScreenSortOptions<TScreen>,
 ) {
   viewPreferenceStore.setState({ [`${screen}Order`]: sortOrder });
-}
-
-export function setLayout(screen: MutableLayout, layout: LayoutOption) {
-  viewPreferenceStore.setState({ [`${screen}Layout`]: layout });
 }
