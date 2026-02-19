29b599a35a3cd5efd45bea72c10cbf8fbe084189
cyanChill <83375816+cyanChill@users.noreply.github.com>
2026-02-19 14:48:39 -0500

    chore: Fix `NScrollListLayout`
    - Required some type fixes for the Legend List refs.

--- mobile/src/components/Base/LegendList.tsx
+++ mobile/src/components/Base/LegendList.tsx
@@ -5,18 +5,22 @@ import type {
 import type { AnimatedLegendListProps } from "@legendapp/list/reanimated";
 import { AnimatedLegendList } from "@legendapp/list/reanimated";
 import { memo, useRef } from "react";
+import type { AnimatedRef } from "react-native-reanimated";
+import { useAnimatedRef } from "react-native-reanimated";
 import { withUniwind } from "uniwind";
 
-type LegendListSignature = <T>(
-  props: AnimatedLegendListProps<T> & { ref?: LegendListRef },
-) => React.JSX.Element;
+type LegendListSignature = <T>(props: LegendListProps<T>) => React.JSX.Element;
+
+type JoinedLegendListRef = LegendListRef | AnimatedLegendListRef;
 
 export type LegendListRef = React.RefObject<RawLegendListRef | null>;
+// @ts-expect-error - Argument should be compatible.
+export type AnimatedLegendListRef = AnimatedRef<RawLegendListRef>;
 
 export type LegendListProps<T = any> = Omit<
-  AnimatedLegendListProps<T>,
+  Omit<AnimatedLegendListProps<T>, "ref">,
   "data"
-> & { data?: readonly T[] };
+> & { ref?: JoinedLegendListRef; data?: readonly T[] };
 
 export type ListRenderItemInfo<T = any> = LegendListRenderItemProps<T>;
 
@@ -38,5 +42,8 @@ export const LegendList = memo(function LegendList(props) {
 }) as LegendListSignature;
 
 export function useLegendListRef() {
-  return useRef<LegendListRef>(null);
+  return useRef<RawLegendListRef>(null);
+}
+export function useAnimatedLegendListRef() {
+  return useAnimatedRef() as unknown as AnimatedLegendListRef;
 }
