e49928d08aca7df9751d532ba80e5b7b62295018
cyanChill <83375816+cyanChill@users.noreply.github.com>
2026-02-22 20:49:38 -0500

    chore: Swap to React Native's Pressable due to heavy rendering costs
    - It's really noticeable that the screens mount slower the more buttons from RNGH they have (ie: navigating to an album from the artists screen results in seeing a blank screen for a bit).

--- mobile/src/components/Base/Pressable.tsx
+++ mobile/src/components/Base/Pressable.tsx
@@ -1,6 +1,8 @@
+import type { PressableProps as RNPressableProps } from "react-native";
+import { Pressable as RNPresasble } from "react-native";
 import type { PressableProps as RNGHPressableProps } from "react-native-gesture-handler";
 import { RNGHPressable } from "uniwind";
 
-export type PressableProps = RNGHPressableProps;
+export type PressableProps = RNPressableProps;
 
-export const Pressable = RNGHPressable;
+export const Pressable = RNPresasble;
