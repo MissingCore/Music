4a5dc542cccc01f9cbaf1ed15ae69781ae84005d
cyanChill <83375816+cyanChill@users.noreply.github.com>
2026-03-20 19:14:05 -0400

    chore: Remove the old global toast provider
    - We'll keep the one in the sheet for now as we consider whether we want the toast to be visible on the sheet level.

--- mobile/src/navigation/providers/AppProvider.tsx
+++ mobile/src/navigation/providers/AppProvider.tsx
@@ -1,8 +1,5 @@
-import { Toasts } from "@backpackapp-io/react-native-toast";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { NavigationBar } from "@zoontek/react-native-navigation-bar";
-import { platformApiLevel } from "expo-device";
-import { useMemo } from "react";
 import { StatusBar, View } from "react-native";
 import { KeyboardProvider } from "react-native-keyboard-controller";
 import { Provider as PaperProvider } from "react-native-paper";
@@ -30,7 +27,6 @@ export function AppProvider(props: { children: React.ReactNode }) {
               <SystemBars />
               <ListenerStateStoreProvider />
               <ChildrenWrapper {...props} />
-              <ToastProvider />
               <Toaster />
             </QueryClientProvider>
           </GestureHandlerRootView>
@@ -61,21 +57,5 @@ function ChildrenWrapper(props: { children: React.ReactNode }) {
       {...props}
     />
   );
-}
-//#endregion
-
-//#region Toast Provider
-function ToastProvider() {
-  const insets = useSafeAreaInsets();
-
-  // Need to provide some extra insets for the global toast provider due
-  // to Android API 35+ changing what we get for "height" from `useWindowDimensions()`.
-  //  - https://github.com/facebook/react-native/issues/47080#issuecomment-2421914957
-  const extraInsets = useMemo(() => {
-    if (!platformApiLevel || platformApiLevel < 35) return undefined;
-    return { bottom: insets.top + insets.bottom };
-  }, [insets]);
-
-  return <Toasts extraInsets={extraInsets} />;
 }
 //#endregion
