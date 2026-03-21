893d1c35832488938254869d548a2c7a068d8895
cyanChill <83375816+cyanChill@users.noreply.github.com>
2026-03-20 22:20:28 -0400

    chore: Add dismiss gesture support
    - It's based off the drag-to-dismiss code for the miniplayer.
    - It might look slightly weird if the drag gesture isn't enough and the 4s timer triggers (the toast might disappear).

--- mobile/src/components/Toast/Toaster.tsx
+++ mobile/src/components/Toast/Toaster.tsx
@@ -1,8 +1,10 @@
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 import { useWindowDimensions } from "react-native";
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
 import Animated, {
   useAnimatedStyle,
   useSharedValue,
+  withSpring,
   withTiming,
 } from "react-native-reanimated";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
@@ -35,7 +37,36 @@ function ToastItem({ toast, exiting }: { toast: Toast; exiting?: boolean }) {
   const windowDimensions = useWindowDimensions();
   const onRemove = useToastStore((s) => s.shiftToast);
   const animationState = useSharedValue(0);
+  const toastHeight = useSharedValue(0);
 
+  const topOffset = insets.top + 16;
+
+  //#region Dismiss Gesture
+  const panAmount = useSharedValue(0);
+
+  const panGesture = useMemo(
+    () =>
+      Gesture.Pan()
+        .enabled(!exiting)
+        .activeOffsetY([-10, 10])
+        .onUpdate(({ translationY }) => {
+          panAmount.value = Math.min(0, translationY);
+        })
+        .onEnd(({ velocityY }) => {
+          const metThreshold = velocityY < -1000;
+          panAmount.value = withSpring(
+            metThreshold ? -(topOffset + toastHeight.value + 256) : 0,
+            undefined,
+            (finished) => {
+              if (finished && metThreshold) scheduleOnRN(onRemove);
+            },
+          );
+        }),
+    [panAmount, toastHeight, topOffset, exiting, onRemove],
+  );
+  //#endregion
+
+  //#region Enter/Exit Animations
   useEffect(() => {
     animationState.value = withTiming(
       exiting ? 0 : 1,
@@ -61,30 +92,41 @@ function ToastItem({ toast, exiting }: { toast: Toast; exiting?: boolean }) {
   }, [animationState, exiting, onRemove, toast.autoDismiss]);
 
   const toastStyles = useAnimatedStyle(() => ({
-    top: insets.top + 16,
+    top: topOffset,
     maxWidth: Math.min(384, windowDimensions.width - 32),
     opacity: animationState.value,
     transform: [
       { translateX: "-50%" },
-      { translateY: `${-100 + animationState.value * 100}%` },
+      {
+        translateY:
+          panAmount.value < 0
+            ? panAmount.value
+            : `${-100 + animationState.value * 100}%`,
+      },
     ],
   }));
+  //#endregion
 
   return (
-    <Animated.View
-      style={toastStyles}
-      className={cn(
-        "absolute left-1/2 min-h-6 items-center justify-center rounded-sm border border-surfaceContainerHigh bg-surfaceContainerLowest p-2",
-        { "border-error bg-error": toast.type === "error" },
-      )}
-    >
-      <StyledText
-        className={cn("text-center text-sm", {
-          "text-onError": toast.type === "error",
-        })}
+    <GestureDetector gesture={panGesture}>
+      <Animated.View
+        onLayout={(e) => {
+          toastHeight.value = e.nativeEvent.layout.height;
+        }}
+        style={toastStyles}
+        className={cn(
+          "absolute left-1/2 min-h-6 items-center justify-center rounded-sm border border-surfaceContainerHigh bg-surfaceContainerLowest p-2",
+          { "border-error bg-error": toast.type === "error" },
+        )}
       >
-        {toast.message}
-      </StyledText>
-    </Animated.View>
+        <StyledText
+          className={cn("text-center text-sm", {
+            "text-onError": toast.type === "error",
+          })}
+        >
+          {toast.message}
+        </StyledText>
+      </Animated.View>
+    </GestureDetector>
   );
 }
