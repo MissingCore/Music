80194195c81bc3df597e8036d70ef8f6b22dc60f
cyanChill <83375816+cyanChill@users.noreply.github.com>
2026-03-25 18:48:25 -0400

    chore: Export new native util to save a bundled resource to the local file system
    - Based on the `expo-file-system/legacy` code for `copyAsync`, which is deprecated.
    - Will only work in release builds as during debug build, we get a http link.

--- mobile/modules/native-utils/src/NativeUtilsModule.ts
+++ mobile/modules/native-utils/src/NativeUtilsModule.ts
@@ -3,6 +3,7 @@ import { NativeModule, requireNativeModule } from "expo";
 declare class NativeUtilsModule extends NativeModule {
   isSystemDarkMode: boolean;
   launchAppViaIntent(): void;
+  saveBundledAssetToURI(assetName: string, toUri: string): Promise<void>;
 }
 
 const nativeModule = requireNativeModule<NativeUtilsModule>("NativeUtils");
@@ -11,4 +12,13 @@ export const isSystemDarkMode = nativeModule.isSystemDarkMode;
 
 export function launchAppViaIntent() {
   return nativeModule.launchAppViaIntent();
+}
+
+/**
+ * Save asset obtained via `require()` to specified URI.
+ * - **Note:** Only works in release build.
+ */
+export async function saveBundledAssetToURI(assetName: string, toUri: string) {
+  if (__DEV__) return;
+  return nativeModule.saveBundledAssetToURI(assetName, toUri);
 }
