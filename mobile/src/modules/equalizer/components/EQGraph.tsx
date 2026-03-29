74e72a26c3eac0a3839eac266cc4b99517a8121d
cyanChill <83375816+cyanChill@users.noreply.github.com>
2026-03-29 18:10:39 -0400

    refactor: Start conversion to using `react-native-svg`
    - Currently only converted the "annotations".
    - `react-native-skia` adds `~11.5 MB` to the final app size, which for only being used in a single location, doesn't seem worth it.
    - We can probably do this through SVGs as that's what `react-native-gifted-charts` does.

--- mobile/src/modules/equalizer/components/EQGraph.tsx
+++ mobile/src/modules/equalizer/components/EQGraph.tsx
@@ -1,12 +1,7 @@
-import {
-  Canvas,
-  Circle,
-  LinearGradient,
-  Path,
-  Skia,
-} from "@shopify/react-native-skia";
-import { useMemo } from "react";
+import { Circle, LinearGradient, Path, Skia } from "@shopify/react-native-skia";
+import { Fragment, useMemo } from "react";
 import { View, useWindowDimensions } from "react-native";
+import { Svg, Path as SPath } from "react-native-svg";
 
 import { useTheme } from "~/hooks/useTheme";
 
@@ -24,178 +19,150 @@ export function EQGraph(props: EQGraphProps) {
   return (
     <View
       style={{ height: GraphHeight }}
-      className="relative w-full rounded-xl bg-surfaceContainerLowest"
+      className="relative mb-4 w-full rounded-xl bg-surfaceContainerLowest"
     >
-      <Canvas style={{ height: "100%", width: "100%" }}>
-        <XAxisPath />
-        <FixedFrequencyLabelPaths />
+      <Svg style={{ height: "100%", width: "100%" }}>
+        <GraphAnnotations />
+      </Svg>
+
+      {/* <Canvas style={{ height: "100%", width: "100%" }}>
         <EQLine bound={props.bound} points={props.points} />
-      </Canvas>
-      <FixedFrequencyLabels />
+      </Canvas> */}
     </View>
   );
 }
 
-//#region Dynamic Components
-interface EQLineProps {
-  bound: number;
-  points: Array<{ x: number; y: number }>;
-}
+// //#region Dynamic Components
+// interface EQLineProps {
+//   bound: number;
+//   points: Array<{ x: number; y: number }>;
+// }
 
-function EQLine(props: EQLineProps) {
-  const width = useGraphWidth();
-  const { onSurfaceVariant, surfaceContainerHighest } = useTheme();
+// function EQLine(props: EQLineProps) {
+//   const width = useGraphWidth();
+//   const { onSurfaceVariant, surfaceContainerHighest } = useTheme();
 
-  const points = useMemo(
-    () =>
-      props.points.map(
-        ({ x, y }) =>
-          [
-            guessXPercentage(x) * width,
-            guessYCoordinate(y, props.bound),
-          ] as const,
-      ),
-    [width, props.points, props.bound],
-  );
+//   const points = useMemo(
+//     () =>
+//       props.points.map(
+//         ({ x, y }) =>
+//           [
+//             guessXPercentage(x) * width,
+//             guessYCoordinate(y, props.bound),
+//           ] as const,
+//       ),
+//     [width, props.points, props.bound],
+//   );
 
-  const path = useMemo(() => {
-    const line = Skia.Path.Make().moveTo(0, XAxisYPos);
-    points.forEach(([x, y]) => line.lineTo(x, y));
-    line.lineTo(width, XAxisYPos).close();
-    return line;
-  }, [width, points]);
+//   const path = useMemo(() => {
+//     const line = Skia.Path.Make().moveTo(0, XAxisYPos);
+//     points.forEach(([x, y]) => line.lineTo(x, y));
+//     line.lineTo(width, XAxisYPos).close();
+//     return line;
+//   }, [width, points]);
 
-  const minY = useMemo(() => Math.min(...points.map(([_, y]) => y)), [points]);
-  const maxY = useMemo(() => Math.max(...points.map(([_, y]) => y)), [points]);
+//   const minY = useMemo(() => Math.min(...points.map(([_, y]) => y)), [points]);
+//   const maxY = useMemo(() => Math.max(...points.map(([_, y]) => y)), [points]);
 
-  return (
-    <>
-      {minY < XAxisYPos ? (
-        <Path path={path} color={surfaceContainerHighest} style="fill">
-          <LinearGradient
-            start={{ x: width / 2, y: minY }}
-            end={{ x: width / 2, y: XAxisYPos }}
-            colors={[surfaceContainerHighest, `${surfaceContainerHighest}0D`]}
-          />
-        </Path>
-      ) : null}
-      {maxY > XAxisYPos ? (
-        <Path path={path} color={surfaceContainerHighest} style="fill">
-          <LinearGradient
-            start={{ x: width / 2, y: XAxisYPos }}
-            end={{ x: width / 2, y: maxY }}
-            colors={[`${surfaceContainerHighest}0D`, surfaceContainerHighest]}
-          />
-        </Path>
-      ) : null}
-      {points.map(([x, y], index) => (
-        <Circle key={index} cx={x} cy={y} r={2} color={onSurfaceVariant} />
-      ))}
-    </>
-  );
-}
+//   return (
+//     <>
+//       {minY < XAxisYPos ? (
+//         <Path path={path} color={surfaceContainerHighest} style="fill">
+//           <LinearGradient
+//             start={{ x: width / 2, y: minY }}
+//             end={{ x: width / 2, y: XAxisYPos }}
+//             colors={[surfaceContainerHighest, `${surfaceContainerHighest}0D`]}
+//           />
+//         </Path>
+//       ) : null}
+//       {maxY > XAxisYPos ? (
+//         <Path path={path} color={surfaceContainerHighest} style="fill">
+//           <LinearGradient
+//             start={{ x: width / 2, y: XAxisYPos }}
+//             end={{ x: width / 2, y: maxY }}
+//             colors={[`${surfaceContainerHighest}0D`, surfaceContainerHighest]}
+//           />
+//         </Path>
+//       ) : null}
+//       {points.map(([x, y], index) => (
+//         <Circle key={index} cx={x} cy={y} r={2} color={onSurfaceVariant} />
+//       ))}
+//     </>
+//   );
+// }
 
-/**
- * Guess how far from the left the point should be positioned, relative
- * to the fixed frequency bands.
- */
-function guessXPercentage(x: number): number {
-  const upperBoundIdx = DisplayedFrequencies.findIndex((f) => x < f.value);
-  if (upperBoundIdx === -1) throw new Error("Frequency Band not supported.");
-  const lowerBound =
-    upperBoundIdx === 0
-      ? { value: 0, label: "0", percentage: 0 }
-      : DisplayedFrequencies[upperBoundIdx - 1]!;
-  const upperBound = DisplayedFrequencies[upperBoundIdx]!;
+// /**
+//  * Guess how far from the left the point should be positioned, relative
+//  * to the fixed frequency bands.
+//  */
+// function guessXPercentage(x: number): number {
+//   const upperBoundIdx = DisplayedFrequencies.findIndex((f) => x < f.value);
+//   if (upperBoundIdx === -1) throw new Error("Frequency Band not supported.");
+//   const lowerBound =
+//     upperBoundIdx === 0
+//       ? { value: 0, label: "0", percentage: 0 }
+//       : DisplayedFrequencies[upperBoundIdx - 1]!;
+//   const upperBound = DisplayedFrequencies[upperBoundIdx]!;
 
-  const workingPercentageArea = upperBound.percentage - lowerBound.percentage;
-  const xPercentageInRange =
-    (x - lowerBound.value) / (upperBound.value - lowerBound.value);
+//   const workingPercentageArea = upperBound.percentage - lowerBound.percentage;
+//   const xPercentageInRange =
+//     (x - lowerBound.value) / (upperBound.value - lowerBound.value);
 
-  return lowerBound.percentage + workingPercentageArea * xPercentageInRange;
-}
+//   return lowerBound.percentage + workingPercentageArea * xPercentageInRange;
+// }
 
-function guessYCoordinate(y: number, bound: number): number {
-  if (y === 0) return XAxisYPos;
-  else if (y > 0) {
-    return ((bound - y) / bound) * BufferedHeightRange + VerticalBuffer;
-  }
-  return Math.abs(y / bound) * BufferedHeightRange + XAxisYPos;
-}
-//#endregion
+// function guessYCoordinate(y: number, bound: number): number {
+//   if (y === 0) return XAxisYPos;
+//   else if (y > 0) {
+//     return ((bound - y) / bound) * BufferedHeightRange + VerticalBuffer;
+//   }
+//   return Math.abs(y / bound) * BufferedHeightRange + XAxisYPos;
+// }
+// //#endregion
 
-//#region "Fixed" Components
-function XAxisPath() {
-  const width = useGraphWidth();
-  const { surfaceContainerHighest } = useTheme();
-
-  const path = useMemo(
-    () =>
-      Skia.Path.Make().moveTo(0, XAxisYPos).lineTo(width, XAxisYPos).close(),
-    [width],
-  );
-
-  return <Path path={path} color={surfaceContainerHighest} style="stroke" />;
-}
-
+//#region Graph Annotations
 const DisplayedFrequencies = [
-  { value: 20000, label: "20", percentage: 0.05 },
-  { value: 50000, label: "50", percentage: 0.18 },
-  { value: 100000, label: "100", percentage: 0.26 },
-  { value: 500000, label: "500", percentage: 0.47 },
-  { value: 1000000, label: "1K", percentage: 0.56 },
-  { value: 5000000, label: "5K", percentage: 0.76 },
-  { value: 10000000, label: "10K", percentage: 0.85 },
-  { value: 20000000, label: "20K", percentage: 0.95 },
+  { label: "20", value: 20000, xPosPercent: 0.05 },
+  { label: "50", value: 50000, xPosPercent: 0.18 },
+  { label: "100", value: 100000, xPosPercent: 0.26 },
+  { label: "500", value: 500000, xPosPercent: 0.47 },
+  { label: "1K", value: 1000000, xPosPercent: 0.56 },
+  { label: "5K", value: 5000000, xPosPercent: 0.76 },
+  { label: "10K", value: 10000000, xPosPercent: 0.85 },
+  { label: "20K", value: 20000000, xPosPercent: 0.95 },
 ];
 
-function FixedFrequencyLabelPaths() {
+/** Draws x-axis and tick marks + labels for certain frequencies. */
+function GraphAnnotations() {
   const width = useGraphWidth();
   const { surfaceContainerHighest } = useTheme();
-
-  const paths = useMemo(
-    () =>
-      DisplayedFrequencies.map(({ percentage }) => {
-        const xPos = width * percentage;
-        return Skia.Path.Make()
-          .moveTo(xPos, 0)
-          .lineTo(xPos, GraphHeight)
-          .close();
-      }),
-    [width],
+  return (
+    <>
+      {/* X-axis */}
+      <SPath
+        d={`M ${0} ${XAxisYPos} L ${width} ${XAxisYPos}`}
+        stroke={surfaceContainerHighest}
+      />
+      {/* Frequency Tick Marks + Labels */}
+      {DisplayedFrequencies.map(({ label, xPosPercent }) => {
+        const xPos = width * xPosPercent;
+        return (
+          <Fragment key={label}>
+            <SPath
+              d={`M ${xPos} ${0} L ${xPos} ${GraphHeight}`}
+              stroke={surfaceContainerHighest}
+            />
+            <Em
+              style={{ left: xPos, fontSize: 8 }}
+              className="absolute top-full left-0 -translate-x-1/2 translate-y-0.5"
+            >
+              {label}
+            </Em>
+          </Fragment>
+        );
+      })}
+    </>
   );
-
-  return paths.map((path, index) => (
-    <Path
-      key={index}
-      path={path}
-      color={surfaceContainerHighest}
-      style="stroke"
-    />
-  ));
-}
-
-function FixedFrequencyLabels() {
-  const width = useGraphWidth();
-
-  const labelCoordinates = useMemo(
-    () =>
-      DisplayedFrequencies.map(({ label, percentage }) => {
-        const xPos = width * percentage;
-        return { label, xPos };
-      }),
-    [width],
-  );
-
-  return labelCoordinates.map(({ label, xPos }) => (
-    <Em
-      key={label}
-      style={{ left: xPos, fontSize: 8 }}
-      className="absolute top-full left-0 -translate-x-1/2 translate-y-0.5"
-    >
-      {label}
-    </Em>
-  ));
 }
 //#endregion
 
