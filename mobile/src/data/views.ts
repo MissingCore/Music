18bec843f801c1a5e0c23f7e28bad684630f86b5
cyanChill <83375816+cyanChill@users.noreply.github.com>
2026-04-03 19:07:09 -0400

    chore: "View" where the `tracks` schema gets merged w/ albums & artists
    - Main benefit is deriving the correct `artwork` field & having the `artists` field already on the object (though needs to be parsed).
    - This also moves all the manaul `sql` operations in one place.

--- mobile/src/data/views.ts
+++ mobile/src/data/views.ts
@@ -1,5 +1,7 @@
+import { eq, getTableColumns, sql } from "drizzle-orm";
+
 import { db } from "~/db";
-import { tracksToArtists } from "~/db/schema";
+import { albums, tracks, tracksToArtists } from "~/db/schema";
 
 import { iAsc } from "~/lib/drizzle";
 
@@ -13,4 +15,38 @@ export function getOrderedTrackArtistsView() {
     .from(tracksToArtists)
     .orderBy(iAsc(tracksToArtists.artistName))
     .as("ordered_track_artists_view");
+}
+
+const { artwork: _, ...trackColumns } = getTableColumns(tracks);
+
+/**
+ * Join main relations (album & artists) onto `tracks` table.
+ *  - `artwork` contains the appropriate artwork (from track or album).
+ *  - `artists` contains a JSON-stringified array (`string[]` or `[null]`).
+ */
+export function getStructuredTracksView() {
+  const orderedTrackArtists = getOrderedTrackArtistsView();
+  return db
+    .select({
+      ...trackColumns,
+      artwork: sql<
+        string | null
+      >`coalesce(${tracks.artwork}, ${albums.artwork})`.as("derived_artwork"),
+      albumName: albums.name,
+      albumArtistsKey: albums.artistsKey,
+      artistsName:
+        sql<string>`GROUP_CONCAT(${orderedTrackArtists.artistName}, ', ')`.as(
+          "joined_artists_name",
+        ),
+      /** We need to unencode these fields. */
+      artists:
+        sql<string>`json_group_array(${orderedTrackArtists.artistName})`.as(
+          "derived_artists",
+        ),
+    })
+    .from(tracks)
+    .leftJoin(albums, eq(tracks.albumId, albums.id))
+    .leftJoin(orderedTrackArtists, eq(tracks.id, orderedTrackArtists.trackId))
+    .groupBy(tracks.id)
+    .as("structured_tracks_view");
 }
