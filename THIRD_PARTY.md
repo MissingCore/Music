04e3419e7b01320654102b6a4849d9b1a2f1279e
cyanChill <83375816+cyanChill@users.noreply.github.com>
2026-06-14 22:33:53 -0400

    chore: Remove `expo-media-library`

--- THIRD_PARTY.md
+++ THIRD_PARTY.md
@@ -24,7 +24,6 @@
 | expo-image-manipulator | MIT | https://github.com/expo/expo/tree/main/packages/expo-image-manipulator |
 | expo-image-picker | MIT | https://github.com/expo/expo/tree/main/packages/expo-image-picker |
 | expo-linear-gradient | MIT | https://github.com/expo/expo/tree/main/packages/expo-linear-gradient |
-| expo-media-library | MIT | https://github.com/expo/expo/tree/main/packages/expo-media-library |
 | expo-sqlite | MIT | https://github.com/expo/expo/tree/main/packages/expo-sqlite |
 | Geist Mono Font | OFL-1.1 | https://github.com/vercel/geist-font |
 | i18next | MIT | https://github.com/i18next/i18next |
--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -2,7 +2,6 @@ import navigationBarPlugin from "@zoontek/react-native-navigation-bar/expo";
 import type { ExpoConfig } from "expo/config";
 import fontPlugin from "expo-font/plugin";
 import imagePickerPlugin from "expo-image-picker/plugin";
-import mediaLibraryPlugin from "expo-media-library/plugin";
 import type { WithAndroidWidgetsParams } from "react-native-android-widget";
 import bootsplashPlugin from "react-native-bootsplash/expo";
 
@@ -47,8 +46,6 @@ export default (): ExpoConfig => {
         "android.permission.VIBRATE",
         // From `expo-image-picker`.
         "android.permission.CAMERA",
-        // From `expo-media-library`.
-        "android.permission.READ_MEDIA_VISUAL_USER_SELECTED",
       ],
       intentFilters: [
         // Whenever we rebuild the `AndroidManifest.xml` file, we should delete
@@ -99,7 +96,6 @@ export default (): ExpoConfig => {
         ],
       }),
       imagePickerPlugin({ microphonePermission: false }),
-      mediaLibraryPlugin({ granularPermissions: ["audio"] }),
       bootsplashPlugin({
         logo: `./assets/${BUILD_THEME}/splash-icon.png`,
         background: BACKGROUND_COLOR,
