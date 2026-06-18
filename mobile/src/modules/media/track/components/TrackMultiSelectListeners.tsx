2d737371f8aca31e8e15fbf8b5db353b5b17de7d
cyanChill <83375816+cyanChill@users.noreply.github.com>
2026-06-18 00:25:10 -0400

    chore: Mechanism for displaying actions for selected tracks

--- mobile/src/modules/media/track/components/TrackMultiSelectListeners.tsx
+++ mobile/src/modules/media/track/components/TrackMultiSelectListeners.tsx
@@ -1,7 +1,13 @@
 import { useNavigation } from "@react-navigation/native";
+import { Portal } from "@rn-primitives/portal";
 import { useEffect } from "react";
+import Animated, { SlideInUp, SlideOutUp } from "react-native-reanimated";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
+import { useTrackMultiSelectStore } from "../core/store";
 import { resetTrackMultiSelect } from "../core/actions";
+
+import { TopDownGradient } from "~/components/Gradient";
 
 export function TrackMultiSelectListeners() {
   const navigation = useNavigation();
@@ -11,5 +17,28 @@ export function TrackMultiSelectListeners() {
     return () => unsubscribe();
   }, [navigation]);
 
-  return null;
+  return <TrackMultiSelectMenu />;
+}
+
+const ESTIMATED_MENU_HEIGHT = 100;
+
+export function TrackMultiSelectMenu() {
+  const insets = useSafeAreaInsets();
+  const isMultiSelectEnabled = useTrackMultiSelectStore((s) => s.enabled);
+
+  return isMultiSelectEnabled ? (
+    <Portal name="track-multi-select-menu-portal">
+      <Animated.View
+        entering={SlideInUp}
+        exiting={SlideOutUp}
+        className="absolute top-0 right-0 left-0"
+      >
+        <TopDownGradient
+          height={ESTIMATED_MENU_HEIGHT + insets.top}
+          startFrom={insets.top}
+          color="primary"
+        />
+      </Animated.View>
+    </Portal>
+  ) : null;
 }
