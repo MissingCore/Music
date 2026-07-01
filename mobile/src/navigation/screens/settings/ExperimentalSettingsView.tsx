81e8a252487441e6021e74fc42938733b60c148c
cyanChill <83375816+cyanChill@users.noreply.github.com>
2026-06-30 21:40:01 -0400

    chore: Improve "Delete All Stored Images" feature
    - It's renamed to "Delete ${unhashedImagesCount} Unhashed Images" and displays the number of unhashed images.
    - The setting disappears if we have no unhashed images.
    - We now only delete unhashed images (in the situation where the user uses this feature long after it's introduced).

--- mobile/src/navigation/screens/settings/ExperimentalSettingsView.tsx
+++ mobile/src/navigation/screens/settings/ExperimentalSettingsView.tsx
@@ -2,6 +2,8 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 
 import { toast } from "@missingcore/ui/toast";
+import { useQuery } from "@tanstack/react-query";
+import { eq, like } from "drizzle-orm";
 import { Directory } from "expo-file-system";
 import { useTranslation } from "react-i18next";
 
@@ -23,6 +25,7 @@ import { sessionStore } from "~/stores/Session/store";
 import { ListLayout } from "~/navigation/layouts/ListLayout";
 
 import { ImageDirectory } from "~/lib/file-system";
+import { queryClient } from "~/lib/react-query";
 import { Links, openLink } from "~/lib/web-browser";
 import { SegmentedList } from "~/components/List/Segmented";
 import { ConfirmableAction } from "~/components/Modal";
@@ -34,6 +37,7 @@ export default function ExperimentalSettings() {
   const downsamplingProcessor = usePreferenceStore(
     (s) => s.downsamplingProcessor,
   );
+  const { data: unhashedImagesCount } = useUnhashedImagesCount();
 
   return (
     <ListLayout>
@@ -67,21 +71,22 @@ export default function ExperimentalSettings() {
         RightElement={<Icon name="open-in-new" />}
       />
 
-      <ConfirmableAction
-        Component={SegmentedList.Item}
-        componentProps={{
-          labelText: "Delete All Stored Images",
-          supportingText:
-            "Delete all stored images to switch to the new hashed artwork strategy, which should make disabling the `Optimized Image Saving` feature less impactful (ie: using more storage from saving the same artwork over and over again).",
-          onPress: deleteAllImages,
-        }}
-        modalMessage={[
-          // @ts-expect-error - If we use a non-translation key, it'll be rendered as a string.
-          "Re-launching the app is necessary after confirming this action.",
-          // @ts-expect-error - If we use a non-translation key, it'll be rendered as a string.
-          "You will need to re-add any images you manually assigned to albums/artists/genres/playlists/tracks.",
-        ]}
-      />
+      {unhashedImagesCount !== undefined && unhashedImagesCount !== 0 ? (
+        <ConfirmableAction
+          Component={SegmentedList.Item}
+          componentProps={{
+            labelText: `Delete ${unhashedImagesCount} Unhashed Images`,
+            supportingText: `Delete ${unhashedImagesCount} unhashed images to switch to the new hashed artwork strategy, which should make disabling the \`Optimized Image Saving\` feature less impactful (ie: using more storage from saving the same artwork over and over again).`,
+            onPress: deleteAllUnhashedImages,
+          }}
+          modalMessage={[
+            // @ts-expect-error - If we use a non-translation key, it'll be rendered as a string.
+            "Re-launching the app is necessary after confirming this action.",
+            // @ts-expect-error - If we use a non-translation key, it'll be rendered as a string.
+            "You will need to re-add any images you manually assigned to albums/artists/genres/playlists/tracks.",
+          ]}
+        />
+      ) : null}
     </ListLayout>
   );
 }
@@ -94,22 +99,66 @@ async function purgeWaveformCache() {
   toast.t("feat.waveformSlider.extra.purgeCacheToast");
 }
 
-/** Delete all images from the database, setting `fetchedArt` to `false`. */
-async function deleteAllImages() {
-  const dir = new Directory(ImageDirectory);
-  dir.delete();
+const queryKey = ["has-unhashed-images"];
 
-  // eslint-disable-next-line drizzle/enforce-update-with-where
-  await db.update(artists).set({ artwork: null });
-  // eslint-disable-next-line drizzle/enforce-update-with-where
-  await db.update(albums).set({ embeddedArtwork: null, altArtwork: null });
-  // eslint-disable-next-line drizzle/enforce-update-with-where
+async function getUnhashedImagesCount() {
+  const knownHashedImages = await db.query.hashedImages.findMany();
+  const dir = new Directory(ImageDirectory);
+  return dir.list().length - knownHashedImages.length;
+}
+
+function useUnhashedImagesCount() {
+  return useQuery({ queryKey, queryFn: getUnhashedImagesCount });
+}
+
+/** Delete all unhashed images from the database, setting `fetchedArt` to `false`. */
+async function deleteAllUnhashedImages() {
+  //? Remove all unhashed images in the database (basically any artwork
+  //? field with a value starting with `file://`).
+  await db
+    .update(artists)
+    .set({ artwork: null })
+    .where(like(artists.artwork, "file://%"));
+  await db
+    .update(genres)
+    .set({ artwork: null })
+    .where(like(genres.artwork, "file://%"));
+  await db
+    .update(playlists)
+    .set({ artwork: null })
+    .where(like(playlists.artwork, "file://%"));
+  await db
+    .update(albums)
+    .set({ embeddedArtwork: null })
+    .where(like(albums.embeddedArtwork, "file://%"));
+  await db
+    .update(albums)
+    .set({ altArtwork: null })
+    .where(like(albums.altArtwork, "file://%"));
   await db
     .update(tracks)
-    .set({ embeddedArtwork: null, altArtwork: null, fetchedArt: false });
-  // eslint-disable-next-line drizzle/enforce-update-with-where
-  await db.update(playlists).set({ artwork: null });
-  // eslint-disable-next-line drizzle/enforce-update-with-where
-  await db.update(genres).set({ artwork: null });
+    .set({ embeddedArtwork: null })
+    .where(like(tracks.embeddedArtwork, "file://%"));
+  await db
+    .update(tracks)
+    .set({ altArtwork: null })
+    .where(like(tracks.altArtwork, "file://%"));
+  await db
+    .update(tracks)
+    .set({ fetchedArt: false })
+    .where(eq(tracks.fetchedArt, true));
+
+  //? Delete all images that aren't hashed.
+  const knownHashedImages = await db.query.hashedImages.findMany();
+  const hashedImageURIs = new Set(knownHashedImages.map(({ uri }) => uri));
+
+  const dir = new Directory(ImageDirectory);
+  for (const file of dir.list()) {
+    // Only images should be stored in this directory.
+    if (hashedImageURIs.has(file.uri)) continue;
+    file.delete();
+  }
+
+  await queryClient.resetQueries({ queryKey });
 }
 //#endregion
