e4c75f2ab01ccfa821331e8fa5191667ec52977a
cyanChill <83375816+cyanChill@users.noreply.github.com>
2026-07-11 14:53:26 -0400

    chore: Switch to using the music glyph SVG in widget
    - It's colored based on the `textColor` config.

--- mobile/src/modules/widget/components/WidgetArtwork.tsx
+++ mobile/src/modules/widget/components/WidgetArtwork.tsx
@@ -1,26 +1,40 @@
 // Copyright (C) 2024 - present, MissingCore
 // SPDX-License-Identifier: AGPL-3.0-only
 
-import type { ClickActionProps } from "react-native-android-widget";
+import type { ClickActionProps, HexColor } from "react-native-android-widget";
 import { ImageWidget } from "react-native-android-widget";
 
 import { getImageUri } from "~/lib/file-system";
+import { WidgetSVG } from "./WidgetSVG";
 
 /** Displays image with placeholder fallback. */
 export function WidgetArtwork({
   size,
   artwork,
+  placeholderColor,
   ...props
 }: ClickActionProps & {
   size: number;
-  artwork: string | null;
+  artwork?: string | null;
+  placeholderColor: HexColor;
 }) {
+  const imgUri = getImageUri(artwork);
   const imageSize = !artwork ? (size * 5) / 6 : size;
+
+  if (!imgUri) {
+    return (
+      <WidgetSVG
+        name="placeholder"
+        size={imageSize}
+        color={placeholderColor}
+        {...props}
+      />
+    );
+  }
   return (
     <ImageWidget
-      image={
-        getImageUri(artwork) ?? require("~/resources/images/music-glyph.png")
-      }
+      // @ts-expect-error - We pass down a `file://` URI.
+      image={imgUri}
       imageHeight={imageSize}
       imageWidth={imageSize}
       {...props}
--- mobile/src/modules/widget/components/WidgetSVG.tsx
+++ mobile/src/modules/widget/components/WidgetSVG.tsx
@@ -25,6 +25,44 @@ const SVGMap: Record<string, (color: HexColor) => string> = {
       <path d="M652.15-303.04v-354.2q0-11.35 8.23-19.58 8.22-8.22 19.77-8.22 11.54 0 19.75 8.24 8.21 8.23 8.21 19.65v354.11q0 11.63-8.24 19.86-8.23 8.22-19.65 8.22-11.64 0-19.86-8.22-8.21-8.23-8.21-19.86Zm-400.26-36.08v-281.87q0-15.35 10.27-24.99 10.26-9.63 23.96-9.63 4.8 0 9.57 1.25 4.77 1.25 9.2 4.36l211.52 141.66q7.78 5.15 11.55 12.57 3.77 7.42 3.77 15.81 0 8.38-3.77 15.77-3.77 7.38-11.55 12.52L304.89-310.19q-4.43 3.3-9.2 4.46-4.77 1.15-9.57 1.15-13.7 0-23.96-9.55-10.27-9.54-10.27-24.99Z" />
     </svg>
   `,
+  placeholder: (color) => `
+    <svg width="450" height="450" viewBox="0 0 450 450" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="312" cy="347" r="15" fill="${color}" />
+      <circle cx="277" cy="347" r="15" fill="${color}" />
+      <circle cx="137" cy="347" r="15" fill="${color}" />
+      <circle cx="102" cy="347" r="15" fill="${color}" />
+      <circle cx="347" cy="312" r="15" fill="${color}" />
+      <circle cx="312" cy="312" r="15" fill="${color}" />
+      <circle cx="277" cy="312" r="15" fill="${color}" />
+      <circle cx="172" cy="312" r="15" fill="${color}" />
+      <circle cx="137" cy="312" r="15" fill="${color}" />
+      <circle cx="102" cy="312" r="15" fill="${color}" />
+      <circle cx="347" cy="277" r="15" fill="${color}" />
+      <circle cx="172" cy="277" r="15" fill="${color}" />
+      <circle cx="347" cy="242" r="15" fill="${color}" />
+      <circle cx="172" cy="242" r="15" fill="${color}" />
+      <circle cx="347" cy="207" r="15" fill="${color}" />
+      <circle cx="172" cy="207" r="15" fill="${color}" />
+      <circle cx="347" cy="172" r="15" fill="${color}" />
+      <circle cx="312" cy="172" r="15" fill="${color}" />
+      <circle cx="277" cy="172" r="15" fill="${color}" />
+      <circle cx="242" cy="172" r="15" fill="${color}" />
+      <circle cx="207" cy="172" r="15" fill="${color}" />
+      <circle cx="172" cy="172" r="15" fill="${color}" />
+      <circle cx="347" cy="137" r="15" fill="${color}" />
+      <circle cx="312" cy="137" r="15" fill="${color}" />
+      <circle cx="277" cy="137" r="15" fill="${color}" />
+      <circle cx="242" cy="137" r="15" fill="${color}" />
+      <circle cx="207" cy="137" r="15" fill="${color}" />
+      <circle cx="172" cy="137" r="15" fill="${color}" />
+      <circle cx="347" cy="102" r="15" fill="${color}" />
+      <circle cx="312" cy="102" r="15" fill="${color}" />
+      <circle cx="277" cy="102" r="15" fill="${color}" />
+      <circle cx="242" cy="102" r="15" fill="${color}" />
+      <circle cx="207" cy="102" r="15" fill="${color}" />
+      <circle cx="172" cy="102" r="15" fill="${color}" />
+    </svg>
+  `,
 } as const;
 
 export type WidgetSVGName = keyof typeof SVGMap;
