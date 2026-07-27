151ca2d8628c38c3009dd9722fa7fd11157fc331
cyanChill <83375816+cyanChill@users.noreply.github.com>
2026-07-26 22:07:13 -0400

    chore: Fix comment

--- mobile/src/modules/i18n/core.ts
+++ mobile/src/modules/i18n/core.ts
@@ -6,10 +6,10 @@ import { useCallback } from "react";
 import { useTranslation } from "react-i18next";
 
 /**
- * Suports regular strings & translation keys. Translation keys will auto-complete.
+ * Supports regular strings & translation keys. Translation keys will auto-complete.
  *
  * To check for removed translation keys, we can temporarily remove `string & {}`
- * and then run the typecheck script.
+ * and then run the `typecheck` script.
  */
 export type TranslationKeyOrString = ParseKeys | (string & {});
 
