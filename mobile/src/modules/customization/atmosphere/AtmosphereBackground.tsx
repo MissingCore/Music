1ecbd75a8762331d7e941e5c3137305b7dd8811b
cyanChill <83375816+cyanChill@users.noreply.github.com>
2026-07-27 21:12:59 -0400

    chore: Go back to using an absolute-positioned image
    - We want to use the `brightness` filter to get white text to always look legible ontop of the blurred image. The problem is that when applied to `<ImageBackground />`, it wraps around all the content, which darkens everything, not just the image.

--- mobile/src/modules/customization/atmosphere/AtmosphereBackground.tsx
+++ mobile/src/modules/customization/atmosphere/AtmosphereBackground.tsx
@@ -1,7 +1,8 @@
 // Copyright (C) 2024 - present, MissingCore
 // SPDX-License-Identifier: AGPL-3.0-only
 
-import { ImageBackground as ExpoImageBackground } from "expo-image";
+import { Image as ExpoImage } from "expo-image";
+import { useWindowDimensions } from "react-native";
 import { ScopedTheme, withUniwind } from "uniwind";
 
 import { usePreferenceStore } from "~/stores/Preference/store";
@@ -10,21 +11,34 @@ import { getImageUri } from "~/lib/file-system";
 import type { Maybe } from "~/utils/types";
 import { DisableGradient } from "~/components/Gradient";
 
-const ImageBackground = withUniwind(ExpoImageBackground);
+const Image = withUniwind(ExpoImage);
 
 export function AtmosphereBackground(props: {
   children: React.ReactNode;
   source: Maybe<string>;
 }) {
+  const dimensions = useWindowDimensions();
   const atmosphereEffect = usePreferenceStore((s) => s.atmosphereEffect);
+
+  const imgSize = Math.max(dimensions.height, dimensions.width) * 1.25;
 
   if (!atmosphereEffect || !props.source) return props.children;
   return (
     <DisableGradient>
       <ScopedTheme theme="atmosphere">
-        <ImageBackground source={getImageUri(props.source)} blurRadius={5}>
-          {props.children}
-        </ImageBackground>
+        <Image
+          source={getImageUri(props.source)}
+          blurRadius={5}
+          // @ts-expect-error - Brightness prop works.
+          style={{
+            height: imgSize,
+            width: imgSize,
+            // Reduce brightness of image so that white text is legible.
+            filter: [{ brightness: "80%" }],
+          }}
+          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+        />
+        {props.children}
       </ScopedTheme>
     </DisableGradient>
   );
