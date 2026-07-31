bf75cc11d8c6f307a9ea97267c75e66e91f1f75f
cyanChill <83375816+cyanChill@users.noreply.github.com>
2026-07-30 22:05:34 -0400

    chore: Temporary opacity slider design
    - We'll probably follow in the footsteps of the waveform sldier and overlay the opacity slider appearance on top of a transparent slider.

--- mobile/src/modules/customization/theme/components/OpacitySlider.tsx
+++ mobile/src/modules/customization/theme/components/OpacitySlider.tsx
@@ -1,4 +1,45 @@
+import { useEffect, useState } from "react";
+import { useAnimatedReaction, useSharedValue } from "react-native-reanimated";
+import { scheduleOnRN } from "react-native-worklets";
+
+import { LabeledSlider } from "~/components/Form/Slider.variant";
+
+export function OpacitySlider(props: {
+  value: string;
+  onComplete: (hex: string) => void;
+}) {
+  const alpha = useSharedValue(1);
+
+  useEffect(() => {
+    alpha.set(hexToAlpha(props.value));
+  }, [alpha, props.value]);
+
+  const [_alpha, _setAlpha] = useState(1);
+  useAnimatedReaction(
+    () => alpha.get(),
+    (currVal) => scheduleOnRN(_setAlpha, currVal),
+  );
+
+  return (
+    <LabeledSlider
+      {...SliderConfig}
+      liveValue={alpha} //? Will override the initial value.
+      onComplete={(alpha) => props.onComplete(alphaToHex(alpha))}
+      displayedValue={`${Math.round(_alpha * 100)}%`}
+    />
+  );
+}
+
 //#region Helpers
+const SliderConfig = {
+  initValue: 1,
+  min: 0,
+  max: 1,
+  step: 0.01,
+  thickness: 28,
+  hitSlop: 0,
+};
+
 /** Returns a value between 0 (0x00) & 1 (0xFF), rounded to 2 decimal places. */
 function hexToAlpha(hex: string) {
   if (hex.length !== 2) return 1;
