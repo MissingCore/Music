9d3c11392735b29a778a03de01d66cc64d06353e
cyanChill <83375816+cyanChill@users.noreply.github.com>
2026-08-03 16:02:16 -0400

    chore: New `RadioChipField` component

--- mobile/src/components/Form/Radio.tsx
+++ mobile/src/components/Form/Radio.tsx
@@ -1,6 +1,7 @@
 // Copyright (C) 2024 - present, MissingCore
 // SPDX-License-Identifier: AGPL-3.0-only
 
+import type { ParseKeys } from "i18next";
 import { memo } from "react";
 import type { StyleProp, ViewStyle } from "react-native";
 import { View } from "react-native";
@@ -8,7 +9,9 @@ import { View } from "react-native";
 import { Icon } from "~/resources/icons";
 
 import { cn } from "~/lib/style";
+import { Button } from "./Button";
 import { Ripple } from "../Base/Pressable";
+import { TStyledText } from "../Typography/StyledText";
 
 //#region Base
 export function Radio(props: {
@@ -52,6 +55,35 @@ export const RadioField = memo(function RadioField(props: RadioFieldProps) {
       {props.children}
       <Radio selected={props.selected} />
     </Ripple>
+  );
+});
+//#endregion
+
+//#region Chip
+type RadioChipProps = Omit<RadioFieldProps, "children"> & {
+  labelKey: ParseKeys;
+  disabled?: boolean;
+};
+
+export const RadioChipField = memo(function RadioChipField(
+  props: RadioChipProps,
+) {
+  return (
+    <Button
+      accessibilityRole="radio"
+      accessibilityState={{ selected: props.selected }}
+      onPress={props.onSelect}
+      disabled={props.disabled || props.selected}
+      className={cn(
+        "min-h-auto rounded-full bg-surfaceContainerLow py-2 disabled:opacity-100",
+        { "bg-primary": props.selected },
+      )}
+    >
+      <TStyledText
+        textKey={props.labelKey}
+        className={cn("text-xs", { "text-onPrimary": props.selected })}
+      />
+    </Button>
   );
 });
 //#endregion
