04b2950a65de38ef825f7d7654207e4e7e47bf08
cyanChill <83375816+cyanChill@users.noreply.github.com>
2026-08-15 15:35:50 -0400

    chore: Remove responsive routing "hack"
    - Basically, we delay the render of the actual screen, so that navigation can happen smoothly before the screen renders (which the rendering may cause stuttering in the animation).
    - Before, the user couldn't clearly notice if the navigation went through due to the press effect being opacity-based. With the ripple effect, it's more clear and gives a better indication that the navigation request was recieved.

--- mobile/src/navigation/routes.tsx
+++ mobile/src/navigation/routes.tsx
@@ -60,7 +60,6 @@ import type { Tab } from "~/stores/Preference/types";
 import { AtmosphereThemeListener } from "~/modules/customization/atmosphere/AtmosphereThemeListener";
 import { TrackMultiSelectListeners } from "~/modules/media/multiSelect/components/TrackMultiSelectListeners";
 import { BottomActions } from "./components/BottomActions";
-import { DeferredRender } from "./components/DeferredRender";
 import { TopAppBar } from "./components/TopAppBar";
 
 //#region Root Screens
@@ -238,7 +237,6 @@ export const RootStack = createNativeStackNavigator({
       <PortalHost />
     </>
   ),
-  screenLayout: ({ children }) => <DeferredRender>{children}</DeferredRender>,
   screenOptions: {
     header: TopAppBar,
     title: "",
@@ -253,7 +251,6 @@ export const RootStack = createNativeStackNavigator({
       linking: {
         path: "now-playing",
       },
-      layout: ({ children }) => children,
       options: {
         headerShown: false,
         presentation: "formSheet",
