c4414176ef6ab5b8c25be917a796b958fda3131d
cyanChill <83375816+cyanChill@users.noreply.github.com>
2026-09-03 16:31:55 -0400

    fix: Unnecessarily always creating a new album entry
    - Due to using `upsertAlbum()`, we would always create a new album instead of updating the existing value if we changed `name` or the album artists.
      - This would cause us to create new albums without any artwork.
      - We now use `updateAlbum()`, which if it throws an error, we use `upsertAlbum()` instead.
    - Also group "context" related form state and ignore validating it.

--- mobile/src/navigation/screens/albums/ModifyView.tsx
+++ mobile/src/navigation/screens/albums/ModifyView.tsx
@@ -3,15 +3,17 @@
 
 import { toast } from "@missingcore/ui/toast";
 import type { StaticScreenProps } from "@react-navigation/native";
-import { inArray } from "drizzle-orm";
+import { eq, inArray } from "drizzle-orm";
 import { z } from "zod/mini";
 
 import { db } from "~/db";
-import { tracks, tracksToGenres } from "~/db/schema";
+import type { albums } from "~/db/schema";
+import { albumsToArtists, tracks, tracksToGenres } from "~/db/schema";
 
-import { upsertAlbums } from "~/data/album/api";
+import { updateAlbum, upsertAlbums } from "~/data/album/api";
 import { useAlbum } from "~/data/album/queries";
 import { AlbumArtistsKey } from "~/data/album/utils";
+import { createArtists } from "~/data/artist/api";
 import { createGenres } from "~/data/genre/api";
 import { Resynchronize } from "~/stores/Playback/actions";
 import { AppCleanUp } from "~/modules/startup/scanning/core/cleanup";
@@ -47,8 +49,13 @@ export default function ModifyAlbum({
     <FormStateProvider
       schema={AlbumMetadataSchema}
       initData={{
-        id: data.id,
-        trackIds: data.tracks.map((t) => t.id),
+        albumContext: {
+          id: data.id,
+          name: data.name,
+          artistsKey: data.artistsKey,
+          isEP: data.isEP,
+          trackIds: data.tracks.map((t) => t.id),
+        },
         name: data.name,
         artists: AlbumArtistsKey.deconstruct(data.artistsKey),
         isEP: data.isEP,
@@ -56,6 +63,7 @@ export default function ModifyAlbum({
         genres: [],
       }}
       onSubmit={onEditAlbum}
+      omittedFields={["albumContext"]}
       //? Prevent duplicates.
       onConstraints={({ artists, genres }) =>
         artists.length ===
@@ -99,8 +107,13 @@ function MetadataForm() {
 //#region Schema
 const AlbumMetadataSchema = z.object({
   // Additional context:
-  id: z.string(),
-  trackIds: z.array(ZSchema.NonEmptyString),
+  albumContext: z.object({
+    id: z.string(),
+    name: z.string(),
+    artistsKey: z.string(),
+    isEP: z.boolean(),
+    trackIds: z.array(ZSchema.NonEmptyString),
+  }),
   // Actual form fields:
   name: ZSchema.NonEmptyString,
   artists: z.array(ZSchema.NonEmptyString).check(z.minLength(1)),
@@ -120,47 +133,75 @@ function useFormState() {
 //#region Submit Handler
 async function onEditAlbum(data: AlbumMetadata) {
   try {
-    const { id: albumId, trackIds, year, genres, ...albumBase } = data;
+    const {
+      albumContext: { id: albumId, trackIds, ...ctx },
+      year,
+      genres,
+      ...albumBase
+    } = data;
 
-    //? First update year & genres on tracks if specified (as we don't
+    //? We shouldn't be able to edit an album if it has no tracks.
+    if (trackIds.length === 0) return router.back();
+
+    //? 1. First update year & genres on tracks if specified (as we don't
     //? want to override unrelated tracks if the album gets changed).
-    if (trackIds.length > 0) {
-      if (year !== null) {
-        await db
-          .update(tracks)
-          .set({ year })
-          .where(inArray(tracks.id, trackIds));
-      }
-
-      if (genres.length > 0) {
-        const genreEntries = genres.flatMap((genreName) =>
-          trackIds.map((trackId) => ({ trackId, genreName })),
-        );
-        await createGenres(genres.map((name) => ({ name })));
-        await db.transaction(async (tx) => {
-          await tx
-            .delete(tracksToGenres)
-            .where(inArray(tracksToGenres.trackId, trackIds));
-          await tx.insert(tracksToGenres).values(genreEntries);
-        });
-      }
+    if (year !== null) {
+      await db.update(tracks).set({ year }).where(inArray(tracks.id, trackIds));
     }
 
-    //? If we change `name` & `artistsKey` to an album that already exists,
-    //? we replace this album with that album.
-    const [updatedAlbum] = await upsertAlbums([
-      {
-        name: albumBase.name,
-        artistsKey: AlbumArtistsKey.from(albumBase.artists)!,
-        isEP: albumBase.isEP,
-      },
-    ]);
+    if (genres.length > 0) {
+      const genreEntries = genres.flatMap((genreName) =>
+        trackIds.map((trackId) => ({ trackId, genreName })),
+      );
+      await createGenres(genres.map((name) => ({ name })));
+      await db.transaction(async (tx) => {
+        await tx
+          .delete(tracksToGenres)
+          .where(inArray(tracksToGenres.trackId, trackIds));
+        await tx.insert(tracksToGenres).values(genreEntries);
+      });
+    }
 
-    if (trackIds.length > 0 && updatedAlbum && updatedAlbum.id !== albumId) {
-      await db
-        .update(tracks)
-        .set({ albumId: updatedAlbum.id })
-        .where(inArray(tracks.id, trackIds));
+    //? 2. Update the album entry.
+    const albumContent: Partial<typeof albums.$inferInsert> = {};
+    if (ctx.name !== albumBase.name) albumContent.name = albumBase.name;
+    const artistsKey = AlbumArtistsKey.from(albumBase.artists)!;
+    if (ctx.artistsKey !== artistsKey) albumContent.artistsKey = artistsKey;
+    if (ctx.isEP !== albumBase.isEP) albumContent.isEP = albumBase.isEP;
+
+    let returnedAlbumId: string | undefined = albumId;
+    if (Object.keys(albumContent).length > 0) {
+      try {
+        await updateAlbum(albumId, albumContent);
+        //? If we're updating `artistsKey`, we need to re-create those relations.
+        if (albumContent.artistsKey) {
+          await createArtists(albumBase.artists.map((name) => ({ name })));
+          await db.transaction(async (tx) => {
+            await tx
+              .delete(albumsToArtists)
+              .where(eq(albumsToArtists.albumId, albumId));
+            await tx.insert(albumsToArtists).values(
+              albumBase.artists.map((artistName) => ({
+                albumId,
+                artistName,
+              })),
+            );
+          });
+        }
+      } catch {
+        //? If we crash, then an album with the changed `name` or `artistsKey`
+        //? already exists. In that case, we use that album's id.
+        const upsertContent = { ...ctx, ...albumContent };
+        const [updatedAlbum] = await upsertAlbums([upsertContent]);
+        //? This should be defined.
+        returnedAlbumId = updatedAlbum?.id;
+
+        if (returnedAlbumId)
+          await db
+            .update(tracks)
+            .set({ albumId: returnedAlbumId })
+            .where(inArray(tracks.id, trackIds));
+      }
     }
 
     // Revalidate `activeTrack` in Playback store if needed.
@@ -168,6 +209,7 @@ async function onEditAlbum(data: AlbumMetadata) {
     await AppCleanUp.media();
     clearAllQueries();
     router.back();
+    router.navigate("Album", { id: returnedAlbumId ?? albumId });
   } catch {
     toast.tError("err.flow.generic.title");
   }
