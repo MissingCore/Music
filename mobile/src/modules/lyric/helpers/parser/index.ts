521cd16929bdc0672d4d7b5aba966fee0227e9ca
cyanChill <83375816+cyanChill@users.noreply.github.com>
2026-09-04 18:27:43 -0400

    chore: Expose a universal parser helper function

---
+++ mobile/src/modules/lyric/helpers/parser/index.ts
@@ -0,0 +1,20 @@
+// Copyright (C) 2024 - present, MissingCore
+// SPDX-License-Identifier: AGPL-3.0-only
+
+import { parseLRC } from "./parseLRC";
+import { parseTTML } from "./parseTTML";
+import type { SynchronizedLine } from "./utils";
+
+/** Identifies and returns the lyrics we want to display. */
+export function parseLyrics(lyrics: string): string | SynchronizedLine[] {
+  if (lyrics.includes(`xmlns="http://www.w3.org/ns/ttml"`)) {
+    return parseTTML(lyrics);
+  }
+
+  //* Current way of testing for `LRC` or `LRC A2` format is to run it
+  //* through the parser and see if it returns a result.
+  const results = parseLRC(lyrics);
+  if (results.length > 0) return results;
+
+  return lyrics;
+}
