0b2d64619e6683fc9230e257d6148f851c9323cb
cyanChill <83375816+cyanChill@users.noreply.github.com>
2026-09-09 19:16:55 -0400

    chore: Re-export `<Icon />` from the components folder
    - We'll eventually remove the export from `~/resources/icons`.

---
+++ mobile/src/components/next/base/icon.tsx
@@ -0,0 +1,32 @@
+// Copyright (C) 2024 - present, MissingCore
+// SPDX-License-Identifier: AGPL-3.0-only
+
+import { createNanoIconSet } from "react-native-nano-icons";
+
+import type { AppColor } from "~/modules/customization/theme/core/constants";
+import { useColor } from "~/modules/customization/theme/hooks";
+import glyphMap from "~/resources/icons/app-icons.glyphmap.json";
+
+const AppIcons = createNanoIconSet(glyphMap);
+
+export type SupportedIconName = React.ComponentProps<typeof AppIcons>["name"];
+
+type Props = {
+  name: SupportedIconName;
+  /** Defaults to `24px`. */
+  size?: number;
+  /** Defaults to theme's `onSurface` color. */
+  color?: AppColor;
+};
+
+export function Icon({ name, size = 24, color }: Props) {
+  const usedColor = useColor(color, "onSurface");
+  return (
+    <AppIcons
+      name={name}
+      size={size}
+      color={usedColor}
+      allowFontScaling={false}
+    />
+  );
+}
