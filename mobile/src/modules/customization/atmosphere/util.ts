4dd941d7a3540c2f0182f56c30cafdcd7aa4261c
cyanChill <83375816+cyanChill@users.noreply.github.com>
2026-09-08 23:19:11 -0400

    refactor: Update `deriveAndSetAtmosphereColors` to use `react-native-material-palette`
    - The flicker is basically gone for the most part. For some images, we may see a brief but not that noticeable flicker for the primary color.

--- mobile/src/modules/customization/atmosphere/util.ts
+++ mobile/src/modules/customization/atmosphere/util.ts
@@ -1,9 +1,9 @@
 // Copyright (C) 2024 - present, MissingCore
 // SPDX-License-Identifier: AGPL-3.0-only
 
-import type { PaletteResult } from "@somesoap/react-native-image-palette";
-import { getPalette } from "@somesoap/react-native-image-palette";
 import { Appearance } from "react-native";
+import type { PaletteResult as RawPaletteResult } from "react-native-material-palette";
+import { createPalette } from "react-native-material-palette";
 import { Uniwind } from "uniwind";
 
 import { preferenceStore } from "~/stores/Preference/store";
@@ -11,6 +11,9 @@ import { preferenceStore } from "~/stores/Preference/store";
 import type { ColorRole, HexColor } from "../theme/core/constants";
 import { Themes } from "../theme/core/constants";
 import { hexToHSV, hsvToHex } from "../theme/helpers/colorConverter";
+
+type PaletteKeys = keyof RawPaletteResult;
+type PaletteResult = Record<PaletteKeys, HexColor | undefined>;
 
 const UpdatedColorRoles = [
   "primary",
@@ -20,9 +23,9 @@ const UpdatedColorRoles = [
 ] as const satisfies ColorRole[];
 
 const paletteColors = {
-  primary: ["muted", "lightMuted", "dominantAndroid"],
-  secondary: ["lightVibrant", "vibrant", "darkMuted", "dominantAndroid"],
-} as const satisfies Record<string, Array<keyof PaletteResult>>;
+  primary: ["muted", "lightMuted", "dominant"],
+  secondary: ["lightVibrant", "vibrant", "darkMuted", "dominant"],
+} as const satisfies Record<string, PaletteKeys[]>;
 
 /** Updates "Atmosphere" Uniwind theme based on image color or fallback. */
 export async function deriveAndSetAtmosphereColors(
@@ -33,7 +36,22 @@ export async function deriveAndSetAtmosphereColors(
   let palette: PaletteResult | undefined;
   if (imgSrc) {
     try {
-      palette = await getPalette(imgSrc);
+      const rawPalette = await createPalette({ uri: imgSrc });
+      // Convert the `color` property into a Hex color as `react-native-material-palette`
+      // reformates the value returned by the Palette API as rgb/rgba.
+      palette = Object.fromEntries(
+        Object.entries(rawPalette).map(([key, result]) => [
+          key,
+          result
+            ? `#` +
+              result.color
+                .match(/\d+/g)!
+                .slice(0, 3)
+                .map((x) => parseInt(x).toString(16).padStart(2, "0"))
+                .join("")
+            : undefined,
+        ]),
+      ) as PaletteResult;
     } catch {}
   }
 
@@ -42,17 +60,12 @@ export async function deriveAndSetAtmosphereColors(
   const updatedVariables = getAtmosphereThemeVariables();
 
   if (palette) {
-    // Returns a color that isn't the fallback & not the dominant color
-    // (if the key isn't `dominantAndroid`).
-    const getSuitableColor = (keys: ReadonlyArray<keyof PaletteResult>) => {
-      const dominantColor = palette.dominantAndroid!;
+    // Returns a color that isn't the dominant color if the key isn't `dominant`.
+    const getSuitableColor = (keys: readonly PaletteKeys[]) => {
+      const dominantColor = palette.dominant;
       for (const key of keys) {
-        const color = palette[key] as HexColor;
-        if (
-          color === "#FFFFFF" ||
-          (key !== "dominantAndroid" && color === dominantColor)
-        )
-          continue;
+        const color = palette[key];
+        if (!color || (key !== "dominant" && color === dominantColor)) continue;
         return color;
       }
       return null;
