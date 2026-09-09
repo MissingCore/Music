ba37b5c452fd9090d26da165cceac166e96ec2e9
cyanChill <83375816+cyanChill@users.noreply.github.com>
2026-09-09 18:12:22 -0400

    chore: Have `cva` use our `tailwind-merge` config
    - https://cva.style/beta/getting-started/installation/#tailwind-merge

--- mobile/src/lib/style.ts
+++ mobile/src/lib/style.ts
@@ -1,8 +1,8 @@
 // Copyright (C) 2024 - present, MissingCore
 // SPDX-License-Identifier: AGPL-3.0-only
 
-import type { ClassValue } from "clsx";
 import { clsx } from "clsx";
+import { defineConfig } from "cva/config";
 import { extendTailwindMerge } from "tailwind-merge";
 
 import { FontSize } from "~/constants/Styles";
@@ -12,6 +12,7 @@ import { ColorRoleOptions } from "~/modules/customization/theme/core/constants";
 //#region Color
 // Need to include `transparent` as otherwise, things will get merged incorrectly.
 const AvailableColors = ["transparent", ...ColorRoleOptions] as const;
+//#endregion
 
 const customTwMerge = extendTailwindMerge({
   override: {
@@ -25,8 +26,10 @@ const customTwMerge = extendTailwindMerge({
   },
 });
 
-/** Combines any number of Tailwind classes nicely. */
-export function cn(...inputs: ClassValue[]) {
-  return customTwMerge(clsx(inputs));
-}
-//#endregion
+export const {
+  cva,
+  /** Combines any number of Tailwind classes nicely. */
+  cx: cn,
+} = defineConfig({
+  cx: (...inputs) => customTwMerge(clsx(inputs)),
+});
