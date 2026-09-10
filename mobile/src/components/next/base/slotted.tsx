868d00d90a44e4d1420e67d412b2cdaaf45e14b0
cyanChill <83375816+cyanChill@users.noreply.github.com>
2026-09-10 14:36:23 -0400

    chore: New `createSlottedComponent` helper
    - This simplifies generating component from a common pattern (we have optional Leading & Trailing elements with some content in the middle).
      - ie: Buttons, List Items
    - If we want to pre-populate some fields (ie: Leading/Trailing), we can just have another component render the generated component and exclude those fields from the type.

---
+++ mobile/src/components/next/base/slotted.tsx
@@ -0,0 +1,38 @@
+// Copyright (C) 2024 - present, MissingCore
+// SPDX-License-Identifier: AGPL-3.0-only
+
+import { View } from "react-native";
+
+import { cn } from "~/lib/style";
+
+export function createSlottedComponent<
+  TContent extends (props: any) => React.ReactNode,
+  TWrapper extends React.ElementType = typeof View,
+>(args: { Content: TContent; Wrapper?: TWrapper }) {
+  const { Wrapper = View, Content } = args;
+
+  type SlottedComponentProps = React.ComponentProps<TWrapper> & {
+    contentConfig: React.ComponentProps<TContent>;
+    Leading?: React.ReactNode;
+    Trailing?: React.ReactNode;
+  };
+
+  return function SlottedComponent({
+    contentConfig,
+    Leading,
+    Trailing,
+    className,
+    ...props
+  }: SlottedComponentProps) {
+    return (
+      <Wrapper
+        {...props}
+        className={cn("flex-row items-center gap-2", className)}
+      >
+        {Leading}
+        <Content {...contentConfig} />
+        {Trailing}
+      </Wrapper>
+    );
+  };
+}
